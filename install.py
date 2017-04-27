ff3fdeae14e70078610d7144eea52a8d186d31c5
sebastian <sebmuell@phys.ethz.ch>
2017-04-27 11:27:44 +0200

    Input corsika credentials via program parameters

--- install.py
+++ install.py
@@ -1,34 +1,51 @@
+#! /usr/bin/env python
+"""
+Install the Atmospheric Cherenkov Plenoscope (ACP) starter kit.
+
+We apologize for this, but you need to have the credentials to the KIT CORSIKA 
+air-shower simulation software.
+
+Go visit https://www.ikp.kit.edu/corsika/ and kindly ask for the username and 
+password combination.
+
+Usage: install --username=USERNAME --password=PASSWORD
+
+Options:
+    --username=USERNAME                 Username for the KIT CORSIKA ftp-server
+    --password=PASSWORD                 Password fot the KIT CORSIKA ftp-server
+"""
+import docopt
 import os
 from subprocess import call
 
-corsika_username = '' 
-corsika_password = ''
+def main():
+    try:
+        arguments = docopt.docopt(__doc__)
 
-call(['pip','install', './corsika_install/'])
+        call(['pip','install', './corsika_install/'])
+        call(['mkdir', 'build'])
+        call(['acp_corsika_install',
+            '-p', './build/corsika', 
+            '--username', arguments['--username'], 
+            '--password', arguments['--password']])
+        call(['pip','install', './corsika_wrapper/'])
+        call(['corsika', 
+            '-c',
+            'build/corsika/corsika-75600/run/corsika75600Linux_QGSII_urqmd'])
+        call(['mkdir', './build/mctracer'])
+        os.chdir('./build/mctracer')
+        call(['cmake', '../../mctracer'])
+        call(['make', '-j', '12'])
+        call(['touch', './../../mctracer/CMakeLists.txt'])
+        call(['make', '-j', '12'])
+        os.chdir('./../..')
+        call(['pip','install', './plenopy/'])
+        call(['pip','install', './gamma_limits_sensitivity/'])
+        call(['pip','install', './instrument_response_function/'])
+        call(['pip','install', './instrument_sensitivity_function/'])
 
-call(['mkdir', 'build'])
+    except docopt.DocoptExit as e:
+        print(e)
 
-call([
-    'acp_corsika_install',
-    '-p', './build/corsika', 
-    '--username', corsika_username, 
-    '--password', corsika_password])
-
-call(['pip','install', './corsika_wrapper/'])
-call([
-    'corsika', 
-    '-c',
-    'build/corsika/corsika-75600/run/corsika75600Linux_QGSII_urqmd'])
-
-call(['mkdir', './build/mctracer'])
-os.chdir('./build/mctracer')
-call(['cmake', '../../mctracer'])
-call(['make', '-j', '12'])
-call(['touch', './../../mctracer/CMakeLists.txt'])
-call(['make', '-j', '12'])
-os.chdir('./../..')
-
-call(['pip','install', './plenopy/'])
-call(['pip','install', './gamma_limits_sensitivity/'])
-call(['pip','install', './instrument_response_function/'])
-call(['pip','install', './instrument_sensitivity_function/'])
+if __name__ == '__main__':
+    main()
