6a4c7dc57ca34ecf6fc9b6eb9555c3140ef4fb2b
sebastian <sebmuell@phys.ethz.ch>
2017-04-27 15:47:12 +0200

    Use a folder structure, and always use os.path.join to join paths

--- example_workflow.py
+++ example_workflow.py
@@ -4,56 +4,58 @@ import pkg_resources
 from subprocess import call
 import acp_instrument_response_function as acp_irf
 
-os.makedirs('./run', exist_ok=True)
+os.makedirs('run', exist_ok=True)
 
 # Plenoscope optical light field calibration
-if not os.path.isdir('./run/light_field_calibration'):
+if not os.path.isdir(join('run','light_field_calibration')):
     call([
-        './build/mctracer/mctPlenoscopeCalibration',
+        join('build','mctracer','mctPlenoscopeCalibration'),
         '--scenery', pkg_resources.resource_filename(
             'acp_instrument_response_function', 
-            'resources/71m_acp/scenery'),
+            join('resources','71m_acp','scenery')),
         '--number_mega_photons', '10',
-        '--output', './run/light_field_calibration'])
+        '--output', join('run','light_field_calibration')])
 
 particles = ['gamma', 'electron', 'proton']
 
+os.makedirs(join('run','irf'), exist_ok=True)
+
 for p in particles:
-    if not os.path.isdir('./run/'+p+'_irf'):
+    if not os.path.isdir(join('run','irf',p)):
         call([
             'acp_instrument_response_function',
             '--corsika_card', pkg_resources.resource_filename(
                 'acp_instrument_response_function', 
-                'resources/71m_acp/'+p+'_steering_card.txt' ),
-            '--output_path', './run/'+p+'_irf',
+                join('resources','71m_acp',p+'_steering_card.txt')),
+            '--output_path', join('run','irf',p),
             '--number_of_runs', '4',
-            '--acp_detector', './run/light_field_calibration',
+            '--acp_detector', join('run','light_field_calibration'),
             '--mct_acp_config', pkg_resources.resource_filename(
                 'acp_instrument_response_function', 
-                'resources/propagation_config.xml'),
-            '--mct_acp_propagator', './build/mctracer/mctPlenoscopePropagation',])
+                join('resources','propagation_config.xml')),
+            '--mct_acp_propagator', join('build','mctracer','mctPlenoscopePropagation'),])
 
-os.makedirs('./run/irf_results', exist_ok=True)
+os.makedirs(join('run','irf','results'), exist_ok=True)
 
 for p in particles:
-    result_path = join('./run/irf_results', p+'_irf.csv')
+    result_path = join('run','irf','results',p+'.csv')
     if not os.path.isfile(result_path):
         acp_irf.gamma_limits_bridge.export_effective_area(
-            input_path='./run/'+p+'_irf',
+            input_path=join('run','irf',p),
             detector_responses_key='raw_lixel_sum',
             detector_response_threshold=100,
             output_path=result_path,
             bins=5)
 
-if not os.path.isdir('./run/isf'):
-    os.makedirs('./run/isf', exist_ok=True)
+if not os.path.isdir(join('run','isf')):
+    os.makedirs(join('run','isf'), exist_ok=True)
     call([
         'acp_isez',
-        '--gamma_area', join('./run/irf_results', 'gamma'+'_irf.csv'),
-        '--electron_acceptance', join('./run/irf_results', 'electron'+'_irf.csv'),
-        '--proton_acceptance', join('./run/irf_results', 'proton'+'_irf.csv'),
+        '--gamma_area', join('run','irf','results','gamma.csv'),
+        '--electron_acceptance', join('run','irf','results','electron.csv'),
+        '--proton_acceptance', join('run','irf','results','proton.csv'),
         '--cutoff', '0.01',
         '--rel_flux', '0.05',
         '--fov', '6.5',
         '--src', '3FGL J2254.0+1608',
-        '--out', './run/isf'])
+        '--out', join('run','isf')])
