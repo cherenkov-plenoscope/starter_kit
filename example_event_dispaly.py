120d41594e34a905e0a778aa5d6ed487800c32e8
sebastian <sebmuell@phys.ethz.ch>
2017-05-04 09:05:59 +0200

    A list of manual picked events found in a gamma-ray run which are representative and interesting to be plotted.

--- example_event_dispaly.py
+++ example_event_dispaly.py
@@ -6,6 +6,24 @@ import plenopy as pl
 import corsika_wrapper as cw
 
 
+"""
+A list of interesting gamma events to be found in the example run to be 
+simulated.
+
+Run 1, Event 17, E 0.71GeV, I 109p.e., 
+Run 1, Event 25, E 2.88GeV, I 146p.e.,
+Run 1, Event 32, E 1.41GeV, I 129p.e.,
+Run 1, Event 35, E 2.92GeV, I 139p.e.,
+Run 1, Event 40, E 0.67GeV, I 145p.e.,
+Run 1, Event 46, E 2.71GeV, I 101p.e.,
+Run 1, Event 47, E 1.56GeV, I 104p.e.,
+Run 1, Event 49, E 3.71GeV, I 2002p.e. nice to be refocused,
+Run 1, Event 76, E 2.24GeV, I 161p.e.,
+Run 1, Event 87, E 2.19GeV, I 117p.e.,
+Run 1, Event 90, E 2.12GeV, I 228p.e.,
+Run 1, Event 96, E 3.59GeV, I 1026p.e.,
+"""
+
 exa = 'example_events'
 os.makedirs(exa, exist_ok=True)
 
