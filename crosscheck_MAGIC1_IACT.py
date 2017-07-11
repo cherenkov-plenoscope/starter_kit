ce815b229a2416b3c8059c330685a9a9ceef0ec2
sebastian <sebmuell@phys.ethz.ch>
2017-07-11 21:36:05 +0200

    uniform 'out_dir', set out_dir optional

--- crosscheck_MAGIC1_IACT.py
+++ crosscheck_MAGIC1_IACT.py
@@ -4,9 +4,11 @@ Run a crosscheck where the ACP simulation tools are used to reproduce the known
 instrument response functions and sensitivities of the MAGIC 1 mono telescope.
 You can run this in parallel on a python scoop cluster, but you do not have to.
 
-Usage: crosscheck_MAGIC1_IACT [-s=SCOOP_HOSTS]
+Usage: crosscheck_MAGIC1_IACT [-o=DIR] [-s=SCOOP_HOSTS]
 
 Options:
+    -o --out_dir=DIR                The output directory.
+                                    [default: examples/crosscheck_MAGIC1_IACT]
     -s --scoop_hosts=SCOOP_HOSTS    Path to the scoop hosts text file.
 """
 import docopt
@@ -20,32 +22,32 @@ def main():
     try:
         arguments = docopt.docopt(__doc__)
 
-        m1 = 'crosscheck_MAGIC1_IACT'
+        out_dir = 'crosscheck_MAGIC1_IACT'
 
         # 1) calibration of the 'MAGIC 1 like' telescope
         # ----------------------------------------------
-        os.makedirs(m1, exist_ok=True)
-        if not os.path.isdir(join(m1,'light_field_calibration')):
+        os.makedirs(out_dir, exist_ok=True)
+        if not os.path.isdir(join(out_dir,'light_field_calibration')):
             call([
                 join('build','mctracer','mctPlenoscopeCalibration'),
                 '--scenery', join('resources','iact','MAGIC_1','scenery'),
                 '--number_mega_photons', '25',
-                '--output', join(m1,'light_field_calibration')
+                '--output', join(out_dir,'light_field_calibration')
             ])
 
 
         # 2) Instrument response functions for typical cosmic particles
         # -------------------------------------------------------------
         particles = ['gamma', 'electron', 'proton']
-        os.makedirs(join(m1,'irf'), exist_ok=True)
+        os.makedirs(join(out_dir,'irf'), exist_ok=True)
         for p in particles:
-            if not os.path.isdir(join(m1,'irf',p)):
+            if not os.path.isdir(join(out_dir,'irf',p)):
                 command = [
                     'acp_instrument_response_function',
                     '--corsika_card', join('resources','iact','MAGIC_1',p+'_steering_card.txt'),
-                    '--output_path', join(m1,'irf',p),
+                    '--output_path', join(out_dir,'irf',p),
                     '--number_of_runs', '96',
-                    '--acp_detector', join(m1,'light_field_calibration'),
+                    '--acp_detector', join(out_dir,'light_field_calibration'),
                     '--mct_acp_config', join('resources','iact','MAGIC_1','mct_propagation_config.xml'),
                     '--mct_acp_propagator', join('build','mctracer','mctPlenoscopePropagation'),
                 ]
@@ -54,12 +56,12 @@ def main():
                     command.insert(2, arguments['--scoop_hosts'])
                 call(command)
 
-        os.makedirs(join(m1,'irf','results'), exist_ok=True)
+        os.makedirs(join(out_dir,'irf','results'), exist_ok=True)
         for p in particles:
-            result_path = join(m1,'irf','results',p+'.csv')
+            result_path = join(out_dir,'irf','results',p+'.csv')
             if not os.path.isfile(result_path):
                 acp_irf.gamma_limits_bridge.export_effective_area(
-                    input_path=join(m1,'irf',p),
+                    input_path=join(out_dir,'irf',p),
                     detector_responses_key='raw_lixel_sum',
                     detector_response_threshold=100,
                     output_path=result_path,
@@ -68,18 +70,18 @@ def main():
 
         # 3) Sensitivity and time-to-detections of the ACP
         # ------------------------------------------------
-        if not os.path.isdir(join(m1,'isf')):
-            os.makedirs(join(m1,'isf'), exist_ok=True)
+        if not os.path.isdir(join(out_dir,'isf')):
+            os.makedirs(join(out_dir,'isf'), exist_ok=True)
             call([
                 'acp_isez',
-                '--gamma_area', join(m1,'irf','results','gamma.csv'),
-                '--electron_acceptance', join(m1,'irf','results','electron.csv'),
-                '--proton_acceptance', join(m1,'irf','results','proton.csv'),
+                '--gamma_area', join(out_dir,'irf','results','gamma.csv'),
+                '--electron_acceptance', join(out_dir,'irf','results','electron.csv'),
+                '--proton_acceptance', join(out_dir,'irf','results','proton.csv'),
                 '--cutoff', '0.01',
                 '--rel_flux', '0.05',
                 '--fov', '3.5',
                 '--src', '3FGL J2254.0+1608',
-                '--out', join(m1,'isf')])
+                '--out', join(out_dir,'isf')])
 
     except docopt.DocoptExit as e:
         print(e)
