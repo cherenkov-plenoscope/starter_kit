9be71c1b04d72ef0424c811c301fe7bd0ceb0a54
sebastian <sebmuell@phys.ethz.ch>
2018-03-08 18:05:47 +0100

    pep8 uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -2,12 +2,12 @@
 import os
 from subprocess import call
 
-call(['pip','uninstall', 'acp_corsika_install'])
-call(['pip','uninstall', 'corsika_wrapper'])
-call(['pip','uninstall', 'plenopy'])
-call(['pip','uninstall', 'gamma_limits_sensitivity'])
-call(['pip','uninstall', 'acp_instrument_response_function'])
-call(['pip','uninstall', 'acp_instrument_sensitivity_function'])
-call(['pip','uninstall', 'reflector_study'])
+call(['pip', 'uninstall', 'acp_corsika_install'])
+call(['pip', 'uninstall', 'corsika_wrapper'])
+call(['pip', 'uninstall', 'plenopy'])
+call(['pip', 'uninstall', 'gamma_limits_sensitivity'])
+call(['pip', 'uninstall', 'acp_instrument_response_function'])
+call(['pip', 'uninstall', 'acp_instrument_sensitivity_function'])
+call(['pip', 'uninstall', 'reflector_study'])
 
 call(['rm', '-rf', 'build'])
