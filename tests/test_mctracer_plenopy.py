526c0aa0f24fb03cebf64339ab850fae2ede3fc8
sebastian <sebmuell@phys.ethz.ch>
2018-04-20 17:57:56 +0200

    have a more complete integration-test

--- tests/test_mctracer_plenopy.py
+++ tests/test_mctracer_plenopy.py
@@ -5,6 +5,7 @@ import plenopy as pl
 import tempfile
 import numpy as np
 import pytest
+import corsika_wrapper as cw
 
 
 @pytest.fixture(scope='session')
@@ -115,3 +116,132 @@ def test_plot_light_field_geometry_with_plenopy(tmp):
     for image_filename in expected_images:
         assert os.path.exists(
             join(tmp, 'light_field_geometry', 'plot', image_filename))
+
+
+def test_corsika_simulation(tmp):
+    card = cw.read_steering_card(
+        join('resources', 'acp', '71m', 'calibration_gamma_event.txt'))
+    rc = cw.corsika(
+        card,
+        join(tmp, 'calibration_gamma.evtio'),
+        save_stdout=True)
+    assert rc == 0
+    assert os.path.exists(join(tmp, 'calibration_gamma.evtio'))
+
+
+def test_propagation_with_mctracer(tmp):
+    assert os.path.exists(join(tmp, 'calibration_gamma.evtio'))
+    rc = call([
+        join('build', 'mctracer', 'mctPlenoscopePropagation'),
+        '--lixel', join(tmp, 'light_field_geometry'),
+        '--config', join(
+            'resources', 'acp',
+            'mct_propagation_config_no_night_sky_background.xml'),
+        '--input', join(tmp, 'calibration_gamma.evtio'),
+        '--output', join(tmp, 'calibration_gamma.acp'),
+        '--all_truth',
+        '--random_seed', '1'
+    ])
+    assert rc == 0
+
+
+import shutil
+
+def test_read_run_with_plenopy(tmp):
+    assert os.path.exists(join(tmp, 'calibration_gamma.acp'))
+    shutil.copytree(tmp, '/home/sebastian/Desktop/this_test_thing')
+    run = pl.Run(join(tmp, 'calibration_gamma.acp'))
+    assert len(run) > 1
+    np.random.seed(0)
+
+    direction_from_image_correct = 0
+    direction_from_arrivale_times_correct = 0
+    plane_fit_correct = 0
+
+    for event in run:
+        # ASSERT the incoming directions of the photons are as expected
+        # -------------------------------------------------------------
+        m = event.simulation_truth.event.corsika_event_header.momentum().copy()
+        assert m[2] >= 0.0 # the negative z-component
+        m[2] *= -1.0
+        momentum_vector_in_corsika_frame = m
+
+        approx_direction_of_photons_in_corsika_frame = (
+            momentum_vector_in_corsika_frame/np.linalg.norm(
+                momentum_vector_in_corsika_frame))
+        # photons are running down towards the observation level.
+
+        approx_incoming_direction_of_photons_in_plenoscope_frame = (
+            -1.0 * approx_direction_of_photons_in_corsika_frame)
+        # incoming directions for the plenoscope are pointing upwards into
+        # the sky.
+
+        (arrival_slices, lixel_ids
+        ) = pl.photon_stream.cython_reader.arrival_slices_and_lixel_ids(
+            event.raw_sensor_response)
+
+        cx_mean = np.mean(run.light_field_geometry.cx_mean[lixel_ids])
+        cy_mean = np.mean(run.light_field_geometry.cy_mean[lixel_ids])
+        cx_std = np.std(run.light_field_geometry.cx_mean[lixel_ids])
+        cy_std = np.std(run.light_field_geometry.cy_mean[lixel_ids])
+
+        expected_cx_mean = (
+            approx_incoming_direction_of_photons_in_plenoscope_frame[0])
+        expected_cy_mean = (
+            approx_incoming_direction_of_photons_in_plenoscope_frame[1])
+
+        if (
+            np.isclose(expected_cx_mean, cx_mean, atol=np.deg2rad(0.3)) and
+            np.isclose(expected_cy_mean, cy_mean, atol=np.deg2rad(0.3))
+        ):
+            direction_from_image_correct += 1
+
+        # ASSERT arrival_slices are as expected
+        # -------------------------------------
+        arrival_times_in_sensors = (
+            arrival_slices*event.raw_sensor_response.time_slice_duration)
+
+        arrival_times_in_sensors -= np.median(arrival_times_in_sensors)
+        # absolute arrival time is not relevant
+
+        arrival_times_on_principal_aperture_plane = (
+            arrival_times_in_sensors - run.light_field_geometry.time_delay_mean[
+            lixel_ids])
+
+        speed_of_light = 3e8
+
+        arrival_path_delays_on_principal_aperture_plane = (
+            arrival_times_on_principal_aperture_plane*speed_of_light)
+
+        zs = arrival_path_delays_on_principal_aperture_plane
+        xs = run.light_field_geometry.x_mean[lixel_ids]
+        ys = run.light_field_geometry.y_mean[lixel_ids]
+
+        xyzs = np.c_[xs, ys, zs]
+
+        plane_model, inlier = pl.tools.ransac_3d_plane.fit(
+            xyz_point_cloud=xyzs,
+            max_number_itarations=1000,
+            min_number_points_for_plane_fit=3,
+            max_orthogonal_distance_of_inlier=
+                event.raw_sensor_response.time_slice_duration*speed_of_light)
+
+        if inlier.sum() >= 0.5*event.raw_sensor_response.number_photons:
+            plane_fit_correct += 1
+
+        fit_plane_model = pl.tools.ransac_3d_plane.estimate_3d_plane_model(
+            xyz_point_cloud=xyzs[inlier])
+
+        if (
+            np.isclose(
+                expected_cx_mean, fit_plane_model[0], atol=np.deg2rad(0.5))
+            and
+            np.isclose(
+                expected_cy_mean, fit_plane_model[1], atol=np.deg2rad(0.5))
+        ):
+            direction_from_arrivale_times_correct += 1
+
+    number_events = len(run)
+    assert direction_from_image_correct >= 0.75*number_events
+    assert direction_from_arrivale_times_correct >= 0.5*number_events
+    assert plane_fit_correct >= 0.5*number_events
