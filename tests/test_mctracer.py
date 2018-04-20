f7ed8c0668fa1b710303a0ae07db6db2beb77e56
sebastian <sebmuell@phys.ethz.ch>
2018-04-20 14:34:51 +0200

    switch back to original working-dir after running mctracer-tests

--- tests/test_mctracer.py
+++ tests/test_mctracer.py
@@ -3,7 +3,9 @@ from subprocess import call
 import pytest
 
 def test_all_mctracer_Cpp_test_cases(capsys):
+    cwd = os.getcwd()
     os.chdir(os.path.join('mctracer', 'Tests'))
     with capsys.disabled():
         rtc = call([os.path.join('..', '..','build', 'mctracer', 'mctTest')])
+    os.chdir(cwd)
     assert rtc == 0
