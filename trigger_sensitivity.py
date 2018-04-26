14d45d41ab229f70b49de61bbf369793716248b4
sebastian <sebmuell@phys.ethz.ch>
2018-04-26 20:42:55 +0200

    pull scoop into the top-level script, submitt all jobs at once

--- trigger_sensitivity.py
+++ trigger_sensitivity.py
@@ -13,10 +13,12 @@ Options:
                             [default: 1337]
 """
 import docopt
+import scoop
 import os
 from os.path import join
 from subprocess import call
-import acp_instrument_response_function as acp_irf
+import acp_instrument_response_function as irf
+import random
 
 
 if __name__ == '__main__':
@@ -37,27 +39,27 @@ if __name__ == '__main__':
         # 2) Instrument-response-functions for typical cosmic particles
         # -------------------------------------------------------------
         particles = ['gamma', 'electron', 'proton']
+        jobs = []
         os.makedirs(join(od, 'irf'), exist_ok=True)
         for p in particles:
             if not os.path.isdir(join(od, 'irf', p)):
-                command = [
-                    'acp_trigger_irf',
-                    '--steering_card', join(
+                jobs += irf.trigger_simulation.make_output_directory_and_jobs(
+                    steering_card_path=join(
                         'resources', 'acp', '71m', p+'_steering.json'),
-                    '--output_path', join(od, 'irf', p),
-                    '--acp_detector', join(od, 'light_field_calibration'),
-                    '--mct_acp_config', join(
+                    output_path=join(od, 'irf', p),
+                    acp_detector_path=join(od, 'light_field_calibration'),
+                    mct_acp_config_path=join(
                         'resources', 'acp', 'mct_propagation_config.xml'),
-                    '--mct_acp_propagator', join(
-                        'build', 'mctracer', 'mctPlenoscopePropagation'),]
-                if arguments['--scoop_hosts']:
-                    command.insert(1, '--scoop_hosts')
-                    command.insert(2, arguments['--scoop_hosts'])
-                call(command)
+                    mct_acp_propagator_path=join(
+                        'build', 'mctracer', 'mctPlenoscopePropagation'))
+                print(len(jobs))
+
+        random.shuffle(jobs)
+        rc = list(scoop.futures.map(irf.trigger_simulation.run_job, jobs))
 
         for p in particles:
             if os.path.isdir(join(od, 'irf', p)):
-                acp_irf.trigger_study_analysis.run_analysis(join(od, 'irf', p))
+                irf.trigger_study_analysis.run_analysis(join(od, 'irf', p))
 
         # 3) Sensitivity and time-to-detections of the ACP
         # ------------------------------------------------
