57a0dff43327044eba9c7924665a5afdace0fb55
sebastian <sebmuell@phys.ethz.ch>
2018-07-12 15:02:43 +0200

    towards shower reconstruction

--- air_shower_reconstruction.py
+++ air_shower_reconstruction.py
@@ -4,35 +4,38 @@ import plenopy as pl
 import json
 import os
 import scipy
-import shutil
 
+light_field_geometry = pl.LightFieldGeometry(
+    os.path.join('run', 'light_field_calibration'))
 
 run = pl.Run(
-    '/home/sebastian/Desktop/phd/starter_kit/run/irf/electron/past_trigger',
-    light_field_geometry=pl.LightFieldGeometry(
-        '/home/sebastian/Desktop/phd/starter_kit/run/light_field_calibration'))
-
-time_radius_roi = 5e-9
-c_radius = np.deg2rad(0.3)
-epsilon_cx_cy_radius = np.deg2rad(0.075)
-min_number_photons = 20
-deg_over_s = 0.375e9
+    os.path.join('run', 'irf', 'electron', 'past_trigger'),
+    light_field_geometry=light_field_geometry)
 
 sum_tpr = 0
 sum_ppv = 0
 number_events = 0
 
 for event in run:
-    roi = pl.classify.center_for_region_of_interest(event)
-    ph = pl.classify.RawPhotons.from_event(event)
+    primary_momentum = event.simulation_truth.event.corsika_event_header.momentum()
+    primary_direction = primary_momentum/np.linalg.norm(primary_momentum)
 
-    ph_cherenkov = pl.classify.cherenkov_photons_in_roi_in_image(
+    core_cx = - primary_direction[0]
+    core_cy = - primary_direction[1]
+    core_x = event.simulation_truth.event.corsika_event_header.core_position_x_meter()
+    core_y = event.simulation_truth.event.corsika_event_header.core_position_y_meter()
+    energy = event.simulation_truth.event.corsika_event_header.total_energy_GeV
+
+    roi = pl.classify.center_for_region_of_interest(event)
+    photons = pl.classify.RawPhotons.from_event(event)
+
+    cherenkov_photons = pl.classify.cherenkov_photons_in_roi_in_image(
         roi=roi,
-        photons=ph)
+        photons=photons)
 
     b = pl.classify.benchmark(
         pulse_origins=event.simulation_truth.detector.pulse_origins,
-        photon_ids_cherenkov=ph_cherenkov.photon_ids)
+        photon_ids_cherenkov=cherenkov_photons.photon_ids)
 
     tpr = b['number_true_positives']/(
         b['number_true_positives'] + b['number_false_negatives'])
@@ -43,12 +46,12 @@ for event in run:
     sum_ppv += ppv
     number_events += 1
 
-    print(
-        'tpr:', sum_tpr/number_events,
-        'ppv:', sum_ppv/number_events)
+    #print(
+    #    'tpr:', sum_tpr/number_events,
+    #    'ppv:', sum_ppv/number_events)
 
     if (
-            ph.photon_ids.shape[0] -
+            photons.photon_ids.shape[0] -
             b['number_true_negatives'] -
             b['number_true_positives'] -
             b['number_false_positives'] -
@@ -56,21 +59,49 @@ for event in run:
         ) != 0:
         break
 
+    trigger_offset = np.sqrt(
+        (roi['cx_center_roi'] - core_cx)**2 +
+        (roi['cy_center_roi'] - core_cy)**2)
 
+    flash_offset = np.sqrt(
+        (np.mean(cherenkov_photons.cx) - core_cx)**2 +
+        (np.mean(cherenkov_photons.cy) - core_cy)**2)
 
+    B, inlier = pl.tools.ransac_3d_plane.fit(
+        xyz_point_cloud=np.c_[
+            cherenkov_photons.x,
+            cherenkov_photons.y,
+            cherenkov_photons.t_pap*3e8],
+        max_number_itarations=500,
+        min_number_points_for_plane_fit=10,
+        max_orthogonal_distance_of_inlier=0.025,)
+    c_pap_time = np.array([B[0], B[1], B[2]])
+    if c_pap_time[2] > 0:
+        c_pap_time *= -1
+    c_pap_time = c_pap_time/np.linalg.norm(c_pap_time)
+    pap_time_offset = np.sqrt(
+        (c_pap_time[0] - core_cx)**2 +
+        (c_pap_time[1] - core_cy)**2)
+
+    print(
+        '{o:.2f}deg, {f:.2}deg, {p:.2f}deg, {e:.2f}GeV'.format(
+            o=np.rad2deg(trigger_offset),
+            f=np.rad2deg(flash_offset),
+            p=np.rad2deg(pap_time_offset),
+            e=energy))
     """
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.set_xlim(roi['cx_center_roi'] - c_radius, roi['cx_center_roi'] + c_radius)
-    ax.set_ylim(roi['cy_center_roi'] - c_radius, roi['cy_center_roi'] + c_radius)
-    ax.set_zlim(start_time, end_time)
+    #ax.set_xlim(roi['cx_center_roi'] - c_radius, roi['cx_center_roi'] + c_radius)
+    #ax.set_ylim(roi['cy_center_roi'] - c_radius, roi['cy_center_roi'] + c_radius)
+    #ax.set_zlim(start_time, end_time)
     ax.set_xlabel('x/rad')
     ax.set_ylabel('y/rad')
     ax.set_zlabel('t/s')
     ax.scatter(
-        ph_roi.cx_cy_in_object_distance(roi['object_distance'])[0],
-        ph_roi.cx_cy_in_object_distance(roi['object_distance'])[1],
-        ph_roi.t_img,
+        cherenkov_photons.cx_cy_in_object_distance(roi['object_distance'])[0],
+        cherenkov_photons.cx_cy_in_object_distance(roi['object_distance'])[1],
+        cherenkov_photons.t_img,
         lw=0,
         alpha=0.075,
         s=55.,
