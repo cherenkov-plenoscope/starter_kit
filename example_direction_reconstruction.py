5a5a40270b94c6a93bc5609db7a11d182eb49c4c
sebastian <sebmuell@phys.ethz.ch>
2018-07-19 11:17:35 +0200

    plot the incident-direction resolution into the common resolution plot

--- example_direction_reconstruction.py
+++ example_direction_reconstruction.py
@@ -2,6 +2,8 @@ import numpy as np
 import plenopy as pl
 import os
 import pandas as pd
+import json
+
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
@@ -18,6 +20,16 @@ run = pl.Run(
 
 number_events = 0
 results = []
+
+
+def integration_width_for_one_sigma(hist, bin_edges):
+    one_sigma = 0.68
+    integral = np.cumsum(hist/np.sum(hist))
+    bin_centers = (bin_edges[0:-1] + bin_edges[1:])/2
+    x = np.linspace(np.min(bin_centers), np.max(bin_centers), 100*bin_centers.shape[0])
+    f = np.interp(x=x, fp=integral, xp=bin_centers)
+    return x[np.argmin(np.abs(f - one_sigma))]
+
 
 cah_path = os.path.join(out_dir, 'incident_direction.msg')
 if not os.path.exists(cah_path):
@@ -98,43 +110,88 @@ for method in methods:
 e_bin_edges = np.logspace(np.log10(0.5), np.log10(5), 6)
 
 for method in methods:
-    median = []
-    error = []
-    for e in range(e_bin_edges.shape[0] - 1):
-        energy_mask = (
-            (rs.energy > e_bin_edges[e]) & (rs.energy <= e_bin_edges[e+1]))
-        med = np.median(rs[method + '_offset'][energy_mask])
-        median.append(med)
-        number_occurences = np.sum(energy_mask)
-        error.append(med * np.sqrt(number_occurences)/number_occurences)
-    median = np.array(median)
-    error = np.array(error)
-    fig = plt.figure(figsize=(6, 6), dpi=320)
-    ax = fig.add_axes((0.1, 0.1, 0.9, 0.9))
-    ax.errorbar(
-        x=e_bin_edges[:-1],
-        y=np.rad2deg(median),
-        yerr=(
-            np.rad2deg(error),
-            np.rad2deg(error)
-        ),
-        fmt='ko',)
-    ax.set_xlabel('energy/GeV')
-    ax.set_ylabel('residual incident-direction/deg')
-    ax.loglog()
-    plt.savefig(os.path.join(out_dir, 'incident_directions_'+method+'.png'))
-
     # 1 GeV regime
     energy_mask = ((rs.energy > 0.75) & (rs.energy <= 1.5))
     fig = plt.figure(figsize=(6, 6), dpi=320)
     ax = fig.add_axes((0.1, 0.1, 0.9, 0.9))
-    ax.hist(
+    h = ax.hist(
         np.rad2deg(rs[method + '_offset'][energy_mask]),
         bins=np.linspace(0, 2, 20),
         fc='gray',
         ec='none')
     ax.set_xlabel('residual incident-direction/deg')
     ax.set_ylabel('number events/1')
+
+    incident_direction_resolution_one_sigma = integration_width_for_one_sigma(
+        h[0], h[1])
+
+    ax.axvline(incident_direction_resolution_one_sigma, color='k')
     plt.savefig(
         os.path.join(out_dir, 'incident_directions_at_750MeV_to_1500MeV_'+method+'.png'))
+
+    # Resolution Figure
+    #------------------
+    resolution_path = os.path.join(
+        'run', 'isf', 'assumed_angular_resolution.json')
+    with open(resolution_path, 'rt') as fin:
+        out = json.loads(fin.read())
+
+    fig = plt.figure(figsize=(6, 6), dpi=320)
+    ax = fig.add_axes((0.1, 0.1, 0.9, 0.9))
+    ax.plot(
+        out['Fermi_LAT']['energy_GeV'],
+        out['Fermi_LAT']['resolution_deg'],
+        'k-.',
+        label='Fermi-LAT')
+    ax.plot(
+        out['Aharonian_et_al_5at5']['energy_GeV'],
+        out['Aharonian_et_al_5at5']['resolution_deg'],
+        linestyle='-',
+        color='k',
+        label='Aharonian et al., 5@5')
+    ax.plot(
+        out['Hofman_limits']['energy_GeV'],
+        out['Hofman_limits']['resolution_deg'],
+        'ko--',
+        markerfacecolor='k',
+        label='Hofmann, limits')
+    ax.plot(
+        out['Hofman_limits_with_earth_magnetic_field']['energy_GeV'],
+        out['Hofman_limits_with_earth_magnetic_field']['resolution_deg'],
+        'ko:',
+        markerfacecolor='white',
+        label='Hofmann, limits, with earth-magnetic-field')
+
+    one_sigma_resolutions = []
+    one_sigma_resolution_errors = []
+    energy_bin_edges = 0.75*(np.cumprod(1.4*np.ones(6))/1.4)
+    for energy_bin in range(len(energy_bin_edges) - 1):
+        energy_mask = (
+            (rs.energy > energy_bin_edges[energy_bin]) &
+            (rs.energy <= energy_bin_edges[energy_bin + 1]))
+        reiduals = np.rad2deg(rs['light_front_offset'][energy_mask])
+        direction_bin_edges = np.linspace(0, 2, 20)
+        hist = np.histogram(reiduals, bins=direction_bin_edges)[0]
+        one_sigma_resolution = integration_width_for_one_sigma(
+            hist, direction_bin_edges)
+        num = np.sum(energy_mask)
+        one_sigma_resolutions.append(one_sigma_resolution)
+        one_sigma_resolution_errors.append(
+            one_sigma_resolution * np.sqrt(num)/num)
+    energy_bin_centers = (energy_bin_edges[0:-1] + energy_bin_edges[1:])/2
+    ax.errorbar(
+        x=energy_bin_centers,
+        y=one_sigma_resolutions,
+        yerr=one_sigma_resolution_errors,
+        fmt='kx:',
+        label='Plenoscope, light-front')
+    ax.loglog()
+    ax.legend(loc='best', fontsize=10)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.set_xlabel('Energy / GeV')
+    ax.set_ylabel('Resolution / deg')
+    ax.grid(color='k', linestyle='-', linewidth=0.66, alpha=0.1)
+    fig.savefig(
+        os.path.join(out_dir, 'assumed_angular_resolution.png'))
 
