6e8ab6007dcd5896f5dc7309efa1aa073cd301a3
sebastian <sebmuell@phys.ethz.ch>
2018-08-26 15:04:55 +0200

    create only a small set of misalignments

--- sensor_misalignment_demo.py
+++ sensor_misalignment_demo.py
@@ -4,41 +4,59 @@ from os.path import join
 import subprocess as sp
 import shutil
 import plenopy as pl
-# import corsika_wrapper as cw
 import numpy as np
 import matplotlib.pyplot as plt
 import xml.etree.ElementTree as xmlElementTree
 import json
 import glob
 import wrapp_mct_photon_propagation as mctw
+import tempfile
 
 
 def triple2mct(x, y, z):
     return '[{x:f}, {y:f}, {z:f}]'.format(x=x, y=y, z=z)
 
 
+def read_light_field_sensor_specifications(path):
+    tree = xmlElementTree.parse(path)
+    scenery = tree.getroot()
+    frame = scenery.find('frame')
+    lfs = frame.find('light_field_sensor')
+    slfs = lfs.find('set_light_field_sensor')
+    return {
+        'expected_imaging_system_focal_length': float(
+            slfs.attrib['expected_imaging_system_focal_length']),
+        'expected_imaging_system_aperture_radius': float(
+            slfs.attrib['expected_imaging_system_aperture_radius']),
+        'max_FoV_diameter_deg': float(
+            slfs.attrib['max_FoV_diameter_deg']),
+        'hex_pixel_FoV_flat2flat_deg': float(
+            slfs.attrib['hex_pixel_FoV_flat2flat_deg']),
+        'housing_overhead': float(
+            slfs.attrib['housing_overhead']),
+        'number_of_paxel_on_pixel_diagonal': float(
+            slfs.attrib['number_of_paxel_on_pixel_diagonal']),
+    }
+
+
 def write_misaligned_plenoscope_scenery(
-    x_in_units_of_f,
-    y_in_units_of_f,
-    z_in_units_of_f,
+    pos_x,
+    pos_y,
+    pos_z,
     rot_x,
     rot_y,
     rot_z,
     path,
-    template_scenery_path=join(
-        'resources', 'acp', '71m', 'scenery', 'scenery.xml')
+    template_scenery_path
 ):
     tree = xmlElementTree.parse(template_scenery_path)
     scenery = tree.getroot()
     frame = scenery.find('frame')
     light_field_sensor = frame.find('light_field_sensor')
-    expected_imaging_system_focal_length = float(light_field_sensor.find(
-        'set_light_field_sensor').attrib[
-            'expected_imaging_system_focal_length'])
     light_field_sensor.find('set_frame').attrib['pos'] = triple2mct(
-        expected_imaging_system_focal_length * x_in_units_of_f,
-        expected_imaging_system_focal_length * y_in_units_of_f,
-        expected_imaging_system_focal_length * z_in_units_of_f)
+        pos_x,
+        pos_y,
+        pos_z)
     light_field_sensor.find('set_frame').attrib['rot'] = triple2mct(
         rot_x,
         rot_y,
@@ -46,438 +64,126 @@ def write_misaligned_plenoscope_scenery(
     tree.write(path)
 
 
-def read_misalignment_from_scenery(path):
-    tree = xmlElementTree.parse(path)
-    scenery = tree.getroot()
-    frame = scenery.find('frame')
-    light_field_sensor = frame.find('light_field_sensor')
-    pos_string = light_field_sensor.find('set_frame').attrib['pos']
-    rot_string = light_field_sensor.find('set_frame').attrib['rot']
-    pos = json.loads(pos_string)
-    rot = json.loads(rot_string)
-    return pos, rot
+def esitmate_light_field_geometry(
+    pos_x,
+    pos_y,
+    pos_z,
+    rot_x,
+    rot_y,
+    rot_z,
+    path,
+):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        write_misaligned_plenoscope_scenery(
+            pos_x=pos_x,
+            pos_y=pos_y,
+            pos_z=pos_z,
+            rot_x=rot_x,
+            rot_y=rot_y,
+            rot_z=rot_z,
+            path=join(tmp_dir, 'scenery.xml'),
+            template_scenery_path=template_scenery_path)
 
-
-d2r = np.deg2rad
+        sp.call([
+            join('.', 'build', 'mctracer', 'mctPlenoscopeCalibration'),
+            '-s', tmp_dir,
+            '-n', '{:d}'.format(number_mega_photons),
+            '-o', path])
 
 out_dir = join('examples', 'compensating_misalignments')
 os.makedirs(out_dir, exist_ok=True)
 
+template_scenery_path = join(
+    'resources',
+    'acp',
+    '71m',
+    'scenery',
+    'scenery.xml')
+
 # Estimating light-field-geometries for various misalignments
 # -----------------------------------------------------------
-number_mega_photons = 100
+number_mega_photons = 1
 
-light_field_geometries_dir = join(out_dir, 'light_field_geometries')
+# translation parallel
+light_field_sensor_specs = read_light_field_sensor_specifications(
+    template_scenery_path)
+focal_length = light_field_sensor_specs['expected_imaging_system_focal_length']
+pixel_fov = np.deg2rad(light_field_sensor_specs['hex_pixel_FoV_flat2flat_deg'])
+diameter = 2*light_field_sensor_specs[
+    'expected_imaging_system_aperture_radius']
 
-y_rotations = d2r(np.linspace(0.0, 5.0, 7))
-z_translations = np.linspace(0.97, 1.03, 7)
+target_object_distance = 10e3
+target_sensor_plane_distance = 1/(1/focal_length - 1/target_object_distance)
 
-i = 0
+projected_pixel = focal_length*np.tan(pixel_fov)
+
+g_minus = target_object_distance*(
+    1 - projected_pixel * target_object_distance/(2*focal_length*diameter))
+g_plus = target_object_distance*(
+    1 + projected_pixel * target_object_distance/(2*focal_length*diameter))
+
+d_minus = 1/(1/focal_length - 1/g_minus)
+d_plus = 1/(1/focal_length - 1/g_plus)
+
+delta_d_minus = d_minus - target_sensor_plane_distance
+delta_d_plus = d_plus - target_sensor_plane_distance
+
+para_tra_dir = join(out_dir, 'translation_parallel')
+os.makedirs(para_tra_dir, exist_ok=True)
+z_positions = np.linspace(
+    target_sensor_plane_distance + 10 * delta_d_plus,
+    target_sensor_plane_distance + 10 * delta_d_minus,
+    4)
+
+for z_pos in z_positions:
+    out_path = join(para_tra_dir, '{:.0f}mm'.format(z_pos*1e3))
+    if not os.path.exists(out_path):
+        esitmate_light_field_geometry(
+            pos_x=0,
+            pos_y=0,
+            pos_z=z_pos,
+            rot_x=0,
+            rot_y=0,
+            rot_z=0,
+            path=out_path)
+
+# rotation perpendicular
+# tollerance: 0.76deg -> 10 fold -> 7.6deg
+perp_rot_dir = join(out_dir, 'rotation_perpendicular')
+os.makedirs(perp_rot_dir, exist_ok=True)
+
+fov = np.deg2rad(light_field_sensor_specs['max_FoV_diameter_deg'])
+sensor_plane_radius = focal_length * np.tan(fov/2)
+
+delta_trans = np.min(np.abs([delta_d_minus, delta_d_plus]))
+
+delta_rot_perp = delta_trans/sensor_plane_radius
+y_rotations = np.linspace(
+    0,
+    10 * delta_rot_perp,
+    4)
+
 for y_rot in y_rotations:
-    for z_trans in z_translations:
-        scenery_dir = join(
-            light_field_geometries_dir,
-            '{:03d}_portal_plenoscope'.format(i))
-        os.makedirs(scenery_dir, exist_ok=True)
-
-        write_misaligned_plenoscope_scenery(
-            x_in_units_of_f=0,
-            y_in_units_of_f=0,
-            z_in_units_of_f=z_trans,
+    out_path = join(perp_rot_dir, '{:.0f}mdeg'.format(np.rad2deg(y_rot)*1e3))
+    if not os.path.exists(out_path):
+        esitmate_light_field_geometry(
+            pos_x=0,
+            pos_y=0,
+            pos_z=target_sensor_plane_distance,
             rot_x=0,
             rot_y=y_rot,
             rot_z=0,
-            path=join(scenery_dir, 'scenery.xml'))
-
-        light_field_geometry_path = join(
-            light_field_geometries_dir,
-            '{:03d}_light_field_geometry'.format(i))
-
-        if not os.path.exists(light_field_geometry_path):
-            sp.call([
-                join('.', 'build', 'mctracer', 'mctPlenoscopeCalibration'),
-                '-s', scenery_dir,
-                '-n', '{:d}'.format(number_mega_photons),
-                '-o', light_field_geometry_path ])
-
-        shutil.rmtree(scenery_dir)
-        i += 1
-
-# Analyse and visualize different misalignments
-# ---------------------------------------------
-
-lfgs = []
-poss = []
-rots = []
-for lfg_path in glob.glob(join(light_field_geometries_dir, '*')):
-    lfg = pl.LightFieldGeometry(lfg_path)
-    lfgs.append(lfg)
-    pos, rot = read_misalignment_from_scenery(
-        join(lfg_path, 'input', 'scenery', 'scenery.xml'))
-    poss.append(pos)
-    rots.append(rot)
-
-"""
-
-direction_std_bin_edges = np.deg2rad(np.linspace(0, 0.2, 100 + 1))
-direction_std_bin_centers = (
-    direction_std_bin_edges[0:-1] + direction_std_bin_edges[1:])/2
-support_std_bin_edges = np.linspace(0, 10, 100 + 1)
-support_std_bin_centers = (
-    support_std_bin_edges[0:-1] + support_std_bin_edges[1:])/2
-
-h = {}
-h['cx_std'] = []
-h['cy_std'] = []
-h['x_std'] = []
-h['y_std'] = []
-
-for l in range(len(lfgs)):
-    v = np.invert(np.isnan(lfgs[l].cx_std))
-    h['cx_std'].append(
-        np.histogram(
-            lfgs[l].cx_std[v],
-            bins=direction_std_bin_edges)[0])
-
-    v = np.invert(np.isnan(lfgs[l].cy_std))
-    h['cy_std'].append(
-        np.histogram(
-            lfgs[l].cy_std[v],
-            bins=direction_std_bin_edges)[0])
-
-    v = np.invert(np.isnan(lfgs[l].x_std))
-    h['x_std'].append(
-        np.histogram(
-            lfgs[l].x_std[v],
-            bins=support_std_bin_edges)[0])
-
-    v = np.invert(np.isnan(lfgs[l].y_std))
-    h['y_std'].append(
-        np.histogram(
-            lfgs[l].y_std[v],
-            bins=support_std_bin_edges)[0])
-
-"""
-
-# phantom source
-# --------------
-number_photons = 1e5
-
-# disk on ground to be illuminated
-disc_x = 0.0
-disc_y = 0.0
-disc_z = 0.0
-disc_radius = 40.0
-
-# triangle at object-distance 5km
-# -------------------------------
-obj_triangle = 5e3
-
-viewing_radius = np.deg2rad(3)
-triangle_radius = obj_triangle*np.tan(viewing_radius)
-tr = triangle_radius
-
-triangle_vertices = np.array([
-    [tr*np.cos(2*np.pi*0.25), tr*np.sin(2*np.pi*0.25), obj_triangle],
-    [tr*np.cos(2*np.pi*0.5833), tr*np.sin(2*np.pi*0.5833), obj_triangle],
-    [tr*np.cos(2*np.pi*0.9166), tr*np.sin(2*np.pi*0.9166), obj_triangle],])
-
-triangle_edges = np.array([
-    [0, 1],
-    [1, 2],
-    [2, 0],])
-
-ids, sups, dirs, wvls = mctw.vertex_wire_source_illuminating_xy_disc(
-    number_photons=number_photons,
-    vertices=triangle_vertices,
-    edges=triangle_edges,
-    disc_x=disc_x,
-    disc_y=disc_y,
-    disc_z=disc_z,
-    disc_radius=disc_radius)
-
-mctw.write_ascii_table_of_photons(
-    triangle_path, ids=ids, supports=sups, directions=dirs, wavelengths=wvls)
-
-# spiral at object-distance 10km
-# ------------------------------
-obj_spiral = 10e3
-N = 11
-spiral_azimuth = np.linspace(0, 3*np.pi, N, endpoint=False)
-spiral_viewing_radius = np.linspace(0, np.deg2rad(3.0), N)
-
-spiral_ids = []
-spiral_sups = []
-spiral_dirs = []
-spiral_wvls = []
-for n in range(N):
-    ids, sups, dirs, wvls = mctw.point_source_illuminating_xy_disc(
-        number_photons=int(number_photons/N),
-        x=np.cos(spiral_azimuth[n])*obj_spiral*np.tan(spiral_viewing_radius[n]),
-        y=np.sin(spiral_azimuth[n])*obj_spiral*np.tan(spiral_viewing_radius[n]),
-        z=obj_spiral,
-        disc_x=disc_x,
-        disc_y=disc_y,
-        disc_z=disc_z,
-        disc_radius=disc_radius)
-ids = np.vstack(spiral_ids)
-sups = np.vstack(spiral_sups)
-dirs = np.vstack(spiral_dirs)
-wcls = np.vstack(spiral_wvls)
-
-mctw.write_ascii_table_of_photons(
-    spiral_path, ids=ids, supports=sups, directions=dirs, wavelengths=wvls)
-
-# sunny circle at object-distance 15km
-# ------------------------------------
-obj_sun = 15e3
-
-
-# Propagate photons to show Point-spread-function
-# -----------------------------------------------
-psf_dir = join(out_dir, 'psf')
-os.makedirs(psf_dir, exist_ok=True)
-
-N = 11
-c_azimuth = np.linspace(0, 3*np.pi, N, endpoint=False)
-c_radius = np.linspace(0, np.deg2rad(3.0), N)
-
-cxs = c_radius*np.cos(c_azimuth)
-cys = c_radius*np.sin(c_azimuth)
-
-config_path = join(
-    'resources',
-    'acp',
-    'mct_propagation_config_no_night_sky_background.xml')
-
-mct_propagate_raw_photons_path = join(
-    '.', 'build', 'mctracer', 'mctPlenoscopeRawPhotonPropagation')
-
-
-raw_responses = []
-for l in range(len(lfgs)):
-    if l > 0:
-        break
-    light_field_geometry_path = join(
-        light_field_geometries_dir,
-        '{:03d}_light_field_geometry'.format(l))
-    lfg = pl.LightFieldGeometry(light_field_geometry_path)
-
-    sum_light_field = np.zeros(lfg.number_lixel, dtype=np.uint32)
-    for p in range(N):
-        output_path = join(
-            psf_dir,
-            '{l:03d}_{p:03d}psf'.format(l=l, p=p))
-
-        if not os.path.exists(output_path):
-            mctw.point_source_in_plenoscope(
-                cx=cxs[p],
-                cy=cys[p],
-                object_distance=10e3,
-                illumination_radius_on_ground=50,
-                number_of_photons=1e5,
-                light_field_geometry_path=light_field_geometry_path,
-                output_path=output_path,
-                mct_propagate_raw_photons_path=mct_propagate_raw_photons_path,
-                config_path=config_path,
-                random_seed=0)
-
-        tmp_run = pl.Run(output_path)
-        tmp_event = tmp_run[0]
-        tmp_light_field = tmp_event.light_field_sequence_raw().sum(axis=0)
-        sum_light_field += tmp_light_field
-    raw_responses.append({
-        'light_field': sum_light_field,
-        'light_field_geometry': lfg})
-
-
-lf = raw_responses[0]['light_field']
-lfg = raw_responses[0]['light_field_geometry']
-
-imrays = pl.image.ImageRays(lfg)
-cx, cy = imrays.cx_cy_in_object_distance(10e3)
-valid = np.invert(np.isnan(cx)) & np.invert(np.isnan(cy))
-pixel_bin_edges = np.linspace(-3.25, 3.35, int(np.round(6.5/0.0626)))
-
-plt.figure()
-plt.hist2d(
-    np.rad2deg(cx[valid]),
-    np.rad2deg(cy[valid]),
-    weights=lf[valid],
-    bins=[pixel_bin_edges, pixel_bin_edges],
-    cmap='inferno')
-plt.xlim([-4,4])
-plt.ylim([-4,4])
-
-lx = lfg.lixel_positions_x
-ly = lfg.lixel_positions_y
-px = lx.reshape((lfg.number_pixel, lfg.number_paxel)).mean(axis=1)
-py = ly.reshape((lfg.number_pixel, lfg.number_paxel)).mean(axis=1)
-focal_length = lfg.sensor_plane2imaging_system.expected_imaging_system_focal_length
-cpx = - np.arctan(px/focal_length)
-cpy = - np.arctan(py/focal_length)
-image = lf.reshape((lfg.number_pixel, lfg.number_paxel)).sum(axis=1)
-
-plt.figure()
-plt.hist2d(
-    np.rad2deg(cpx), np.rad2deg(cpy),
-    weights=image,
-    bins=[pixel_bin_edges, pixel_bin_edges],
-    cmap='inferno')
-plt.xlim([-4,4])
-plt.ylim([-4,4])
-plt.show()
-
-"""
-N = 4
-styles = ['k-', 'k--', 'k:', 'k-.']
-fig = plt.figure(figsize=(6, 6), dpi=320)
-ax = fig.add_axes((0.1, 0.1, 0.89, 0.89))
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.set_xlabel(r'$\sigma_{c_x}$ / deg')
-ax.set_ylabel(r'Intensity / 1')
-for i, l in enumerate([0, 2, 4, 6]):
-    ax.plot(
-        np.rad2deg(direction_std_bin_centers),
-        h['cx_std'][l],
-        styles[i],
-        alpha=(i + 1)/N,
-        label='{:0.1f}, {:0.1f}'.format(
-            lfgs[l].sensor_plane2imaging_system.sensor_plane2imaging_system[2,3],
-            lfgs[l].sensor_plane2imaging_system.sensor_plane2imaging_system[2,2]))
-    ax.grid(
-        color='k',
-        linestyle='-',
-        linewidth=0.66,
-        alpha=0.1)
-ax.semilogy()
-ax.legend(loc='best', fontsize=10)
-fig.savefig(join(out_dir, 'fig.jpg'))
-"""
-
-
-
-
-"""
-# trans z
-for l in range(len(lfgs)):
-    homtra = lfgs[l].sensor_plane2imaging_system.sensor_plane2imaging_system
-    if np.abs(homtra[0:3, 0:3] - np.eye(3)) < 0.1:
-"""
-
-
-
-"""
-out_dir = join('examples', 'sensor_misalignment_demo')
-os.makedirs(out_dir, exist_ok=True)
-
-steering_card = cw.read_steering_card(
-    join('resources', 'acp', '71m',
-        'high_energy_example_helium_corsika_steering_card.txt'))
-
-if not os.path.exists(join(out_dir, 'He.evtio')):
-    cw.corsika(
-        steering_card=steering_card,
-        output_path=join(out_dir, 'He.evtio'),
-        save_stdout=True)
-
-if not os.path.exists(join(out_dir, 'target_He.acp')):
-    call([
-        join('build', 'mctracer', 'mctPlenoscopePropagation'),
-        '--lixel', join('resources', 'acp', '71m', 'light_field_calibration'),
-        '--input', join(out_dir, 'He.evtio'),
-        '--config', join('resources', 'acp', 'mct_propagation_config.xml'),
-        '--output', join(out_dir, 'target_He.acp'),
-        '--random_seed', '0',
-        '--all_truth'])
-
-if not os.path.exists(join(out_dir, 'misaligned_He.acp')):
-    call([
-        join('build', 'mctracer', 'mctPlenoscopePropagation'),
-        '--lixel', join('resources', 'acp', '71m',
-                        'light_field_calibration_off_target_example'),
-        '--input', join(out_dir, 'He.evtio'),
-        '--config', join('resources', 'acp', 'mct_propagation_config.xml'),
-        '--output', join(out_dir, 'misaligned_He.acp'),
-        '--random_seed', '0',
-        '--all_truth'])
-
-# 1,4,6,18,21,22,23*,25
-runpaths = [
-    join(out_dir, 'target_He.acp'),
-    join(out_dir, 'misaligned_He.acp')]
-
-lixel_efficiencies = [
-    pl.Run(run_path).light_field_geometry.efficiency for run_path in runpaths]
-min_efficiency = np.minimum(lixel_efficiencies[0], lixel_efficiencies[1])
-rel_min_efficiency = min_efficiency/min_efficiency.max()
-valid = rel_min_efficiency > 0.5
-
-target_light_field_geometry = pl.Run(runpaths[0]).light_field_geometry
-target_cx = target_light_field_geometry.pixel_pos_cx
-target_cy = target_light_field_geometry.pixel_pos_cy
-
-refocussed_images = []
-
-for run_path in runpaths:
-    run = pl.Run(run_path)
-    event = run[22]
-
-    # event.show()
-
-    image_rays = pl.image.ImageRays(run.light_field_geometry)
-    image_rays.pixel_pos_tree = run.light_field_geometry.pixel_pos_tree
-    object_distance = 2.5e3
-    lixels2pixel = image_rays.pixel_ids_of_lixels_in_object_distance(
-        object_distance)
-
-    image_sequence = np.zeros(
-        shape=(
-            event.light_field.number_time_slices,
-            event.light_field.number_pixel),
-        dtype=np.uint16)
-
-    for lix, lixel2pixel in enumerate(lixels2pixel):
-        if valid[lix]:
-            image_sequence[:, lixel2pixel] += (
-                event.light_field.sequence[:, lix])
-
-    t_m = pl.light_field.sequence.time_slice_with_max_intensity(image_sequence)
-    ts = np.max([t_m-1, 0])
-    te = np.min([t_m+1, image_sequence.shape[0]-1])
-
-    intense = np.sum(image_sequence[ts:te, :], axis=0)
-
-    refocussed_image = pl.Image(
-        intense,
-        run.light_field_geometry.pixel_pos_cx,
-        run.light_field_geometry.pixel_pos_cy,)
-    refocussed_images.append(refocussed_image)
-
-    raw_image = pl.Image(
-        (event.light_field.pixel_sequence())[ts:te].sum(axis=0),
-        event.light_field.pixel_pos_cx,
-        event.light_field.pixel_pos_cy)
-
-    fig, ax = plt.subplots()
-    plt.title('raw '+run_path)
-    pl.image.plot.add_pixel_image_to_ax(raw_image, ax)
-
-    fig, ax = plt.subplots()
-    plt.title('post '+run_path)
-    pl.image.plot.add_pixel_image_to_ax(refocussed_image, ax)
-    plt.savefig(run_path+'.jpg')
-
-diff = pl.Image(
-    refocussed_images[1].intensity.astype('float') -
-    refocussed_images[0].intensity.astype('float'),
-    target_cx,
-    target_cy)
-
-fig, ax = plt.subplots()
-plt.title('Difference')
-pl.image.plot.add_pixel_image_to_ax(diff, ax)
-
-plt.show()
-"""
+            path=out_path)
+
+
+# All bad misalignment
+out_path = join(out_dir, 'composition_all_bad')
+if not os.path.exists(out_path):
+    esitmate_light_field_geometry(
+        pos_x=0,
+        pos_y=0,
+        pos_z=target_sensor_plane_distance,
+        rot_x=0,
+        rot_y=0,
+        rot_z=0,
+        path=out_path)
