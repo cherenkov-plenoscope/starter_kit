3d63ad092502ee77201d73fa9be4359bb1de8c03
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2019-06-03 19:25:34 +0200

    adopt sun grid engine

--- estimate_sensitivity.py
+++ estimate_sensitivity.py
@@ -16,13 +16,16 @@ import docopt
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import scoop
+import sun_grid_engine_map as sge
 import os
 from os.path import join
+from os.path import abspath
+import shutil
 from subprocess import call
 import acp_instrument_response_function as irf
 import acp_instrument_sensitivity_function as isf
 import random
+import plenopy as pl
 
 
 if __name__ == '__main__':
@@ -33,17 +36,29 @@ if __name__ == '__main__':
         integration_time_in_slices = 10
 
         arguments = docopt.docopt(__doc__)
-        od = arguments['--out_dir']
+        od = os.path.abspath(arguments['--out_dir'])
 
         # 1) Light-field-calibration of Plenoscope
         # ----------------------------------------
         os.makedirs(od, exist_ok=True)
         if not os.path.isdir(join(od, 'light_field_calibration')):
+            lfc_tmp_dir = join(od, 'light_field_calibration.tmp')
+            os.makedirs(lfc_tmp_dir)
+            lfc_jobs = pl.light_field_geometry.map_reduce.make_jobs(
+                merlict_map_path=abspath(
+                    join('build', 'merlict', 'merlict-plenoscope-calibration-map')),
+                scenery_path=os.path.abspath(join('resources', 'acp', '71m', 'scenery')),
+                out_dir=lfc_tmp_dir,
+                num_photons_per_block=1000*1000,
+                num_blocks=int(arguments['--lfc_Mp']),
+                random_seed=0)
+
+            rc = sge.map(pl.light_field_geometry.map_reduce.worker_node, lfc_jobs)
             call([
-                join('build', 'merlict', 'merlict-plenoscope-calibration'),
-                '--scenery', join('resources', 'acp', '71m', 'scenery'),
-                '--number_mega_photons', arguments['--lfc_Mp'],
+                abspath(join('build', 'merlict', 'merlict-plenoscope-calibration-reduce')),
+                '--input', lfc_tmp_dir,
                 '--output', join(od, 'light_field_calibration')])
+            shutil.rmtree(lfc_tmp_dir)
 
         # 2) Instrument-response-functions for typical cosmic particles
         # -------------------------------------------------------------
@@ -53,20 +68,20 @@ if __name__ == '__main__':
         for p in particles:
             if not os.path.isdir(join(od, 'irf', p)):
                 jobs += irf.trigger_simulation.make_output_directory_and_jobs(
-                    steering_card_path=join(
-                        'resources', 'acp', '71m', p+'_steering.json'),
+                    steering_card_path=abspath(join(
+                        'resources', 'acp', '71m', p+'_steering.json')),
                     output_path=join(od, 'irf', p),
                     acp_detector_path=join(od, 'light_field_calibration'),
-                    mct_acp_config_path=join(
-                        'resources', 'acp', 'merlict_propagation_config.json'),
-                    mct_acp_propagator_path=join(
-                        'build', 'merlict', 'merlict-plenoscope-propagation'),
+                    mct_acp_config_path=abspath(join(
+                        'resources', 'acp', 'merlict_propagation_config.json')),
+                    mct_acp_propagator_path=abspath(join(
+                        'build', 'merlict', 'merlict-plenoscope-propagation')),
                     trigger_patch_threshold=patch_threshold,
                     trigger_integration_time_in_slices=(
                         integration_time_in_slices))
 
         random.shuffle(jobs)
-        rc = list(scoop.futures.map(irf.trigger_simulation.run_job, jobs))
+        rc = sge.map(irf.trigger_simulation.run_job, jobs)
 
         for p in particles:
             if (
