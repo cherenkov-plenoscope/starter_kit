2d7d54572d2b02ac7f993ca1f8254f09c0a91733
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2019-06-06 18:41:07 +0200

    protons E_start 5GeV, all particles up to 25GeV GAMMA-index is -1.

--- estimate_magnetic_deflection.py
+++ estimate_magnetic_deflection.py
@@ -4,7 +4,11 @@ import magnetic_deflection as md
 import sun_grid_engine_map as sge
 import acp_instrument_response_function as irf
 
-particle_types = ['gamma', 'electron', 'proton']
+particles = [
+    {'type': 'gamma', 'E_start': 0.25, 'E_stop': 25, 'num_runs': 512},
+    {'type': 'electron', 'E_start': 0.25, 'E_stop': 25, 'num_runs': 512},
+    {'type': 'proton', 'E_start': 5., 'E_stop': 25, 'num_runs': 128},
+]
 
 location_steerong_card = irf.utils.read_json(
     'resources/acp/71m/chile_paranal.json')
@@ -13,9 +17,9 @@ reduce_dir = os.path.abspath('run/magnetic_deflection/reduce')
 merlict_path=os.path.abspath('build/merlict/merlict-magnetic-field-explorer')
 
 jobs = []
-for particle_type in particle_types:
+for particle in particles:
     jobs += md.map_and_reduce.make_jobs(
-        particle_type=particle_type,
+        particle_type=particle['type'],
         max_zenith_scatter_angle_deg=15.,
         observation_level_altitude_asl=
             location_steerong_card['observation_level_altitude_asl'],
@@ -24,18 +28,18 @@ for particle_type in particle_types:
         earth_magnetic_field_z_muT=
             location_steerong_card['earth_magnetic_field_z_muT'],
         atmosphere_model=location_steerong_card['atmosphere_model'],
-        E_start=0.25,
-        E_stop=10.,
-        out_dir=os.path.join(reduce_dir, particle_type),
+        E_start=particle['E_start'],
+        E_stop=particle['E_stop'],
+        out_dir=os.path.join(reduce_dir, particle['type']),
         merlict_path=merlict_path,
-        num_runs=1000,
+        num_runs=particle['num_runs'],
         num_events_in_run=1280)
 
 random.shuffle(jobs)
 rc = sge.map(md.map_and_reduce.run_job, jobs)
 
-for particle_type in particle_types:
+for particle in particles:
     md.map_and_reduce.reduce_output(
-        in_dir=os.path.join(reduce_dir, particle_type),
-        out_path=os.path.join(reduce_dir, particle_type+".float32"))
+        in_dir=os.path.join(reduce_dir, particle['type']),
+        out_path=os.path.join(reduce_dir, particle['type']+".float32"))
 
