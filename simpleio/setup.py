0f1fe9710b67ff3d6770395f038c4145c5a28b00
Sebastian Achim Mueller <sebmuell@phys.ethz.ch>
2019-06-15 09:54:57 +0200

    have simpleio pip installable

---
+++ simpleio/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='simpleio',
+    version='0.0.0',
+    description='read merlict simpleio format.',
+    url='https://github.com/cherenkov-plenoscope',
+    author='Sebastian Achim Mueller',
+    author_email='sebmuell@phys.ethz.ch',
+    license='GPL v3',
+    packages=[
+        'simpleio',
+    ],
+    zip_safe=False,
+)
