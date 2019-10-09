914d7735fa888b536045ad0ddc83a236564ada59
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2019-10-09 14:27:14 +0200

    Add README.md

--- plenoscope_map_reduce/setup.py
+++ plenoscope_map_reduce/setup.py
@@ -1,9 +1,14 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name='plenoscope_map_reduce',
     version='0.0.0',
     description='Collection of map and reduce tools for parralel computing.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/cherenkov-plenoscope',
     author='Sebastian Achim Mueller',
     author_email='sebastian-achim.mueller@mpi-hd.mpg.de',
