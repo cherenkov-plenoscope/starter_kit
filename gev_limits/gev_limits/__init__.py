915bf716e038ec58664f0a480023e83ca4244eae
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-01-20 13:15:27 +0100

    not longer needed. Novel CORSIKA primary mod

--- gev_limits/gev_limits/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import lookup
-from . import light_field
-from . import process_run
-from . import job_structure
-from . import thrown_structure
-from . import map_and_reduce
-from . import features
