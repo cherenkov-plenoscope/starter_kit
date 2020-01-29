1d34eaa7cf9a87ea2c46d709bf95e5293a920c51
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-01-29 11:58:40 +0100

    rm obsolete functions, add plotting

--- plenoscope_map_reduce/plenoscope_map_reduce/instrument_response_reduce.py
+++ plenoscope_map_reduce/plenoscope_map_reduce/instrument_response_reduce.py
@@ -9,25 +9,10 @@ import glob
 import plenoscope_map_reduce as plmr
 from plenoscope_map_reduce import instrument_response as irf
 import tarfile
-
-
-
-def resizs_hist(hist, shape):
-    xf = hist.shape[0] // shape[0]
-    assert hist.shape[0] % shape[0] == 0
-    yf = hist.shape[1] // shape[1]
-    assert hist.shape[1] % shape[1] == 0
-    out = np.zeros(shape, dtype=hist.dtype)
-    for x in range(shape[0]):
-        for y in range(shape[1]):
-            x_start = x*xf
-            x_stop = (x+1)*xf - 1
-            y_start = y*yf
-            y_stop = (y+1)*yf - 1
-            out[x, y] = np.sum(hist[x_start:x_stop, y_start:y_stop])
-    return out
-
-
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import multiprocessing
 
 
 EXAMPLE_IRF_DIR = op.join("..", "run-2020-01-27_1011")
@@ -37,17 +22,28 @@ irf_dir = EXAMPLE_IRF_DIR
 with open(op.join(irf_dir, "config.json"), "rt") as f:
     cfg = json.loads(f.read())
 
+
+_scenery_path = op.join(
+    "resources",
+    "acp",
+    "71m",
+    "scenery",
+    "scenery.json")
+_light_field_sensor_geometry = irf.merlict.read_plenoscope_geometry(
+    merlict_scenery_path=_scenery_path)
+plenoscope_diameter = 2.0*_light_field_sensor_geometry[
+        "expected_imaging_system_aperture_radius"]
+plenoscope_grid_geometry = irf.grid.init(
+    plenoscope_diameter=plenoscope_diameter,
+    num_bins_radius=cfg["grid"]["num_bins_radius"])
+
+
 particle_keys = list(cfg["particles"].keys())
 particle_keys = ["electron"]
 site_keys = list(cfg["sites"].keys())
 
 # collect features
 site_key = "namibia"
-
-
-query_str = '(run_id == {:d}) and (airshower_id == {:d})'
-
-grid_shape = (128, 128)
 
 features = {}
 grids = {}
@@ -71,72 +67,178 @@ for particle_key in particle_keys:
             pp,
             config=irf.table.CONFIG)
 
-    # reduce grid_histogram
-    num_bins_grid_edge = 1024
-    num_energy_bins = 10
-    energy_start = 0.25
-    energy_stop = 1000
-    energy_bin_edges = np.geomspace(
-        energy_start,
-        energy_stop,
-        num_energy_bins + 1)
-
-    num_cxy_bins = 10
-    cx_start_stop = np.deg2rad(30)
-    cxy_bin_edges = np.linspace(
-        -cx_start_stop,
-        cx_start_stop,
-        num_cxy_bins + 1)
-
     grids[particle_key] = irf.grid.reduce_histograms(feature_dir=feature_dir)
 
-energy = 1
-energy_abs_delta = 1
-cx_deg = 0.0
-cy_deg = 0.0
-c_delta_deg = 1.5
 
-prm = features['electron']['primary']
+def add_circle(ax, x, y, r, linestyle='k-', num_points=1000):
+    xs = np.zeros(num_points)
+    ys = np.zeros(num_points)
+    phis = np.linspace(0, 2*np.pi, num_points)
+    xs = np.cos(phis)*r + x
+    ys = np.sin(phis)*r + y
+    ax.plot(xs, ys, linestyle)
 
 
-cxs = np.cos(prm['azimuth_rad'])*prm['zenith_rad']
-cys = np.sin(prm['azimuth_rad'])*prm['zenith_rad']
+def rm_splines(ax):
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
 
-directions = irf.grid._make_bunch_direction(cxs, cys)
-target_direction = irf.grid._make_bunch_direction(
-    np.array([np.deg2rad(cx_deg)]),
-    np.array([np.deg2rad(cy_deg)]))
 
-deltas = irf.grid._make_angle_between(
-    directions=directions,
-    direction=target_direction.T)[:, 0]
-mask_cxcy = deltas <= np.deg2rad(c_delta_deg)
-mask_energy = np.logical_and(
-    prm['energy_GeV'] > (energy - energy_abs_delta),
-    prm['energy_GeV'] <= (energy + energy_abs_delta))
-mask_match = np.logical_and(mask_cxcy, mask_energy)
+def add_slider_axes(ax, start, stop, values, label, log=False):
+    ax.set_ylim([start, stop])
+    if log:
+        ax.semilogy()
+    rm_splines(ax=ax)
+    ax.set_xticklabels([])
+    ax.set_xticks([])
+    ax.spines['bottom'].set_visible(False)
+    ax.set_xlabel(label)
+    for value in values:
+        ax.plot(
+            [0, 1],
+            [value, value],
+            "k",
+            linewidth=5)
 
-match = prm[mask_match]
+def save_view(
+    path = '',
+    energy_GeV_start = 0.5,
+    energy_GeV_stop = 5,
+    cx_deg = 0.,
+    cy_deg = -5.0,
+    cone_opening_angle_deg = 1.5,
+    primary_table = features[particle_key]['primary'],
+    grid_histograms = grids[particle_key],
+    plenoscope_grid_geometry = plenoscope_grid_geometry,
+    ff=1.5
+):
+    histogram, num_airshower = irf.query.query_grid_histograms(
+        energy_GeV_start=energy_GeV_start,
+        energy_GeV_stop=energy_GeV_stop,
+        cx_deg=cx_deg,
+        cy_deg=cy_deg,
+        cone_opening_angle_deg=cone_opening_angle_deg,
+        primary_table=primary_table,
+        grid_histograms=grid_histograms,
+        num_bins_radius=plenoscope_grid_geometry['num_bins_radius'])
+    grid_bin_edges = plenoscope_grid_geometry['xy_bin_edges']
+    fig = plt.figure(figsize=(16/ff, 9/ff), dpi=120*ff)
+    ax_hist = fig.add_axes((0.075, 0.05, 0.45, 0.9))
+    rm_splines(ax_hist)
+    hh = histogram
+    hh_log = hh.copy()/num_airshower
+    hh_log[hh>1] = np.log10(hh[hh>1])
+    ax_hist.pcolormesh(
+        grid_bin_edges*1e-3,
+        grid_bin_edges*1e-3,
+        hh_log,
+        cmap='Greys')
+    ax_hist.set_aspect('equal')
+    ax_hist.set_xlabel('observation-level x/km')
+    ax_hist.set_ylabel('observation-level y/km')
+    ax_hist.grid(color='k', linestyle='-', linewidth=0.66, alpha=0.1)
+    ax_incident = fig.add_axes((0.675, 0.5, 0.4, 0.4))
+    rm_splines(ax_incident)
+    add_circle(ax_incident, 0, 0, 10)
+    add_circle(ax_incident, 0, 0, 20)
+    add_circle(ax_incident, 0, 0, 30)
+    ax_incident.grid(color='k', linestyle='-', linewidth=0.66, alpha=0.1)
+    add_circle(ax_incident, cx_deg, cy_deg, cone_opening_angle_deg, 'r-')
+    ax_incident.set_aspect('equal')
+    ax_incident.set_xlabel('incident-x/deg')
+    ax_incident.set_ylabel('incident-y/deg')
+    ax_size = fig.add_axes((0.575, 0.15, 0.03, 0.7))
+    add_slider_axes(
+        ax=ax_size,
+        start=1e0,
+        stop=1e6,
+        values=[np.sum(histogram)/num_airshower],
+        label='size/1',
+        log=True)
+    ax_size = fig.add_axes((0.675, 0.15, 0.03, 0.7))
+    add_slider_axes(
+        ax=ax_size,
+        start=1e-1,
+        stop=1e4,
+        values=[energy_GeV_start, energy_GeV_stop],
+        label='energy/GeV',
+        log=True)
+    ax_text = fig.add_axes((0.725, 0.05, 0.3, 0.3))
+    ax_text.set_axis_off()
+    ax_text.set_xlim([0, 10])
+    ax_text.set_ylim([0, 10])
+    ax_text.text(1, 10, 'num. airshower {: 6d}'.format(num_airshower))
+    ax_text.text(1, 9, 'incident-x/deg {:1.1f}'.format(cx_deg))
+    ax_text.text(1, 8, 'incident-y/deg {:1.1f}'.format(cy_deg))
+    ax_text.text(1, 7, 'opening angle/deg {:1.1f}'.format(
+        cone_opening_angle_deg))
+    fig.savefig(path)
+    plt.close(fig)
 
-cx_match = cxs[mask_match]
-cy_match = cys[mask_match]
 
-hist = np.zeros((1024, 1024))
-num_airshower = 0
-for asho in match:
-    seed = irf.table.random_seed_based_on(
-        run_id=asho['run_id'],
-        airshower_id=asho['airshower_id'])
-    print(seed)
-    hist += irf.grid.bytes_to_histogram(grids[particle_key][seed])
-    num_airshower += 1
+def _move_linear(view_stations, num_steps_per_station=15):
+    num_steps = num_steps_per_station
+    views = []
+    station = 0
+    while station < (len(view_stations) - 1):
+        start = view_stations[station]
+        stop = view_stations[station + 1]
+        block_views = np.array([
+                np.linspace(start['el'], stop['el'], num_steps),
+                np.linspace(start['eu'], stop['eu'], num_steps),
+                np.linspace(start['cx'], stop['cx'], num_steps),
+                np.linspace(start['cy'], stop['cy'], num_steps),
+                np.linspace(start['op'], stop['op'], num_steps),
+            ])
+        views.append(block_views.T)
+        station += 1
+    return np.concatenate(views)
 
-hh, nn = irf.query.query_grid_histograms(
-    energy_GeV_start=100,
-    energy_GeV_stop=1000,
-    cx_deg=0.,
-    cy_deg=0.,
-    cone_opening_angle_deg=5,
-    primary_table=features[particle_key]['primary'],
-    grid_histograms=grids[particle_key],
-    num_bins_radius=512)
+
+view_stations = [
+    {"el": 5, "eu": 50, 'cx': 0.0, 'cy': 0.0, 'op': 8.0},
+    {"el": 0.5, "eu": 5, 'cx': 15.0, 'cy': 0.0, 'op': 2.0},
+    {"el": 0.5, "eu": 5, 'cx': 0.0, 'cy': 15.0, 'op': 2.0},
+    {"el": 0.5, "eu": 5, 'cx': -15.0, 'cy': 0.0, 'op': 2.0},
+    {"el": 0.5, "eu": 5, 'cx': 0.0, 'cy': -15.0, 'op': 2.0},
+    {"el": 0.5, "eu": 5, 'cx': 15.0, 'cy': 0.0, 'op': 2.0},
+    {"el": 5, "eu": 50, 'cx': 0.0, 'cy': 0.0, 'op': 8.0},
+]
+
+views = _move_linear(view_stations, num_steps_per_station=30)
+
+jobs = []
+for idx, v in enumerate(views):
+    job = {}
+    job['out_dir'] = os.path.curdir
+    job['idx'] = idx
+    job['el'] = v[0]
+    job['eu'] = v[1]
+    job['cx'] = v[2]
+    job['cy'] = v[3]
+    job['op'] = v[4]
+    job['primary'] = features[particle_key]['primary']
+    job['grid'] = grids[particle_key]
+    job['grid_geometry'] = plenoscope_grid_geometry
+    jobs.append(job)
+
+
+def run_job(job):
+    out_path = os.path.join(
+        job['out_dir'],
+        "{:06d}.{:s}".format(job['idx'], 'jpg'))
+    save_view(
+        path=out_path,
+        energy_GeV_start = job['el'],
+        energy_GeV_stop = job['eu'],
+        cx_deg = job['cx'],
+        cy_deg = job['cy'],
+        cone_opening_angle_deg = job['op'],
+        primary_table = job['primary'],
+        grid_histograms = job['grid'],
+        plenoscope_grid_geometry = job['grid_geometry'])
+    return 0
+
+
+pool = multiprocessing.Pool(8)
+pool.map(run_job, jobs)
