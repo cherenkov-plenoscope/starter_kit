4b79dcca25cbcf7cc44cd89c6a43e90977db2158
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-01-30 15:24:02 +0100

    collect runtime statistics from logs

--- plenoscope_map_reduce/plenoscope_map_reduce/instrument_response/logging.py
+++ plenoscope_map_reduce/plenoscope_map_reduce/instrument_response/logging.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import os
 
 
 class JsonlLog:
@@ -17,3 +18,31 @@ class JsonlLog:
                 "msg": msg}
             f.write(json.dumps(d)+"\n")
         self.last_log_time = now
+
+
+def reduce(
+    list_of_log_paths,
+    keys=[
+        'run CORSIKA',
+        'run merlict',
+        'reuse, grid',
+        'run sum-trigger',
+        'prepare refocus-sum-trigger',
+        'Cherenkov classification',
+        'extract features from light-field',
+        'export, level 1, and level 2',
+    ]
+):
+    logs = []
+    for log_path in list_of_log_paths:
+        run_id = int(os.path.basename(log_path)[0:6])
+        run = {'run_id': run_id}
+        for key in keys:
+            run[key] = None
+        with open(log_path, 'rt') as fin:
+            for line in fin:
+                logline = json.loads(line)
+                if logline['msg'] in keys:
+                    run[logline['msg']] = logline['delta_t']
+            logs.append(run)
+    return logs
