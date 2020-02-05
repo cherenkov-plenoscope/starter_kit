c3b314b1a573ddf7a276a9af0d411bb78264c4ee
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-02-05 12:46:05 +0100

    this is moved into plmr.irf

--- plenoscope_map_reduce/plenoscope_map_reduce/instrument_response/__init__.py
+++ plenoscope_map_reduce/plenoscope_map_reduce/instrument_response/__init__.py
@@ -1,17 +1,7 @@
-#! /usr/bin/env python
-"""
-Estimate the instrument-response for the Cherenkov-plenoscope.
-
-Usage: trigger_sensitivity [--out_dir=DIR]
-
-Options:
-    -h --help               Prints this help message.
-    -o --out_dir=DIR        Output directory [default: ./run]
-"""
-import docopt
 import os
 import numpy as np
 from os import path as op
+from os.path import join as opj
 import shutil
 import subprocess
 import random
@@ -23,34 +13,20 @@ import sun_grid_engine_map as sge
 import plenoscope_map_reduce as plmr
 from plenoscope_map_reduce import instrument_response as irf
 
-
-MODE = "test"
-
-if MODE == "test":
-    MULTIPROCESSING_POOL = "local"
-    TMP_DIR_ON_WORKERNODE = False
-    KEEP_TMP = True
-    MAX_ENERGY_GEV = 1e2
-    NUM_AIRSHOWERS_PER_RUN = 100
-    NUM_GAMMA_RUNS = 2
-    LAZY_REDUCTION = True
-elif MODE == "production":
-    MULTIPROCESSING_POOL = "sun_grid_engine"
-    TMP_DIR_ON_WORKERNODE = True
-    KEEP_TMP = False
-    MAX_ENERGY_GEV = 1e3
-    NUM_AIRSHOWERS_PER_RUN = 1000
-    NUM_GAMMA_RUNS = 300
-    LAZY_REDUCTION = False
-else:
-    raise KeyError("unknown mode {:s}".format(MODE))
+from . import random
+from . import table
+from . import grid
+from . import merlict
+from . import logging
+from . import query
+from . import map_and_reduce
 
 
 def absjoin(*args):
-    return op.abspath(op.join(*args))
+    return op.abspath(opj(*args))
 
 
-executables = {
+EXAMPLE_EXECUTABLES = {
     "corsika_primary_abspath": absjoin(
         "build",
         "corsika",
@@ -75,7 +51,7 @@ executables = {
         "merlict-plenoscope-calibration-reduce"),
 }
 
-cfg_files = {
+EXAMPLE_CONFIG_FILES = {
     "merlict_plenoscope_propagator_config_abspath": absjoin(
         "resources",
         "acp",
@@ -88,15 +64,7 @@ cfg_files = {
         "scenery"),
 }
 
-cfg = {
-    "plenoscope_scenery_relpath": op.join(
-        "input",
-        "scenery"),
-
-    "merlict_plenoscope_propagator_config_relpath": op.join(
-        "input",
-        "merlict_propagation_config.json"),
-
+EXAMPLE_CONFIG = {
     "light_field_geometry": {
         "num_photons_per_block": 1000*1000,
         "num_blocks": 1337,
@@ -125,19 +93,19 @@ cfg = {
     "particles": {
         "gamma": {
             "particle_id": 1,
-            "energy_bin_edges_GeV": [0.5, MAX_ENERGY_GEV],
+            "energy_bin_edges_GeV": [0.5, 100],
             "max_scatter_angle_deg": 5,
             "energy_power_law_slope": -1.7,
         },
         "electron": {
             "particle_id": 3,
-            "energy_bin_edges_GeV": [0.5, MAX_ENERGY_GEV],
+            "energy_bin_edges_GeV": [0.5, 100],
             "max_scatter_angle_deg": 30,
             "energy_power_law_slope": -1.7,
         },
         "proton": {
             "particle_id": 14,
-            "energy_bin_edges_GeV": [5, MAX_ENERGY_GEV],
+            "energy_bin_edges_GeV": [5, 100],
             "max_scatter_angle_deg": 30,
             "energy_power_law_slope": -1.7,
         },
@@ -156,30 +124,51 @@ cfg = {
     },
 
     "num_runs": {
-        "gamma": NUM_GAMMA_RUNS,
-        "electron": 5*NUM_GAMMA_RUNS,
-        "proton": 4*NUM_GAMMA_RUNS
+        "gamma": 1,
+        "electron": 4,
+        "proton": 5
     },
 
-    "num_airshowers_per_run": NUM_AIRSHOWERS_PER_RUN,
+    "num_airshowers_per_run": 100,
 }
 
 
-if __name__ == '__main__':
-    try:
-        arguments = docopt.docopt(__doc__)
-        out_absdir = op.abspath(arguments['--out_dir'])
-    except docopt.DocoptExit as e:
-        print(e)
+def init(out_dir, config=EXAMPLE_CONFIG, cfg_files=EXAMPLE_CONFIG_FILES):
+    out_absdir =op.abspath(out_dir)
+    os.makedirs(out_absdir)
+    os.makedirs(opj(out_absdir, 'input'))
+    with open(opj(out_absdir, 'input', 'config.json'+'tmp'), "wt") as fout:
+        fout.write(json.dumps(cfg, indent=4))
+    shutil.move(
+        opj(out_absdir, 'input', 'config.json'+'tmp'),
+        opj(out_absdir, 'input', 'config.json'))
 
-    date_dict_now = irf.date_dict_now()
-    sge._print("Start main()")
+    plmr.instrument_response.safe_copy(
+        src=cfg_files['plenoscope_scenery_abspath'],
+        dst=opj(out_absdir, 'input', 'scenery'))
+
+    plmr.instrument_response.safe_copy(
+        src=cfg_files['merlict_plenoscope_propagator_config_abspath'],
+        dst=opj(out_absdir, 'input', 'merlict_propagation_config.json'))
+
+
+def run(
+    path,
+    MULTIPROCESSING_POOL = "sun_grid_engine",
+    executables=EXAMPLE_EXECUTABLES,
+    TMP_DIR_ON_WORKERNODE=True,
+    KEEP_TMP=False,
+    LAZY_REDUCTION=False,
+):
+    date_dict_now = map_and_reduce.date_dict_now()
+    sge._print("Start run()")
+    out_absdir =op.abspath(path)
 
     if TMP_DIR_ON_WORKERNODE:
         tmp_absdir = None
         sge._print("Use tmp_dir on workernodes.")
     else:
-        tmp_absdir = op.join(out_absdir, "tmp")
+        tmp_absdir = opj(out_absdir, "tmp")
         os.makedirs(tmp_absdir, exist_ok=True)
         sge._print("Use tmp_dir in out_dir {:s}.".format(tmp_absdir))
 
@@ -193,72 +182,60 @@ if __name__ == '__main__':
         raise KeyError(
             "Unknown MULTIPROCESSING_POOL: {:s}".format(MULTIPROCESSING_POOL))
 
-    os.makedirs(out_absdir, exist_ok=True)
-    input_absdir = op.join(out_absdir, 'input')
-    os.makedirs(input_absdir, exist_ok=True)
-    cfg_abspath = op.join(input_absdir, "config.json")
-
-    if op.exists(cfg_abspath):
-        sge._print("Read existing config")
-        with open(cfg_abspath, "rt") as fin:
-            cfg = json.loads(fout.read())
-    else:
-        sge._print("Write new config")
-        with open(cfg_abspath, "wt") as fout:
-            fout.write(json.dumps(cfg, indent=4))
-
-        for cfg_file in cfg_files:
-            cfg_file_abspath = cfg_files[cfg_file]
-            local_cfg_file_abspath = op.join(
-                input_absdir,
-                op.basename(cfg_file_abspath))
-            local_cfg_file_relpath = op.join(
-                'input',
-                op.basename(cfg_file_abspath))
-            if not op.exists(local_cfg_file_abspath):
-                plmr.instrument_response.safe_copy(
-                    src=cfg_file_abspath,
-                    dst=local_cfg_file_abspath)
-            cfg_file_rel = cfg_file.replace('abspath', 'relpath')
-            cfg[cfg_file_rel] = local_cfg_file_relpath
+    sge._print("Read config")
+    with open(opj(out_absdir, 'input', 'config.json'), "rt") as fin:
+        cfg = json.loads(fout.read())
 
     sge._print("Estimating light-field-geometry.")
     # ============================================
-    lfg_abspath = op.join(out_absdir, 'light_field_geometry')
-    if not op.exists(lfg_abspath):
-        lfg_tmp_absdir = lfg_abspath+".tmp"
-        os.makedirs(lfg_tmp_absdir)
-        lfg_jobs = plmr.make_jobs_light_field_geometry(
-            merlict_map_path=executables[
-                "merlict_plenoscope_calibration_map_abspath"],
-            scenery_path=op.join(
+    if op.exists(opj(out_absdir, 'light_field_geometry')):
+        assert plmr.contains_same_bytes(
+            opj(
+                out_dir,
+                'input',
+                'scenery',
+                'scenery.json'),
+            opj(
                 out_absdir,
-                cfg["plenoscope_scenery_relpath"]),
-            out_dir=lfg_tmp_absdir,
-            num_photons_per_block=cfg[
-                'light_field_geometry']['num_photons_per_block'],
-            num_blocks=cfg[
-                'light_field_geometry']['num_blocks'],
-            random_seed=0)
-        rc = pool.map(plmr.run_job_light_field_geometry, lfg_jobs)
-        subprocess.call([
-            executables["merlict_plenoscope_calibration_reduce_abspath"],
-            '--input', lfg_tmp_absdir,
-            '--output', lfg_abspath])
-        shutil.rmtree(lfg_tmp_absdir)
+                'light_field_geometry',
+                'input',
+                'scenery',
+                'scenery.json'))
+    else:
+        with tempfile.TemporaryDirectory(
+            prefix='light_field_geometry',
+            dir=out_absdir
+        ) as tmp_dir:
+            lfg_jobs = plmr.light_field_geometry.make_jobs(
+                merlict_map_path=executables[
+                    "merlict_plenoscope_calibration_map_abspath"],
+                scenery_path=opj(
+                    out_absdir,
+                    cfg["plenoscope_scenery_relpath"]),
+                out_dir=tmp_dir,
+                num_photons_per_block=cfg[
+                    'light_field_geometry']['num_photons_per_block'],
+                num_blocks=cfg[
+                    'light_field_geometry']['num_blocks'],
+                random_seed=0)
+            rc = pool.map(plmr.light_field_geometry.run_job, lfg_jobs)
+            subprocess.call([
+                executables["merlict_plenoscope_calibration_reduce_abspath"],
+                '--input', tmp_dir,
+                '--output', opj(out_absdir, 'light_field_geometry')])
 
     sge._print("Estimating instrument-response.")
     # ===========================================
     irf_jobs = []
     run_id = 1
     for site_key in cfg["sites"]:
-        site_absdir = op.join(out_absdir, site_key)
+        site_absdir = opj(out_absdir, site_key)
         if op.exists(site_absdir):
             continue
         os.makedirs(site_absdir, exist_ok=True)
 
         for particle_key in cfg["particles"]:
-            site_particle_absdir = op.join(site_absdir, particle_key)
+            site_particle_absdir = opj(site_absdir, particle_key)
             if op.exists(site_particle_absdir):
                 continue
             os.makedirs(site_particle_absdir, exist_ok=True)
@@ -274,21 +251,24 @@ if __name__ == '__main__':
                     "sum_trigger": cfg["sum_trigger"],
                     "corsika_primary_path": executables[
                         "corsika_primary_abspath"],
-                    "plenoscope_scenery_path": op.join(
+                    "plenoscope_scenery_path": opj(
                         out_absdir,
-                        cfg["plenoscope_scenery_relpath"]),
+                        'input',
+                        'scenery'),
                     "merlict_plenoscope_propagator_path": executables[
                         "merlict_plenoscope_propagator_abspath"],
-                    "light_field_geometry_path": lfg_abspath,
-                    "merlict_plenoscope_propagator_config_path": op.join(
+                    "light_field_geometry_path":
+                        opj(out_absdir, 'light_field_geometry'),
+                    "merlict_plenoscope_propagator_config_path": opj(
                         out_absdir,
-                        cfg["merlict_plenoscope_propagator_config_relpath"]),
+                        'input',
+                        'merlict_propagation_config.json'),
                     "log_dir":
-                        op.join(site_particle_absdir, "log.map"),
+                        opj(site_particle_absdir, "log.map"),
                     "past_trigger_dir":
-                        op.join(site_particle_absdir, "past_trigger.map"),
+                        opj(site_particle_absdir, "past_trigger.map"),
                     "feature_dir":
-                        op.join(site_particle_absdir, "features.map"),
+                        opj(site_particle_absdir, "features.map"),
                     "keep_tmp": KEEP_TMP,
                     "tmp_dir": tmp_absdir,
                     "date": date_dict_now,
@@ -301,17 +281,17 @@ if __name__ == '__main__':
     sge._print("Reduce instrument-response.")
 
     for site_key in cfg["sites"]:
-        site_absdir = op.join(out_absdir, site_key)
+        site_absdir = opj(out_absdir, site_key)
         for particle_key in cfg["particles"]:
-            site_particle_absdir = op.join(site_absdir, particle_key)
-            log_absdir = op.join(site_particle_absdir, "log.map")
-            feature_absdir = op.join(site_particle_absdir, "features.map")
+            site_particle_absdir = opj(site_absdir, particle_key)
+            log_absdir = opj(site_particle_absdir, "log.map")
+            feature_absdir = opj(site_particle_absdir, "features.map")
 
             # run-time
             # ========
-            log_abspath = op.join(site_particle_absdir, 'runtime.csv')
+            log_abspath = opj(site_particle_absdir, 'runtime.csv')
             if not op.exists(log_abspath) or not LAZY_REDUCTION:
-                _lop_paths = glob.glob(op.join(log_absdir, "*_runtime.jsonl"))
+                _lop_paths = glob.glob(opj(log_absdir, "*_runtime.jsonl"))
                 irf.logging.reduce(
                     list_of_log_paths=_lop_paths,
                     out_path=log_abspath)
@@ -320,12 +300,12 @@ if __name__ == '__main__':
 
             # event table
             # ===========
-            event_table_abspath = op.join(
+            event_table_abspath = opj(
                 site_particle_absdir,
                 'event_table.tar')
             if not op.exists(event_table_abspath) or not LAZY_REDUCTION:
                 _feature_paths = glob.glob(
-                    op.join(feature_absdir, "*_event_table.tar"))
+                    opj(feature_absdir, "*_event_table.tar"))
                 irf.table.reduce(
                     list_of_feature_paths=_feature_paths,
                     out_path=event_table_abspath)
@@ -334,9 +314,9 @@ if __name__ == '__main__':
 
             # grid images
             # ===========
-            grid_abspath = op.join(site_particle_absdir, 'grid.tar')
+            grid_abspath = opj(site_particle_absdir, 'grid.tar')
             if not op.exists(grid_abspath) or not LAZY_REDUCTION:
-                _grid_paths = glob.glob(op.join(feature_absdir, "*_grid.tar"))
+                _grid_paths = glob.glob(opj(feature_absdir, "*_grid.tar"))
                 irf.grid.reduce(
                     list_of_grid_paths=_grid_paths,
                     out_path=grid_abspath)
