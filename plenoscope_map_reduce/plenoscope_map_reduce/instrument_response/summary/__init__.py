788f54b36c93721a4c56c8860b6da5b6c28e7a1c
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-02-10 10:31:45 +0100

    use independent script for ease of debugging

--- plenoscope_map_reduce/plenoscope_map_reduce/instrument_response/summary/__init__.py
+++ plenoscope_map_reduce/plenoscope_map_reduce/instrument_response/summary/__init__.py
@@ -3,12 +3,9 @@ from os.path import join as opj
 import pandas as pd
 import numpy as np
 import json
-from . import runtime
 from .. import table
 from .. import merlict
 from .. import grid
-from . import grid_direction
-from . import grid_area
 from . import figure
 from . import effective
 
@@ -120,137 +117,6 @@ def summarize(
                 site_key,
                 particle_key,
                 'event_table.tar'))
-
-            # runtime
-            # -------
-            runtime_table = runtime.read(opj(
-                run_dir,
-                site_key,
-                particle_key,
-                'runtime.csv'))
-            '''
-            extended_runtime_table = runtime.merge_event_table(
-                runtime_table=runtime_table,
-                event_table=event_table)
-
-            runtime.write(
-                path=opj(out_dir, prefix+'_runtime.csv'),
-                table=extended_runtime_table,)
-
-            runtime.write_relative_runtime(
-                table=extended_runtime_table,
-                out_path=opj(out_dir, prefix+'_relative_runtime'),
-                figure_config=fc16by9)
-
-            runtime.write_speed(
-                table=extended_runtime_table,
-                out_path=opj(out_dir, prefix+'_speed_runtime'),
-                figure_config=fc16by9)
-
-            # grid directions
-            # ---------------
-            com_pri_grd = table.merge(
-                event_table=event_table,
-                level_keys=['primary', 'grid'])
-
-            c_bin_edges = grid_direction.guess_c_bin_edges(
-                num_events=com_pri_grd['primary'].shape[0],
-                num_c_bins=num_c_bins)
-            icu, ecu, nev = grid_direction.histogram_grid_trigger(
-                event_table_common_primary_grid=com_pri_grd,
-                energy_bin_edges=energy_bin_edges,
-                c_bin_edges=c_bin_edges)
-            grid_direction.write_qube_of_figures(
-                out_path=opj(out_dir, prefix+'_grid_direction'),
-                intensity_cube=icu,
-                exposure_cube=ecu,
-                num_events_stack=nev,
-                c_bin_edges=c_bin_edges,
-                energy_bin_edges=energy_bin_edges,
-                figure_config=fc5by4)
-
-            c_bin_edges = grid_direction.guess_c_bin_edges(
-                num_events=event_table['pasttrigger'].shape[0],
-                num_c_bins=num_c_bins)
-            icu, ecu, nev = grid_direction.histogram_plenoscope_trigger(
-                event_table=event_table,
-                energy_bin_edges=energy_bin_edges,
-                c_bin_edges=c_bin_edges)
-            grid_direction.write_qube_of_figures(
-                out_path=opj(out_dir, prefix+'_pasttrigger_direction'),
-                intensity_cube=icu,
-                exposure_cube=ecu,
-                num_events_stack=nev,
-                c_bin_edges=c_bin_edges,
-                energy_bin_edges=energy_bin_edges,
-                figure_config=fc5by4)
-
-            '''
-            # grid areas pasttrigger
-            # ----------------------
-
-            mrg_table = table.merge(
-                event_table=event_table,
-                level_keys=[
-                    'primary',
-                    'grid',
-                    'core',
-                    'cherenkovsize',
-                    'cherenkovpool',
-                    'cherenkovsizepart',
-                    'cherenkovpoolpart',
-                    'trigger',
-                    'pasttrigger'])
-
-            _grid_pasttrigger_path = opj(
-                out_dir,
-                'cache',
-                '{:s}_{:s}_grid_pasttrigger.tar'.format(
-                    site_key,
-                    particle_key))
-            if os.path.exists(_grid_pasttrigger_path):
-                grid_histograms_pasttrigger = grid.read_histograms(
-                    path=_grid_pasttrigger_path,
-                    indices=mrg_table['pasttrigger'])
-            else:
-                grid_histograms_pasttrigger = grid.read_histograms(
-                    path=opj(run_dir, site_key, particle_key, 'grid.tar'),
-                    indices=mrg_table['pasttrigger'])
-                grid.write_histograms(
-                    path=_grid_pasttrigger_path,
-                    grid_histograms=grid_histograms_pasttrigger)
-
-
-            c_bin_edges = grid_direction.guess_c_bin_edges(
-                num_events=mrg_table['pasttrigger'].shape[0],
-                num_c_bins=num_c_bins)
-
-            adh = grid_area.histogram_area_direction(
-                energy_bin_edges=energy_bin_edges,
-                primary_table=mrg_table['primary'],
-                grid_histograms=grid_histograms_pasttrigger,
-                grid_geometry=inp['grid_geometry'],
-                c_bin_edges_deg=c_bin_edges)
-
-            print(adh)
-
-            for energy_idx in range(len(energy_bin_edges) - 1):
-                grid_area.write_area_direction(
-                    path=opj(
-                        out_dir,
-                        '{:s}_{:s}_grid_pasttrigger_{:06d}'.format(
-                            site_key,
-                            particle_key,
-                            energy_idx)),
-                    grid_intensity=adh['grid_intensities'][energy_idx],
-                    grid_xy_bin_edges=inp['grid_geometry']['xy_bin_edges'],
-                    direction_intensity=adh['direction_intensities'][energy_idx],
-                    c_bin_edges_deg=c_bin_edges,
-                    num_airshower=adh['num_airshowers'][energy_idx],
-                    energy_GeV_start=energy_bin_edges[energy_idx],
-                    energy_GeV_stop=energy_bin_edges[energy_idx + 1],
-                    figure_config=figure.CONFIG_16_9)
-
 
             # effective quantity
             # ------------------
--- plenoscope_map_reduce/plenoscope_map_reduce/instrument_response/summary/scripts/runtime.py
+++ plenoscope_map_reduce/plenoscope_map_reduce/instrument_response/summary/scripts/runtime.py
@@ -2,6 +2,10 @@
 import sys
 from os.path import join as opj
 import os
+import pandas as pd
+import numpy as np
+import json
+import shutil
 from plenoscope_map_reduce import instrument_response as irf
 
 import matplotlib
@@ -17,13 +21,167 @@ summary_dir = argv[2]
 irf_config = irf.summary.read_instrument_response_config(run_dir=run_dir)
 sum_config = irf.summary.read_summary_config(summary_dir=summary_dir)
 
+
+def read(path):
+    return pd.read_csv(path).to_records(index=False)
+
+
+def write(path, table):
+    df = pd.DataFrame(table)
+    with open(path+'.tmp', 'wt') as f:
+        f.write(df.to_csv(index=False))
+    os.rename(path+'.tmp', path)
+
+
+def _sum_energy_in_runs(event_table, run_ids):
+    _out = []
+    for run_id in run_ids:
+        mask = event_table["primary"]["run_id"] == run_id
+        sum_energy = np.sum(event_table["primary"]["energy_GeV"][mask])
+        _out.append({
+            "run_id": int(run_id),
+            "energy_sum_GeV": float(sum_energy)})
+    out = pd.DataFrame(_out).to_records(index=False)
+    return out
+
+
+def _num_events_in_runs(event_table, level_key, run_ids, key):
+    _out = []
+    for run_id in run_ids:
+        mask = event_table[level_key]["run_id"] == run_id
+        _out.append({
+            "run_id": int(run_id),
+            key: int(np.sum(mask))})
+    out = pd.DataFrame(_out).to_records(index=False)
+    return out
+
+
+def merge_event_table(runtime_table, event_table):
+    runtime = runtime_table
+    sum_energies = _sum_energy_in_runs(
+        event_table=event_table,
+        run_ids=runtime["run_id"])
+    num_events_corsika = _num_events_in_runs(
+        event_table=event_table,
+        level_key="primary",
+        run_ids=runtime["run_id"],
+        key="num_events_corsika")
+    num_events_merlict = _num_events_in_runs(
+        event_table=event_table,
+        level_key="trigger",
+        run_ids=runtime["run_id"],
+        key="num_events_merlict")
+    num_events_past_trigger = _num_events_in_runs(
+        event_table=event_table,
+        level_key="pasttrigger",
+        run_ids=runtime["run_id"],
+        key="num_events_pasttrigger")
+    rta = pd.DataFrame(runtime)
+    rta = pd.merge(rta, pd.DataFrame(sum_energies), on=["run_id"])
+    rta = pd.merge(rta, pd.DataFrame(num_events_corsika), on=["run_id"])
+    rta = pd.merge(rta, pd.DataFrame(num_events_merlict), on=["run_id"])
+    rta = pd.merge(rta, pd.DataFrame(num_events_past_trigger), on=["run_id"])
+    return rta.to_records(index=False)
+
+
+def write_relative_runtime(table, out_path, figure_config):
+    ert = table
+    total_times = {}
+    total_time = 0
+    for key in irf.logging.KEYS:
+        total_times[key] = np.sum(ert[key])
+        total_time += total_times[key]
+
+    relative_times = {}
+    for key in irf.logging.KEYS:
+        relative_times[key] = float(total_times[key]/total_time)
+
+    fig = irf.summary.figure.figure(figure_config)
+    ax = fig.add_axes([0.3, 0.15, 0.5, 0.8])
+    labels = []
+    sizes = []
+    _y = np.arange(len(irf.logging.KEYS))
+    for ikey, key in enumerate(relative_times):
+        labels.append(key)
+        sizes.append(relative_times[key])
+        x = relative_times[key]
+        ax.plot(
+            [0, x, x, 0],
+            [_y[ikey]-.5, _y[ikey]-.5, _y[ikey]+.5, _y[ikey]+.5],
+            "k")
+    ax.set_xlabel('rel. runtime')
+    ax.set_yticks(_y)
+    ax.set_yticklabels(labels, rotation=0)
+    ax.set_xlim([0, 1])
+    ax.grid(color='k', linestyle='-', linewidth=0.66, alpha=0.1)
+    ax.spines['top'].set_color('none')
+    ax.spines['right'].set_color('none')
+    out_path_jpg = out_path+'.jpg'
+    fig.savefig(out_path_jpg+".tmp.jpg")
+    os.rename(out_path_jpg+".tmp.jpg", out_path_jpg)
+    plt.close(fig)
+    out_path_json = out_path+'.json'
+    with open(out_path_json+'.tmp', "wt") as fout:
+        fout.write(json.dumps(relative_times))
+    os.rename(out_path_json+'.tmp', out_path_json)
+
+
+def write_speed(table, out_path, figure_config):
+    ert = table
+    speed_keys = {
+        "corsika": "num_events_corsika",
+        "grid": "num_events_corsika",
+        "merlict": "num_events_merlict",
+        "trigger": "num_events_merlict",
+        "cherenkov_classification": "num_events_pasttrigger",
+        "feature_extraction": "num_events_pasttrigger",
+    }
+    speeds = {}
+    for key in speed_keys:
+        num_events = ert[speed_keys[key]]
+        mask = num_events > 0
+        if np.sum(mask) == 0:
+            speeds[key] = 0.
+        else:
+            speeds[key] = float(np.mean(num_events[mask]/ert[key][mask]))
+
+    fig = irf.summary.figure.figure(figure_config)
+    ax = fig.add_axes([0.3, 0.15, 0.5, 0.8])
+    labels = []
+    sizes = []
+    _y = np.arange(len(speeds))
+    for ikey, key in enumerate(speeds):
+        labels.append(key)
+        sizes.append(speeds[key])
+        x = speeds[key]
+        ax.plot(
+            [0, x, x, 0],
+            [_y[ikey]-.5, _y[ikey]-.5, _y[ikey]+.5, _y[ikey]+.5],
+            "k")
+    sizes = np.array(sizes)
+    valid = np.logical_not(np.logical_or(np.isinf(sizes), np.isnan(sizes)))
+    ax.set_xlabel('speed / events s$^{-1}$')
+    ax.set_yticks(_y)
+    ax.set_yticklabels(labels, rotation=0)
+    ax.set_xlim([0, np.max(sizes[valid])*1.1])
+    ax.grid(color='k', linestyle='-', linewidth=0.66, alpha=0.1)
+    ax.spines['top'].set_color('none')
+    ax.spines['right'].set_color('none')
+    fig.savefig(out_path+'.tmp'+'.jpg')
+    os.rename(out_path+'.tmp'+'.jpg', out_path+'.jpg')
+    plt.close(fig)
+    with open(out_path+'.json'+'.tmp', "wt") as fout:
+        fout.write(json.dumps(speeds))
+    os.rename(out_path+'.json'+'.tmp', out_path+'.json')
+
+
 for site_key in irf_config['config']['sites']:
     for particle_key in irf_config['config']['particles']:
         prefix_str = '{:s}_{:s}'.format(site_key, particle_key)
 
         extended_runtime_path = opj(summary_dir, prefix_str+'_runtime.csv')
         if os.path.exists(extended_runtime_path):
-            extended_runtime_table = irf.summary.runtime.read(
+            extended_runtime_table = read(
                 extended_runtime_path)
         else:
             event_table = irf.summary.read_event_table_cache(
@@ -31,24 +189,24 @@ for site_key in irf_config['config']['sites']:
                 run_dir=run_dir,
                 site_key=site_key,
                 particle_key=particle_key)
-            runtime_table = irf.summary.runtime.read(opj(
+            runtime_table = read(opj(
                 run_dir,
                 site_key,
                 particle_key,
                 'runtime.csv'))
-            extended_runtime_table = irf.summary.runtime.merge_event_table(
+            extended_runtime_table = merge_event_table(
                 runtime_table=runtime_table,
                 event_table=event_table)
-            irf.summary.runtime.write(
+            write(
                 path=opj(summary_dir, prefix_str+'_runtime.csv'),
                 table=extended_runtime_table,)
 
-        irf.summary.runtime.write_relative_runtime(
+        write_relative_runtime(
             table=extended_runtime_table,
             out_path=opj(summary_dir, prefix_str+'_relative_runtime'),
             figure_config=sum_config['figure_16_9'])
 
-        irf.summary.runtime.write_speed(
+        write_speed(
             table=extended_runtime_table,
             out_path=opj(summary_dir, prefix_str+'_speed_runtime'),
             figure_config=sum_config['figure_16_9'])
