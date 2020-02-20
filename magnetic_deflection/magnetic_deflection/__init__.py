25e7465a507640543dbac5aa84c961aeb8e1a9c1
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-02-20 08:59:45 +0100

    example sites

--- magnetic_deflection/magnetic_deflection/__init__.py
+++ magnetic_deflection/magnetic_deflection/__init__.py
@@ -11,11 +11,18 @@ import sys
 import time
 
 
-EXAMPLE_SITE = {
+EXAMPLE_SITE_CHILE = {
     "earth_magnetic_field_x_muT": 20.815,
     "earth_magnetic_field_z_muT": -11.366,
     "observation_level_asl_m": 5e3,
     "atmosphere_id": 26,
+}
+
+EXAMPLE_SITE_NAMIBIA = {
+    'earth_magnetic_field_x_muT': 12.5,
+    'earth_magnetic_field_z_muT': -25.9,
+    'observation_level_asl_m': 2300,
+    'atmosphere_id': 10
 }
 
 NUM_FLOATS_IN_EVENTSUMMARY = 25
