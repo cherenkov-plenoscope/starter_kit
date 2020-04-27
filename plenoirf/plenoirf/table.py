62baa47e19b450277ac076828df3e1e6b81ef97b
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-04-27 07:28:51 +0200

    prove

--- plenoirf/plenoirf/table.py
+++ plenoirf/plenoirf/table.py
@@ -12,30 +12,44 @@ NUM_DIGITS_RUN_ID = 6
 NUM_DIGITS_AIRSHOWER_ID = 3
 NUM_DIGITS_SEED = NUM_DIGITS_RUN_ID + NUM_DIGITS_AIRSHOWER_ID
 
-MAX_NUM_EVENTS_IN_RUN = 10**NUM_DIGITS_AIRSHOWER_ID
-MAX_NUM_RUNS = 10**NUM_DIGITS_RUN_ID
-MAX_NUM_EVENTS_TOTAL = MAX_NUM_EVENTS_IN_RUN*MAX_NUM_RUNS
+NUM_AIRSHOWER_IDS_IN_RUN = 10**NUM_DIGITS_AIRSHOWER_ID
+NUM_RUN_IDS = 10**NUM_DIGITS_RUN_ID
+NUM_SEEDS = NUM_AIRSHOWER_IDS_IN_RUN*NUM_RUN_IDS
 
 SEED_TEMPLATE_STR = '{seed:0'+str(NUM_DIGITS_SEED)+'d}'
 
 
 def random_seed_based_on(run_id, airshower_id):
-    assert run_id < MAX_NUM_RUNS
-    assert airshower_id < MAX_NUM_EVENTS_IN_RUN
-    return run_id*MAX_NUM_EVENTS_IN_RUN + airshower_id
+    assert is_valid_run_id(run_id)
+    assert is_valid_airshower_id(airshower_id)
+    return run_id*NUM_AIRSHOWER_IDS_IN_RUN + airshower_id
 
 
 def run_id_from_seed(seed):
     if np.isscalar(seed):
-        assert seed <= MAX_NUM_EVENTS_TOTAL
+        assert seed <= NUM_SEEDS
     else:
         seed = np.array(seed)
-        assert (seed <= MAX_NUM_EVENTS_TOTAL).all()
-    return seed//MAX_NUM_EVENTS_IN_RUN
+        assert (seed <= NUM_SEEDS).all()
+    return seed//NUM_AIRSHOWER_IDS_IN_RUN
 
 
 def airshower_id_from_seed(seed):
-    return seed - run_id_from_seed(seed)*MAX_NUM_EVENTS_IN_RUN
+    return seed - run_id_from_seed(seed)*NUM_AIRSHOWER_IDS_IN_RUN
+
+
+def is_valid_run_id(run_id):
+    if run_id >= 0 and run_id < NUM_RUN_IDS:
+        return True
+    else:
+        return False
+
+
+def is_valid_airshower_id(airshower_id):
+    if airshower_id >= 0 and airshower_id < NUM_AIRSHOWER_IDS_IN_RUN:
+        return True
+    else:
+        return False
 
 
 STRUCTURE = {}
--- plenoirf/plenoirf/tests/test_seed.py
+++ plenoirf/plenoirf/tests/test_seed.py
@@ -4,47 +4,69 @@ import numpy as np
 import pytest
 
 
+def test_valid_airshower_id():
+    assert not plenoirf.table.is_valid_airshower_id(-1)
+    assert plenoirf.table.is_valid_airshower_id(0)
+    assert plenoirf.table.is_valid_airshower_id(1)
+
+    assert plenoirf.table.is_valid_airshower_id(
+        plenoirf.table.NUM_AIRSHOWER_IDS_IN_RUN - 1)
+    assert not plenoirf.table.is_valid_airshower_id(
+        plenoirf.table.NUM_AIRSHOWER_IDS_IN_RUN)
+
+
+def test_valid_run_id():
+    assert not plenoirf.table.is_valid_run_id(-1)
+    assert plenoirf.table.is_valid_run_id(0)
+    assert plenoirf.table.is_valid_run_id(1)
+
+    assert plenoirf.table.is_valid_run_id(
+        plenoirf.table.NUM_RUN_IDS - 1)
+    assert not plenoirf.table.is_valid_run_id(
+        plenoirf.table.NUM_RUN_IDS)
+
+
 def test_seed_limit_run_id():
     seed = plenoirf.table.random_seed_based_on(
-        run_id=plenoirf.table.MAX_NUM_RUNS - 1,
-        airshower_id=0)
+        run_id=plenoirf.table.NUM_RUN_IDS - 1,
+        airshower_id=1)
     assert (
-        plenoirf.table.MAX_NUM_RUNS - 1 ==
+        plenoirf.table.NUM_RUN_IDS - 1 ==
         plenoirf.table.run_id_from_seed(seed=seed)
     )
-    assert 0 == plenoirf.table.airshower_id_from_seed(seed=seed)
+    assert 1 == plenoirf.table.airshower_id_from_seed(seed=seed)
 
     with pytest.raises(AssertionError) as e:
         plenoirf.table.random_seed_based_on(
-            run_id=plenoirf.table.MAX_NUM_RUNS,
-            airshower_id=0)
+            run_id=plenoirf.table.NUM_RUN_IDS,
+            airshower_id=1)
 
 
 def test_seed_limit_airshower_id():
     seed = plenoirf.table.random_seed_based_on(
-        run_id=0,
-        airshower_id=plenoirf.table.MAX_NUM_EVENTS_IN_RUN - 1)
+        run_id=1,
+        airshower_id=plenoirf.table.NUM_AIRSHOWER_IDS_IN_RUN - 1)
     assert (
-        plenoirf.table.MAX_NUM_EVENTS_IN_RUN - 1 ==
+        plenoirf.table.NUM_AIRSHOWER_IDS_IN_RUN - 1 ==
         plenoirf.table.airshower_id_from_seed(seed=seed)
     )
-    assert 0 == plenoirf.table.run_id_from_seed(seed=seed)
+    assert 1 == plenoirf.table.run_id_from_seed(seed=seed)
 
     with pytest.raises(AssertionError) as e:
         plenoirf.table.random_seed_based_on(
             run_id=0,
-            airshower_id=plenoirf.table.MAX_NUM_EVENTS_IN_RUN)
+            airshower_id=plenoirf.table.NUM_AIRSHOWER_IDS_IN_RUN)
 
 
 def test_seed_combinations():
     np.random.seed(0)
     run_ids = np.random.uniform(
         0,
-        plenoirf.table.MAX_NUM_RUNS,
+        plenoirf.table.NUM_RUN_IDS - 1,
         size=300).astype('i4')
     airshower_ids = np.random.uniform(
         0,
-        plenoirf.table.MAX_NUM_EVENTS_IN_RUN,
+        plenoirf.table.NUM_AIRSHOWER_IDS_IN_RUN - 1,
         size=300).astype('i4')
 
     for run_id in run_ids:
@@ -63,18 +85,18 @@ def test_seed_combinations():
 def test_seed_num_digits():
     assert plenoirf.table.NUM_DIGITS_SEED >= 6
     assert plenoirf.table.NUM_DIGITS_SEED <= 12
-    assert plenoirf.table.MAX_NUM_EVENTS_TOTAL < np.iinfo(np.int32).max
+    assert plenoirf.table.NUM_SEEDS < np.iinfo(np.int32).max
 
 
 def test_template_string():
     np.random.seed(0)
     run_ids = np.random.uniform(
         0,
-        plenoirf.table.MAX_NUM_RUNS,
+        plenoirf.table.NUM_RUN_IDS - 1,
         size=30).astype('i4')
     airshower_ids = np.random.uniform(
         0,
-        plenoirf.table.MAX_NUM_EVENTS_IN_RUN,
+        plenoirf.table.NUM_AIRSHOWER_IDS_IN_RUN - 1,
         size=30).astype('i4')
 
     for run_id in run_ids:
