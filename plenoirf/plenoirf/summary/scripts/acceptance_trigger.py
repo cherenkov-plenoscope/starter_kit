2f6b8b3482b5602838a1e41dedda32fa761a3f52
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-07-16 17:00:38 +0200

    kick nsb-rate estimate

--- plenoirf/plenoirf/summary/scripts/acceptance_trigger.py
+++ plenoirf/plenoirf/summary/scripts/acceptance_trigger.py
@@ -18,15 +18,6 @@ sum_config = irf.summary.read_summary_config(summary_dir=pa['summary_dir'])
 
 os.makedirs(pa['out_dir'], exist_ok=True)
 
-MAX_CHERENKOV_IN_NSB_PE = sum_config[
-    'night_sky_background'][
-    'max_num_true_cherenkov_photons']
-TIME_SLICE_DURATION = 0.5e-9
-NUM_TIME_SLICES_PER_EVENT = (
-    100 -
-    irf_config['config']['sum_trigger']['integration_time_slices']
-)
-EXPOSURE_TIME_PER_EVENT = NUM_TIME_SLICES_PER_EVENT*TIME_SLICE_DURATION
 NUM_GRID_BINS = irf_config['grid_geometry']['num_bins_diameter']**2
 
 MAX_SOURCE_ANGLE_DEG = sum_config[
@@ -49,11 +40,9 @@ trigger_thresholds = sum_config['trigger']['ratescan_thresholds_pe']
 trigger_modus = sum_config["trigger"]["modus"]
 
 cosmic_response = {}
-_tmp_nsb_response = {}
 
 for site_key in irf_config['config']['sites']:
     cosmic_response[site_key] = {}
-    _tmp_nsb_response[site_key] = {}
     for particle_key in irf_config['config']['particles']:
 
         cosmic_response[site_key][particle_key] = {}
@@ -165,67 +154,6 @@ for site_key in irf_config['config']['sites']:
             _diffuse['relative_uncertainty'].append(_q_unc)
             cosmic_response[site_key][particle_key]['diffuse'] = _diffuse
 
-        # acceidental triggers in night-sky-background
-        # --------------------------------------------
-        idx_nsb = diffuse_particle_table['trigger'][spt.IDX][
-            diffuse_particle_table['trigger']['num_cherenkov_pe'] <=
-            MAX_CHERENKOV_IN_NSB_PE
-        ]
-        nsb_table = spt.cut_level_on_indices(
-            table=diffuse_particle_table,
-            structure=irf.table.STRUCTURE,
-            level_key='trigger',
-            indices=idx_nsb
-        )
-
-        _tmp_nsb_response[site_key][particle_key] = []
-        for threshold in trigger_thresholds:
-            idx_detected = irf.analysis.light_field_trigger_modi.make_indices(
-                trigger_table=nsb_table,
-                threshold=threshold,
-                modus=trigger_modus,
-            )
-            _tmp_nsb_response[site_key][particle_key].append({
-                'num_nsb_exposures': len(idx_nsb),
-                'num_nsb_triggers': len(idx_detected)
-            })
-
-nsb_response = {}
-for site_key in irf_config['config']['sites']:
-    num_nsb_exposures = 0
-    num_nsb_triggers_vs_threshold = np.zeros(
-        len(trigger_thresholds),
-        dtype=np.int
-    )
-    for particle_key in irf_config['config']['particles']:
-        num_nsb_exposures += _tmp_nsb_response[
-            site_key][
-            particle_key][
-            0][
-            'num_nsb_exposures']
-        for tt, threshold in enumerate(trigger_thresholds):
-            num_nsb_triggers_vs_threshold[tt] += _tmp_nsb_response[
-                site_key][
-                particle_key][
-                tt][
-                'num_nsb_triggers']
-
-    nsb_rate = (
-        num_nsb_triggers_vs_threshold /
-        (num_nsb_exposures*EXPOSURE_TIME_PER_EVENT)
-    )
-    relative_uncertainty = irf.analysis.effective_quantity._divide_silent(
-        numerator=np.sqrt(num_nsb_triggers_vs_threshold),
-        denominator=num_nsb_triggers_vs_threshold,
-        default=np.nan
-    )
-
-    nsb_response[site_key] = {
-        "rate": nsb_rate,
-        "rate_relative_uncertainty": relative_uncertainty,
-        "unit": "s$^{-1}$",
-    }
-
 Qout = {
     "energy_bin_edges": {
         "value": energy_bin_edges,
@@ -235,8 +163,8 @@ Qout = {
         "value": trigger_thresholds,
         "unit": "p.e."
     },
+    "trigger_modus": trigger_modus,
     "cosmic_response": cosmic_response,
-    "night_sky_background_response": nsb_response,
 }
 
 with open(os.path.join(pa['out_dir'], 'acceptance_trigger.json'), 'wt') as f:
