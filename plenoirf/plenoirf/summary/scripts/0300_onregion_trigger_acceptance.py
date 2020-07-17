61930a15f7da92a406229cf6408607e3dda446c0
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-07-17 17:26:53 +0200

    export to file-system

--- plenoirf/plenoirf/summary/scripts/0300_onregion_trigger_acceptance.py
+++ plenoirf/plenoirf/summary/scripts/0300_onregion_trigger_acceptance.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python
 import sys
 import numpy as np
-import magnetic_deflection as mdfl
 import plenoirf as irf
 import sparse_table as spt
-import corsika_primary_wrapper as cpw
 import os
-import json
-
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import matplotlib.colors as plt_colors
 
 argv = irf.summary.argv_since_py(sys.argv)
 pa = irf.summary.paths_from_argv(argv)
 
 irf_config = irf.summary.read_instrument_response_config(run_dir=pa['run_dir'])
 sum_config = irf.summary.read_summary_config(summary_dir=pa['summary_dir'])
-
 np.random.seed(sum_config['random_seed'])
 
 os.makedirs(pa['out_dir'], exist_ok=True)
@@ -46,43 +37,33 @@ energy_bin_edges = np.geomspace(
 max_relative_leakage = sum_config['quality']['max_relative_leakage']
 min_reconstructed_photons = sum_config['quality']['min_reconstructed_photons']
 
+_psf = irf.json_numpy.read_tree(
+    os.path.join(pa['summary_dir'], "0200_gamma_point_spread_function")
+)
 onregion68 = {}
-
 for site_key in irf_config['config']['sites']:
-    with open(psf_path.format(site_key=site_key), 'rt') as f:
-        _site_psf = json.loads(f.read())
-    _energy_bin_centers = irf.summary.bin_centers(
-        bin_edges=np.array(_site_psf['energy_bin_edges_GeV'])
-    )
-    pivot_psf68_deg = np.interp(
-        x=PSF_ENERGY_GEV,
-        xp=_energy_bin_centers,
-        fp=_site_psf['delta_deg']
-    )
-    onregion68[site_key] = {
-        "pivot_energy_GeV": PSF_ENERGY_GEV,
-        "opening_angle_deg": pivot_psf68_deg
-    }
-
-fc16by9 = sum_config['plot']['16_by_9']
-fc5by4 = fc16by9.copy()
-fc5by4['cols'] = fc16by9['cols']*(9/16)*(5/4)
-
-response = {}
+    onregion68[site_key] = _psf[
+        site_key][
+        'gamma'][
+        'containment_angle_for_fix_onregion'][
+        'containment_angle']
 
 cosmic_ray_keys = list(irf_config['config']['particles'].keys())
 cosmic_ray_keys.remove('gamma')
 
-
 for site_key in irf_config['config']['sites']:
-    response[site_key] = {}
+
+    actual_onregion_over_possible_onregion_factor = (
+        onregion68[site_key]**2 /
+        MAX_SOURCE_ANGLE_DEG**2
+    )
 
     for particle_key in irf_config['config']['particles']:
-        response[site_key][particle_key] = {}
+        site_particle_dir = os.path.join(pa['out_dir'], site_key, particle_key)
+        os.makedirs(site_particle_dir, exist_ok=True)
 
         # point source
         # ------------
-        response[site_key][particle_key]['point'] = {}
         table_diffuse = spt.read(
             path=os.path.join(
                 pa['run_dir'],
@@ -110,7 +91,7 @@ for site_key in irf_config['config']['sites']:
         )
 
         # detected
-        idx_passed_trigger = irf.analysis.light_field_trigger_modi.make_indices(
+        idx_trigger = irf.analysis.light_field_trigger_modi.make_indices(
             trigger_table=table_point['trigger'],
             threshold=analysis_trigger_threshold,
             modus=trigger_modus,
@@ -123,9 +104,9 @@ for site_key in irf_config['config']['sites']:
         idx_has_features = table_point['features'][spt.IDX]
 
         idx_candidates = spt.intersection([
-            idx_passed_trigger,
+            idx_trigger,
             idx_has_features,
-            #idx_quality,
+            idx_quality,
         ])
 
         candidate_table = spt.cut_table_on_indices(
@@ -139,16 +120,20 @@ for site_key in irf_config['config']['sites']:
             common_indices=idx_candidates
         )
 
-        idx_detected_in_onregion = irf.analysis.cuts.cut_reconstructed_source_in_true_onregion(
-            table=candidate_table,
-            radial_angle_onregion_deg=onregion68[site_key]['opening_angle_deg'],
-        )
+        idx_detected_in_onregion = \
+            irf.analysis.cuts.cut_reconstructed_source_in_true_onregion(
+                table=candidate_table,
+                radial_angle_onregion_deg=onregion68[site_key],
+            )
 
         mask_detected = spt.make_mask_of_right_in_left(
             left_indices=table_point['primary'][spt.IDX],
             right_indices=idx_detected_in_onregion,
         )
-        (_q_eff, _q_unc) = irf.analysis.effective_quantity.effective_quantity_for_grid(
+        (
+            _q_eff,
+            _q_unc
+        ) = irf.analysis.effective_quantity.effective_quantity_for_grid(
             energy_bin_edges_GeV=energy_bin_edges,
             energy_GeV=table_point['primary']['energy_GeV'],
             mask_detected=mask_detected,
@@ -159,16 +144,21 @@ for site_key in irf_config['config']['sites']:
             total_num_grid_cells=NUM_GRID_BINS,
         )
 
-        response[site_key][particle_key]['point'] = {
-            "value": _q_eff,
-            "relative_uncertainty": _q_unc,
-            "unit": "m$^{2}$",
-        }
+        irf.json_numpy.write(
+            os.path.join(site_particle_dir, "point.json"),
+            {
+                "comment": (
+                    "Effective area "
+                    "for a point source, reconstructed in onregion. "
+                    "VS energy-bins"),
+                "unit": "m$^{2}$",
+                "mean": _q_eff,
+                "relative_uncertainty": _q_unc,
+            }
+        )
 
         # diffuse source
         # ---------------
-
-        response[site_key][particle_key]['diffuse'] = {}
 
         # thrown
         table_diffuse = table_diffuse
@@ -186,11 +176,10 @@ for site_key in irf_config['config']['sites']:
         )
         idx_features = table_diffuse['features'][spt.IDX]
 
-
         idx_candidates = spt.intersection([
             idx_trigger,
             idx_features,
-            #idx_quality,
+            idx_quality,
         ])
         table_candidates = spt.cut_table_on_indices(
             table=table_diffuse,
@@ -202,20 +191,23 @@ for site_key in irf_config['config']['sites']:
             table=table_candidates,
             common_indices=idx_candidates
         )
-        idx_in_onregion = irf.analysis.cuts.cut_reconstructed_source_in_random_possible_onregion(
-            feature_table=table_candidates['features'],
-            radial_angle_to_put_possible_onregion_deg=MAX_SOURCE_ANGLE_DEG,
-            radial_angle_onregion_deg=onregion68[site_key]['opening_angle_deg']
-        )
+        idx_in_possible_onregion = \
+            irf.analysis.cuts.cut_reconstructed_source_in_possible_onregion(
+                feature_table=table_candidates['features'],
+                radial_angle_to_put_possible_onregion_deg=MAX_SOURCE_ANGLE_DEG,
+            )
 
-        mask_detected = spt.make_mask_of_right_in_left(
+        mask_detected_in_possible_onregion = spt.make_mask_of_right_in_left(
             left_indices=table_diffuse['primary'][spt.IDX],
-            right_indices=idx_in_onregion,
+            right_indices=idx_in_possible_onregion,
         )
-        (_q_eff, _q_unc) = irf.analysis.effective_quantity.effective_quantity_for_grid(
+        (
+            _q_eff_all_possible_onregion,
+            _q_unc
+        ) = irf.analysis.effective_quantity.effective_quantity_for_grid(
             energy_bin_edges_GeV=energy_bin_edges,
             energy_GeV=table_diffuse['primary']['energy_GeV'],
-            mask_detected=mask_detected,
+            mask_detected=mask_detected_in_possible_onregion,
             quantity_scatter=(
                 table_diffuse['grid']['area_thrown_m2'] *
                 table_diffuse['primary']['solid_angle_thrown_sr']
@@ -225,26 +217,20 @@ for site_key in irf_config['config']['sites']:
                 'num_bins_above_threshold'],
             total_num_grid_cells=NUM_GRID_BINS,
         )
-
-        response[site_key][particle_key]['diffuse'] = {
-            "value": _q_eff,
-            "relative_uncertainty": _q_unc,
-            "unit": "m$^{2}$ sr",
-        }
-
-
-with open(os.path.join(pa['out_dir'], 'acceptance_trigger_in_onregion.json'), 'wt') as f:
-    f.write(
-        json.dumps(
-            {
-                "energy_bin_edges": {
-                    "value": energy_bin_edges,
-                    "unit": "GeV"
-                },
-                "cosmic_response": response,
-                "onregion": onregion68,
-            },
-            indent=4,
-            cls=irf.json_numpy.Encoder
+        _q_eff = (
+            _q_eff_all_possible_onregion *
+            actual_onregion_over_possible_onregion_factor
         )
-    )
+
+        irf.json_numpy.write(
+            os.path.join(site_particle_dir, "diffuse.json"),
+            {
+                "comment": (
+                    "Effective acceptance (area x solid angle) "
+                    "for a diffuse source, reconstructed in onregion. "
+                    "VS energy-bins"),
+                "unit": "m$^{2}$ sr",
+                "mean": _q_eff,
+                "relative_uncertainty": _q_unc,
+            }
+        )
