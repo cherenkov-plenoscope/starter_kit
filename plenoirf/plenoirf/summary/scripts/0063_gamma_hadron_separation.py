86d2ac8dd9d68d9561dd802ec033a0b089491f0f
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-08-26 21:04:06 +0200

    export gammaness in test-set

--- plenoirf/plenoirf/summary/scripts/0063_gamma_hadron_separation.py
+++ plenoirf/plenoirf/summary/scripts/0063_gamma_hadron_separation.py
@@ -45,6 +45,10 @@ random_seed = sum_config["random_seed"]
 PARTICLES = irf_config["config"]["particles"]
 SITES = irf_config["config"]["sites"]
 
+PARTICLE_IDS = {}
+for pk in PARTICLES:
+    PARTICLE_IDS[PARTICLES[pk]["particle_id"]] = pk
+
 fig_16_by_9 = sum_config["plot"]["16_by_9"]
 fig_1_by_1 = fig_16_by_9.copy()
 fig_1_by_1["rows"] = fig_16_by_9["rows"] * (16 / 9)
@@ -128,19 +132,22 @@ for sk in SITES:
         alpha=1e-2,
         hidden_layer_sizes=(num_features, num_features, num_features),
         random_state=random_seed,
-        verbose=True,
+        verbose=False,
         max_iter=5000,
         learning_rate_init=0.1,
     )
     models["RandomForest"] = sklearn.ensemble.RandomForestRegressor(
-        random_state=random_seed
+        random_state=random_seed, n_estimators=10,
     )
 
     _X_shuffle, _y_shuffle = sklearn.utils.shuffle(
         MA["train"]["x"], MA["train"]["y"], random_state=random_seed
     )
 
+    gammaness = {}
+    area_under_curve = {}
     for mk in models:
+        gammaness[mk] = {}
         models[mk].fit(_X_shuffle, _y_shuffle)
 
         model_gh_path = os.path.join(pa["out_dir"], mk + "_gamma_hadron_model")
@@ -155,7 +162,7 @@ for sk in SITES:
             y_true=y_gammaness_true, y_score=y_gammaness_score
         )
 
-        auc_gh = sklearn.metrics.roc_auc_score(
+        area_under_curve[mk] = sklearn.metrics.roc_auc_score(
             y_true=y_gammaness_true, y_score=y_gammaness_score
         )
 
@@ -163,9 +170,21 @@ for sk in SITES:
             "false_positive_rate": fpr_gh.tolist(),
             "true_positive_rate": tpr_gh.tolist(),
             "gamma_hadron_threshold": thresholds_gh.tolist(),
-            "area_under_curve": float(auc_gh),
+            "area_under_curve": float(area_under_curve[mk]),
             "num_events_for_training": int(MA["train"]["x"].shape[0]),
         }
+
+        for pk in PARTICLES:
+            gammaness[mk][pk] = {}
+            particle_mask = (
+                ET["test"]["primary.particle_id"].values
+                == PARTICLES[pk]["particle_id"]
+            )
+            gammaness[mk][pk][spt.IDX] = (ET["test"][spt.IDX].values)[
+                particle_mask
+            ]
+            gammaness[mk][pk]["gammaness"] = y_gammaness_score[particle_mask]
+
         roc_gh_path = os.path.join(pa["out_dir"], sk + "_" + mk + "_roc")
         with open(roc_gh_path + ".json", "wt") as fout:
             fout.write(json.dumps(roc_gh, indent=4))
@@ -173,8 +192,21 @@ for sk in SITES:
         fig = irf.summary.figure.figure(fig_1_by_1)
         ax = irf.summary.figure.add_axes(fig, [0.15, 0.15, 0.8, 0.8])
         ax.plot(fpr_gh, tpr_gh, "k")
-        ax.set_title("area under curve {:.2f}".format(auc_gh))
+        ax.set_title("area under curve {:.2f}".format(area_under_curve[mk]))
         ax.set_xlabel("false positive rate / 1\nproton acceptance")
         ax.set_ylabel("true positive rate / 1\ngamma-ray acceptance")
         plt.savefig(roc_gh_path + ".png")
         plt.close("all")
+
+    _methods = [mk for mk in area_under_curve]
+    _areas = [area_under_curve[mk] for mk in area_under_curve]
+    best_method = _methods[np.argmax(_areas)]
+
+    for pk in PARTICLES:
+        site_particle_dir = os.path.join(pa["out_dir"], sk, pk)
+        os.makedirs(site_particle_dir, exist_ok=True)
+
+        irf.json_numpy.write(
+            os.path.join(site_particle_dir, "test.json"),
+            gammaness[best_method][pk],
+        )
