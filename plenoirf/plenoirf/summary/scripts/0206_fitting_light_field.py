30efec3eba82a878ac56069751fdf29b52872a34
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-09-24 12:41:21 +0200

    fit likelihood

---
+++ plenoirf/plenoirf/summary/scripts/0206_fitting_light_field.py
@@ -0,0 +1,241 @@
+#!/usr/bin/python
+import sys
+import plenoirf as irf
+import sparse_numeric_table as spt
+import airshower_template_generator as atg
+import os
+import plenopy as pl
+from iminuit import Minuit
+import pandas
+
+import matplotlib
+
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+
+argv = irf.summary.argv_since_py(sys.argv)
+pa = irf.summary.paths_from_argv(argv)
+
+irf_config = irf.summary.read_instrument_response_config(run_dir=pa["run_dir"])
+sum_config = irf.summary.read_summary_config(summary_dir=pa["summary_dir"])
+
+_passed_trigger_indices = irf.json_numpy.read_tree(
+    os.path.join(pa["summary_dir"], "0066_passing_trigger")
+)
+
+passed_trigger_idx_sets = {}
+for sk in irf_config["config"]["sites"]:
+    passed_trigger_idx_sets[sk] = {}
+    for pk in irf_config["config"]["particles"]:
+        passed_trigger_idx_sets[sk][pk] = set(
+            _passed_trigger_indices[sk][pk]["passed_trigger"][spt.IDX]
+        )
+
+lfg = pl.LightFieldGeometry(
+    os.path.join(pa["run_dir"], "light_field_geometry")
+)
+image_rays = pl.image.ImageRays(lfg)
+
+
+fov_radius_deg = (
+    0.5 * irf_config["light_field_sensor_geometry"]["max_FoV_diameter_deg"]
+)
+
+max_cxcy_radius = 1.5 * fov_radius_deg
+max_core_radius = 1e3
+
+
+# FIT MODEL
+# =========
+
+
+def gaussian_bell(mu, sigma, x):
+    # norm = 1/(sigma * np.sqrt(2 * np.pi))
+    norm = 1.0
+    return norm * np.exp(-((x - mu) ** 2) / (2 * sigma ** 2))
+
+
+def model_distance_to_main_axis(c_para, c_perp, perp_distance_threshold):
+    """
+    ^ w
+    |
+   1|-
+    | \
+    |  \
+    |   \
+    o----|-------> c_perp
+        thr
+    """
+    num = len(c_perp)
+    weights = gaussian_bell(mu=0.0, sigma=perp_distance_threshold, x=c_perp)
+    perp_weight = np.sum(weights) / num
+
+    return perp_weight
+
+
+def disc_potential_wall(r, r0, r1):
+    if r > r0:
+        return ((r - r0) / (r1 - r0)) ** 2
+    else:
+        return 0.0
+
+
+def _f(
+    source_cx, source_cy, core_x, core_y, light_field,
+):
+    WRT_DOWNWARDS = -1.0
+    c_para, c_perp = atg.project_light_field_onto_source_image(
+        cer_cx_rad=WRT_DOWNWARDS * light_field["cx"],
+        cer_cy_rad=WRT_DOWNWARDS * light_field["cy"],
+        cer_x_m=light_field["x"],
+        cer_y_m=light_field["y"],
+        primary_cx_rad=WRT_DOWNWARDS * source_cx,
+        primary_cy_rad=WRT_DOWNWARDS * source_cy,
+        primary_core_x_m=core_x,
+        primary_core_y_m=core_y,
+    )
+    w = model_distance_to_main_axis(
+        c_para=c_para, c_perp=c_perp, perp_distance_threshold=np.deg2rad(2.0)
+    )
+
+    return 1.0 - w
+
+
+class LightField:
+    def __init__(
+        self,
+        cx,
+        cy,
+        x,
+        y,
+        time_slice,
+        cxcy_radius_0,
+        cxcy_radius_1,
+        xy_radius_0,
+        xy_radius_1,
+    ):
+        self.cx = cx
+        self.cy = cy
+        self.x = x
+        self.y = y
+        self.time_slice = time_slice
+
+        self.cxcy_radius_0 = cxcy_radius_0
+        self.cxcy_radius_1 = cxcy_radius_1
+        self.xy_radius_0 = xy_radius_0
+        self.xy_radius_1 = xy_radius_1
+
+        assert self.cxcy_radius_0 > 0.0
+        assert self.cxcy_radius_1 > self.cxcy_radius_0
+
+        assert self.xy_radius_0 > 0.0
+        assert self.xy_radius_1 > self.xy_radius_0
+
+    def model(self, source_cx, source_cy, core_x, core_y):
+        reppelling_source = disc_potential_wall(
+            r=np.hypot(source_cx, source_cy),
+            r0=self.cxcy_radius_0,
+            r1=self.cxcy_radius_1,
+        )
+
+        reppelling_core = disc_potential_wall(
+            r=np.hypot(core_x, core_y),
+            r0=self.xy_radius_0,
+            r1=self.xy_radius_1,
+        )
+
+        w = _f(
+            source_cx,
+            source_cy,
+            core_x,
+            core_y,
+            light_field={
+                "cx": self.cx,
+                "cy": self.cy,
+                "x": self.x,
+                "y": self.y,
+            },
+        )
+
+        return w + reppelling_source + reppelling_core
+
+
+for sk in irf_config["config"]["sites"]:
+    for pk in ["gamma"]:  # irf_config["config"]["particles"]:
+
+        run = pl.photon_stream.loph.LopfTarReader(
+            os.path.join(
+                pa["run_dir"], "event_table", sk, pk, "cherenkov.phs.loph.tar",
+            )
+        )
+
+        reco = []
+        for event in run:
+            airshower_id, phs = event
+            lixel_ids = phs["photons"]["channels"]
+            num_reconstructed_photons = lixel_ids.shape[0]
+
+            if airshower_id not in passed_trigger_idx_sets[sk][pk]:
+                continue
+
+            if (
+                num_reconstructed_photons
+                < sum_config["quality"]["min_reconstructed_photons"]
+            ):
+                continue
+
+            lf = LightField(
+                cx=lfg.cx_mean[lixel_ids],
+                cy=lfg.cy_mean[lixel_ids],
+                x=lfg.x_mean[lixel_ids],
+                y=lfg.y_mean[lixel_ids],
+                time_slice=phs["photons"]["arrival_time_slices"],
+                cxcy_radius_0=np.deg2rad(max_cxcy_radius),
+                cxcy_radius_1=np.deg2rad(1.1 * max_cxcy_radius),
+                xy_radius_0=max_core_radius,
+                xy_radius_1=1.1 * max_core_radius,
+            )
+
+            guess_cx = np.median(lf.cx)
+            guess_cy = np.median(lf.cy)
+            scan_cr = np.deg2rad(1.1 * max_cxcy_radius)
+            guess_x = 0.0
+            guess_y = 0.0
+            scan_r = 1.1 * max_core_radius
+
+            mm = Minuit(
+                fcn=lf.model,
+                source_cx=guess_cx,
+                limit_source_cx=(guess_cx - scan_cr, guess_cx + scan_cr),
+                source_cy=guess_cy,
+                limit_source_cy=(guess_cy - scan_cr, guess_cy + scan_cr),
+                core_x=guess_x,
+                limit_core_x=(guess_x - scan_r, guess_x + scan_r),
+                core_y=guess_y,
+                limit_core_y=(guess_y - scan_r, guess_y + scan_r),
+                print_level=0,
+                errordef=Minuit.LEAST_SQUARES,
+            )
+            mm.migrad()
+
+            reco.append(
+                {
+                    spt.IDX: airshower_id,
+                    "cx": mm.values["source_cx"],
+                    "cy": mm.values["source_cy"],
+                    "x": mm.values["core_x"],
+                    "y": mm.values["core_y"],
+                }
+            )
+            print(airshower_id, num_reconstructed_photons, mm.values["source_cx"], mm.values["source_cy"])
+        reco_df = pandas.DataFrame(reco)
+        reco_di = reco_df.to_dict(orient="list")
+
+        site_particle_dir = os.path.join(pa["out_dir"], sk, pk)
+        os.makedirs(site_particle_dir, exist_ok=True)
+
+        irf.json_numpy.write(
+            path=os.path.join(site_particle_dir, "reco" + ".json"),
+            out_dict=reco_di,
+        )
---
+++ plenoirf/plenoirf/summary/scripts/0208_fitting_light_field_benchmark.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+import sys
+import plenoirf as irf
+import sparse_numeric_table as spt
+import airshower_template_generator as atg
+import os
+import pandas
+import plenopy as pl
+from iminuit import Minuit
+
+import matplotlib
+
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+
+argv = irf.summary.argv_since_py(sys.argv)
+pa = irf.summary.paths_from_argv(argv)
+
+irf_config = irf.summary.read_instrument_response_config(run_dir=pa["run_dir"])
+sum_config = irf.summary.read_summary_config(summary_dir=pa["summary_dir"])
+
+os.makedirs(pa["out_dir"], exist_ok=True)
+
+_passed_trigger_indices = irf.json_numpy.read_tree(
+    os.path.join(pa["summary_dir"], "0066_passing_trigger")
+)
+
+fig_16_by_9 = sum_config["plot"]["16_by_9"]
+particle_colors = sum_config["plot"]["particle_colors"]
+
+theta_square_bin_edges_deg2 = np.linspace(
+    0,
+    sum_config["point_spread_function"]["theta_square"]["max_angle_deg"] ** 2,
+    sum_config["point_spread_function"]["theta_square"]["num_bins"] * 2,
+)
+psf_containment_factor = sum_config["point_spread_function"][
+    "containment_factor"
+]
+
+# READ reconstruction
+# ===================
+_rec = irf.json_numpy.read_tree(
+    os.path.join(pa["summary_dir"], "0206_fitting_light_field")
+)
+reconstruction = {}
+for sk in _rec:
+    reconstruction[sk] = {}
+    for pk in _rec[sk]:
+        _df = pandas.DataFrame(_rec[sk][pk]["reco"])
+        reconstruction[sk][pk] = _df.to_records(index=False)
+
+for sk in reconstruction:
+    for pk in reconstruction[sk]:
+
+        event_table = spt.read(
+            path=os.path.join(
+                pa["run_dir"], "event_table", sk, pk, "event_table.tar"
+            ),
+            structure=irf.table.STRUCTURE,
+        )
+        all_truth = spt.cut_table_on_indices(
+            event_table,
+            irf.table.STRUCTURE,
+            common_indices=reconstruction[sk][pk][spt.IDX],
+            level_keys=[
+                "primary",
+                "cherenkovsize",
+                "grid",
+                "cherenkovpool",
+                "cherenkovsizepart",
+                "cherenkovpoolpart",
+                "core",
+                "trigger",
+                "pasttrigger",
+                "cherenkovclassification",
+            ],
+        )
+        all_truth = spt.sort_table_on_common_indices(
+            table=all_truth, common_indices=reconstruction[sk][pk][spt.IDX]
+        )
+        all_reco = reconstruction[sk][pk]
+        (
+            true_cx,
+            true_cy,
+        ) = irf.analysis.gamma_direction.momentum_to_cx_cy_wrt_aperture(
+            primary=all_truth["primary"],
+            plenoscope_pointing=irf_config["config"]["plenoscope_pointing"],
+        )
+        true_x = -all_truth["core"]["core_x_m"]
+        true_y = -all_truth["core"]["core_y_m"]
+
+
+        delta_c_deg = np.rad2deg(
+            np.hypot(all_reco["cx"] - true_cx, all_reco["cy"] - true_cy)
+        )
+
+        delta_hist = np.histogram(
+            delta_c_deg ** 2,
+            bins=theta_square_bin_edges_deg2
+        )[0]
+
+
+        theta_square_deg2 = irf.analysis.gamma_direction.integration_width_for_containment(
+            bin_counts=delta_hist,
+            bin_edges=theta_square_bin_edges_deg2,
+            containment=psf_containment_factor,
+        )
+
+        fig = irf.summary.figure.figure(fig_16_by_9)
+        ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+        irf.summary.figure.ax_add_hist(
+            ax=ax,
+            bin_edges=theta_square_bin_edges_deg2,
+            bincounts=delta_hist,
+            linestyle="-",
+            linecolor="k",
+            bincounts_upper=None,
+            bincounts_lower=None,
+            face_color="k",
+            face_alpha=0.3,
+        )
+        ax.semilogy()
+        ax.axvline(x=theta_square_deg2, color="k", linestyle="--", alpha=0.75)
+        ax.set_xlabel("delta c**2 / deg**2")
+        ax.set_ylabel("intensity / 1")
+        ax.grid(color="k", linestyle="-", linewidth=0.66, alpha=0.1)
+        ax.spines["top"].set_color("none")
+        ax.spines["right"].set_color("none")
+        fig.savefig(
+            os.path.join(pa["out_dir"], sk + "_" + pk + "_benchmark" + ".jpg")
+        )
+        plt.close(fig)
