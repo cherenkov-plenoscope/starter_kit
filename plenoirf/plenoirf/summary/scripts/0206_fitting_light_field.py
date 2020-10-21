11fb8f7393d4b92fe95ae734b8ae7d15cc8fc365
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-10-21 16:02:57 +0200

    latest bell model

--- plenoirf/plenoirf/summary/scripts/0206_fitting_light_field.py
+++ plenoirf/plenoirf/summary/scripts/0206_fitting_light_field.py
@@ -18,6 +18,32 @@ pa = irf.summary.paths_from_argv(argv)
 irf_config = irf.summary.read_instrument_response_config(run_dir=pa["run_dir"])
 sum_config = irf.summary.read_summary_config(summary_dir=pa["summary_dir"])
 
+bell_model_lut_path = "2020-09-26_gamma_lut/bell_model/bell_model.json"
+
+def read_energy_altitude_guess(
+    site_key,
+    particle_key,
+    path=os.path.join(
+        pa["summary_dir"], "0065_learning_airshower_maximim_and_energy"
+    ),
+):
+    tt = irf.json_numpy.read_tree(path)
+    guess_energy = {}
+    guess_altitude = {}
+
+    sk_alt = tt[site_key][particle_key]["airshower_maximum"]
+    num_alt = len(sk_alt["idx"])
+    for i in range(num_alt):
+        guess_altitude[sk_alt["idx"][i]] = sk_alt["airshower_maximum"][i]
+
+    sk_ene = tt[site_key][particle_key]["energy"]
+    num_ene = len(sk_ene["idx"])
+    for i in range(num_ene):
+        guess_energy[sk_ene["idx"][i]] = sk_ene["energy"][i]
+
+    return guess_energy, guess_altitude
+
+
 _passed_trigger_indices = irf.json_numpy.read_tree(
     os.path.join(pa["summary_dir"], "0066_passing_trigger")
 )
@@ -75,7 +101,7 @@ def _f(
     source_cx, source_cy, core_x, core_y, light_field,
 ):
     WRT_DOWNWARDS = -1.0
-    c_para, c_perp = atg.project_light_field_onto_source_image(
+    c_para, c_perp = atg.projection.project_light_field_onto_source_image(
         cer_cx_rad=WRT_DOWNWARDS * light_field["cx"],
         cer_cy_rad=WRT_DOWNWARDS * light_field["cy"],
         cer_x_m=light_field["x"],
@@ -151,14 +177,17 @@ class LightField:
         return w + reppelling_source + reppelling_core
 
 
-def make_jobs(loph_chunk_dir, quality, limits):
+def make_jobs(loph_chunk_dir, quality, limits, bell_model_lut_path, site_key, particle_key):
     chunk_paths = glob.glob(os.path.join(loph_chunk_dir, "*.tar"))
     jobs = []
     for chunk_path in chunk_paths:
         job = {}
         job["loph_path"] = str(chunk_path)
+        job["bell_model_lut_path"] = str(bell_model_lut_path)
         job["quality"] = dict(quality)
         job["limits"] = dict(limits)
+        job["site_key"] = str(site_key)
+        job["particle_key"] = str(particle_key)
 
         jobs.append(job)
     return jobs
@@ -167,12 +196,19 @@ def make_jobs(loph_chunk_dir, quality, limits):
 def run_job(job):
     run = pl.photon_stream.loph.LopfTarReader(job["loph_path"])
     limits = job["limits"]
+    bell_model_lut = atg.model.read_bell_model_lut(job["bell_model_lut_path"])
+
+    guess_energy, guess_altitude = read_energy_altitude_guess(
+        site_key=job["site_key"],
+        particle_key=job["particle_key"]
+    )
+    INSTRUMENT_RADIUS = 40.0
 
     result = []
     for event in run:
-        airshower_id, phs = event
-        lixel_ids = phs["photons"]["channels"]
-        num_reconstructed_photons = lixel_ids.shape[0]
+        airshower_id, loph_record = event
+        print("airshower_id", airshower_id)
+        num_reconstructed_photons = loph_record["photons"]["channels"].shape[0]
 
         if (
             num_reconstructed_photons
@@ -180,6 +216,81 @@ def run_job(job):
         ):
             continue
 
+        slf = atg.model.SplitLightField(
+            loph_record=loph_record, light_field_geometry=lfg
+        )
+
+        if slf.number_photons  < 200:
+            print("skip ", airshower_id)
+            continue
+
+
+        try:
+            print("guess_energy", guess_energy[airshower_id], "GeV")
+            print("guess_altitude", guess_altitude[airshower_id]*1e-3, "km")
+            print("num cherenkov ", slf.number_photons, "p.e.")
+
+            bell_fit = atg.model.BellLightFieldFitter(
+                bell_model_lut=bell_model_lut,
+                split_light_field=slf,
+                energy_GeV=guess_energy[airshower_id],
+                altitude_m=guess_altitude[airshower_id],
+            )
+
+            max_core_radius = bell_fit.max_core_radius()
+            print("max_core_radius ", max_core_radius, "m")
+
+            error_cxcy = np.deg2rad(0.1)
+            error_xy = 5.0
+            guess_cx = slf.median_cx
+            guess_cy = slf.median_cy
+            scan_cr = np.deg2rad(1.1 * limits["max_cxcy_radius"])
+            scan_r = 0.7 * max_core_radius - INSTRUMENT_RADIUS
+            scan_r = np.max([0.0, scan_r])
+
+            guess_x = 0.5*scan_r
+            guess_y = 0.5*scan_r
+
+            print("max scan core radius ", scan_r, "m")
+
+            mm = Minuit(
+                fcn=bell_fit.model,
+                source_cx=guess_cx,
+                error_source_cx=error_cxcy,
+                limit_source_cx=(guess_cx - scan_cr, guess_cx + scan_cr),
+                source_cy=guess_cy,
+                error_source_cy=error_cxcy,
+                limit_source_cy=(guess_cy - scan_cr, guess_cy + scan_cr),
+                core_x=guess_x,
+                error_core_x=error_xy,
+                limit_core_x=(-scan_r, scan_r),
+                core_y=guess_y,
+                error_core_y=error_xy,
+                limit_core_y=(-scan_r, scan_r),
+                print_level=0,
+                errordef=Minuit.LIKELIHOOD,
+            )
+            mm.migrad()
+
+            reco = {
+                spt.IDX: airshower_id,
+                "cx": mm.values["source_cx"],
+                "cy": mm.values["source_cy"],
+                "x": mm.values["core_x"],
+                "y": mm.values["core_y"],
+                "fval": mm.fval,
+            }
+            print(reco)
+            result.append(reco)
+
+        except AssertionError as e:
+            print(e)
+            print("failed airshower_id", airshower_id)
+        except KeyError as e:
+            print(e)
+            print("failed airshower_id", airshower_id)
+
+        """
         lf = LightField(
             cx=lfg.cx_mean[lixel_ids],
             cy=lfg.cy_mean[lixel_ids],
@@ -219,16 +330,9 @@ def run_job(job):
             errordef=Minuit.LEAST_SQUARES,
         )
         mm.migrad()
+        """
 
-        result.append(
-            {
-                spt.IDX: airshower_id,
-                "cx": mm.values["source_cx"],
-                "cy": mm.values["source_cy"],
-                "x": mm.values["core_x"],
-                "y": mm.values["core_y"],
-            }
-        )
+
     return result
 
 
@@ -263,14 +367,26 @@ for sk in ["namibia"]:  # irf_config["config"]["sites"]:
 
         result_path = os.path.join(site_particle_dir, "reco" + ".json")
         if not os.path.exists(result_path):
+            print("make jobs")
             jobs = make_jobs(
                 loph_chunk_dir=loph_chunk_dir,
+                bell_model_lut_path=bell_model_lut_path,
                 quality=sum_config["quality"],
                 limits=fit_limits,
+                site_key=sk,
+                particle_key=pk,
             )
-
+            # limit jobs
+            jobs = jobs[0:1]
+            """
             pool = multiprocessing.Pool(8)
             _results = pool.map(run_job, jobs)
+            """
+            _results = []
+            for job in jobs:
+                print("job", job)
+                _results.append(run_job(job))
+
             results = []
             for chunk in _results:
                 for r in chunk:
