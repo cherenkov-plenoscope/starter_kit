c2ccbda0573011ab7e3910dccfbc5f6817807bfc
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-10-22 21:13:29 +0200

    do not filter and split in 0211, use prepared set instead

--- plenoirf/plenoirf/summary/scripts/0211_simple_light_field.py
+++ plenoirf/plenoirf/summary/scripts/0211_simple_light_field.py
@@ -10,6 +10,7 @@ import plenopy as pl
 import glob
 import multiprocessing
 
+
 PARALLEL = True
 
 argv = irf.summary.argv_since_py(sys.argv)
@@ -18,22 +19,14 @@ pa = irf.summary.paths_from_argv(argv)
 irf_config = irf.summary.read_instrument_response_config(run_dir=pa["run_dir"])
 sum_config = irf.summary.read_summary_config(summary_dir=pa["summary_dir"])
 
-_passed_trigger_indices = irf.json_numpy.read_tree(
-    os.path.join(pa["summary_dir"], "0066_passing_trigger")
-)
-
-passed_trigger_idx_sets = {}
-for sk in irf_config["config"]["sites"]:
-    passed_trigger_idx_sets[sk] = {}
-    for pk in irf_config["config"]["particles"]:
-        passed_trigger_idx_sets[sk][pk] = set(
-            _passed_trigger_indices[sk][pk]["passed_trigger"][spt.IDX]
-        )
-
 lfg = pl.LightFieldGeometry(
     os.path.join(pa["run_dir"], "light_field_geometry")
 )
 
+loph_chunk_base_dir = os.path.join(
+    pa["summary_dir"],
+    "0068_prepare_loph_passed_trigger_and_quality"
+)
 
 def make_jobs(loph_chunk_dir, quality, site_key, particle_key):
     chunk_paths = glob.glob(os.path.join(loph_chunk_dir, "*.tar"))
@@ -55,13 +48,6 @@ def run_job(job):
     for event in run:
         airshower_id, loph_record = event
         print("airshower_id", airshower_id)
-        num_reconstructed_photons = loph_record["photons"]["channels"].shape[0]
-
-        if (
-            num_reconstructed_photons
-            < job["quality"]["min_reconstructed_photons"]
-        ):
-            continue
 
         slf = atg.model.SplitLightField(
             loph_record=loph_record, light_field_geometry=lfg
@@ -82,69 +68,40 @@ def run_job(job):
     return result
 
 
-for sk in ["namibia"]:  # irf_config["config"]["sites"]:
+for sk in irf_config["config"]["sites"]:
     for pk in ["gamma"]:  # irf_config["config"]["particles"]:
+
         site_particle_dir = os.path.join(pa["out_dir"], sk, pk)
         os.makedirs(site_particle_dir, exist_ok=True)
 
-        raw_loph_run = os.path.join(
-            pa["run_dir"], "event_table", sk, pk, "cherenkov.phs.loph.tar",
+        print("make jobs")
+        jobs = make_jobs(
+            loph_chunk_dir=os.path.join(loph_chunk_base_dir, sk, pk, "chunks"),
+            quality=sum_config["quality"],
+            site_key=sk,
+            particle_key=pk,
         )
 
-        loph_run_passed_trigger = os.path.join(
-            pa["out_dir"], sk, pk, "passed_trigger_cherenkov.phs.loph.tar",
-        )
-
-        print("filter events passed tritgger")
-        if not os.path.exists(loph_run_passed_trigger):
-            pl.photon_stream.loph.read_filter_write(
-                in_path=raw_loph_run,
-                out_path=loph_run_passed_trigger,
-                identity_set=passed_trigger_idx_sets[sk][pk],
-            )
-
-        print("split workload into chunks")
-        loph_chunk_dir = os.path.join(pa["out_dir"], sk, pk, "loph_chunks")
-        if not os.path.exists(loph_chunk_dir):
-            pl.photon_stream.loph.split_into_chunks(
-                loph_path=loph_run_passed_trigger,
-                out_dir=loph_chunk_dir,
-                chunk_prefix="chunk_",
-                num_events_in_chunk=256,
-            )
-
-        print("reconstruct")
-        result_path = os.path.join(site_particle_dir, "reco" + ".json")
-        if not os.path.exists(result_path):
-            print("make jobs")
-            jobs = make_jobs(
-                loph_chunk_dir=loph_chunk_dir,
-                quality=sum_config["quality"],
-                site_key=sk,
-                particle_key=pk,
-            )
-
-            print("run jobs")
-            if PARALLEL:
-                print("PARALLEL")
-                pool = multiprocessing.Pool(8)
-                _results = pool.map(run_job, jobs)
-            else:
-                print("SEQUENTIEL")
-                _results = []
-                for job in jobs:
-                    print("job", job)
-                    _results.append(run_job(job))
-            results = []
-            for chunk in _results:
-                for r in chunk:
-                    results.append(r)
-
-            reco_df = pandas.DataFrame(results)
-            reco_di = reco_df.to_dict(orient="list")
-
-            irf.json_numpy.write(
-                path=result_path, out_dict=reco_di,
-            )
+        print("run jobs")
+        if PARALLEL:
+            print("PARALLEL")
+            pool = multiprocessing.Pool(8)
+            _results = pool.map(run_job, jobs)
         else:
-            reco_di = irf.json_numpy.read(result_path)
+            print("SEQUENTIEL")
+            _results = []
+            for job in jobs:
+                print("job", job)
+                _results.append(run_job(job))
+        results = []
+        for chunk in _results:
+            for r in chunk:
+                results.append(r)
+
+        reco_df = pandas.DataFrame(results)
+        reco_di = reco_df.to_dict(orient="list")
+
+        irf.json_numpy.write(
+            path=os.path.join(site_particle_dir, "reco" + ".json"),
+            out_dict=reco_di,
+        )
