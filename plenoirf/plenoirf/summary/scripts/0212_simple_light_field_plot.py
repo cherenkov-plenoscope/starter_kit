a03a17b3da00014d914764a8ca0e80c652aa9589
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-11-24 14:00:18 +0100

    put fuzzy reconstruction into plenoy

--- plenoirf/plenoirf/summary/scripts/0212_simple_light_field_plot.py
+++ plenoirf/plenoirf/summary/scripts/0212_simple_light_field_plot.py
@@ -3,11 +3,11 @@ import sys
 import numpy as np
 import plenoirf as irf
 import sparse_numeric_table as spt
-import airshower_template_generator as atg
 import os
 import pandas
 import plenopy as pl
 import glob
+import scipy
 
 import matplotlib
 
@@ -39,9 +39,11 @@ fov_radius_deg = np.rad2deg(
     0.5 * lfg.sensor_plane2imaging_system.max_FoV_diameter
 )
 
-ib = atg.model.IMAGE_BINNING
-c_bin_edges = np.linspace(
-    -ib["radius_deg"], ib["radius_deg"], ib["num_bins"] + 1
+fuzzy_binning = pl.fuzzy.direction.EXAMPLE_IMAGE_BINNING
+fuzzy_c_bin_edges = np.linspace(
+    -fuzzy_binning["radius_deg"],
+    fuzzy_binning["radius_deg"],
+    fuzzy_binning["num_bins"] + 1
 )
 
 fig_16_by_9 = sum_config["plot"]["16_by_9"]
@@ -102,6 +104,12 @@ for sk in irf_config["config"]["sites"]:
                 "y": -all_truth["core"]["core_y_m"][ii],
             }
 
+fuzz_ring_gaussian_kernel = pl.fuzzy.discrete_kernel.gauss1d(num_steps=61)
+fuzz_img_gaussian_kernel = pl.fuzzy.discrete_kernel.gauss2d(num_steps=5)
+
+scale = 1.5
+
+fuzzy_model_config = pl.fuzzy.direction.EXAMPLE_MODEL_CONFIG
 
 for sk in irf_config["config"]["sites"]:
     for pk in ["gamma"]:  # irf_config["config"]["particles"]:
@@ -125,19 +133,30 @@ for sk in irf_config["config"]["sites"]:
             true_x = truth_by_index[sk][pk][airshower_id]["x"]
             true_y = truth_by_index[sk][pk][airshower_id]["y"]
 
-            slf = atg.model.SplitLightField(
+            slf = pl.fuzzy.direction.SplitLightField(
                 loph_record=loph, light_field_geometry=lfg
             )
-            slf_model = atg.model.estimate_model_from_light_field(
-                split_light_field=slf, model_config=atg.model.MODEL_CONFIG
+
+            if slf.number_photons < 150:
+                continue
+
+            slf_model = pl.fuzzy.direction.estimate_model_from_light_field(
+                split_light_field=slf, model_config=fuzzy_model_config
             )
-            img = atg.model.make_image_from_model(
+            fuzz_img = pl.fuzzy.direction.make_image_from_model(
                 light_field_model=slf_model,
-                model_config=atg.model.MODEL_CONFIG,
-                image_binning=ib
+                model_config=fuzzy_model_config,
+                image_binning=fuzzy_binning
             )
-            reco_cx_deg, reco_cy_deg = atg.model.argmax_image_cx_cy_deg(
-                image=img, image_binning=ib
+
+            smooth_fuzz_img = scipy.signal.convolve2d(
+                in1=fuzz_img,
+                in2=fuzz_img_gaussian_kernel,
+                mode="same"
+            )
+
+            reco_cx_deg, reco_cy_deg = pl.fuzzy.direction.argmax_image_cx_cy_deg(
+                image=smooth_fuzz_img, image_binning=fuzzy_binning,
             )
             med_cx_deg = np.rad2deg(slf.median_cx)
             med_cy_deg = np.rad2deg(slf.median_cy)
@@ -146,16 +165,53 @@ for sk in irf_config["config"]["sites"]:
                 (reco_cx_deg - med_cx_deg),
             )
 
+            ring = pl.fuzzy.direction.project_image_onto_ring(
+                image=smooth_fuzz_img,
+                image_binning=fuzzy_binning,
+                ring_cx_deg=med_cx_deg,
+                ring_cy_deg=med_cy_deg,
+                ring_radius_deg=1.5
+            )
+            smooth_ring = pl.fuzzy.direction.circular_convolve1d(
+                in1=ring, in2=fuzz_ring_gaussian_kernel
+            )
+            smooth_ring /= np.max(smooth_ring)
 
-            model_pe = [m["num_photons_pe"] for m in slf_model]
-            model_az = [m["azimuth_rad"] for m in slf_model]
-            for ii in range(len(model_az)):
-                if model_az[ii] <= 0.0:
-                    model_az[ii] += np.pi
-                if model_az[ii] > np.pi:
-                    model_az[ii] -= np.pi
+            azi_maxima = pl.fuzzy.direction.circular_argmaxima(smooth_ring)
+            azi_maxima_weights = [smooth_ring[mm] for mm in azi_maxima]
 
-            scale = 1.5
+            azimuth_main2_axis = np.deg2rad(np.argmax(smooth_ring))
+
+            fuzzy_result = {
+                "median_cx_deg": med_cx_deg,
+                "median_cy_deg": med_cy_deg,
+                "main_axis_azimuth_deg": np.rad2deg(azimuth_main2_axis),
+                "reco_cx_deg": reco_cx_deg,
+                "reco_cy_deg": reco_cy_deg,
+            }
+
+            fig_ring = plt.figure(figsize=(16 / scale, 9 / scale), dpi=100 * scale)
+            ax_ring = fig_ring.add_axes([0.1, 0.1, 0.8, 0.8])
+            ax_ring.plot(smooth_ring, "k")
+            for yy in range(len(azi_maxima)):
+                ax_ring.plot(azi_maxima[yy], azi_maxima_weights[yy], "or")
+            ax_ring.set_xlim([0, 360])
+            ax_ring.set_ylim([0.0, 1.0])
+            ax_ring.set_xlabel("main-axis-azimuth / deg")
+            ax_ring.set_ylabel("probability density / deg$^{-1}$")
+            ax_ring.spines["top"].set_color("none")
+            ax_ring.spines["right"].set_color("none")
+            ax_ring.spines["bottom"].set_color("none")
+            ax_ring.spines["left"].set_color("none")
+            ax_ring.grid(color="k", linestyle="-", linewidth=0.66, alpha=0.1)
+            path_ring = os.path.join(
+                pa["out_dir"], sk, pk, "{:09d}_ring.jpg".format(airshower_id),
+            )
+            fig_ring.savefig(path_ring)
+            plt.close(fig_ring)
+
+
+
             fig = plt.figure(figsize=(16 / scale, 9 / scale), dpi=100 * scale)
             ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
             for pax in range(slf.number_paxel):
@@ -165,19 +221,21 @@ for sk in irf_config["config"]["sites"]:
                     "xb",
                     alpha=0.03,
                 )
-
-            ax.pcolor(c_bin_edges, c_bin_edges, img, cmap="Reds")
+            ax.pcolor(
+                fuzzy_c_bin_edges,
+                fuzzy_c_bin_edges,
+                smooth_fuzz_img,
+                cmap="Reds"
+            )
             phi = np.linspace(0, 2 * np.pi, 1000)
             ax.plot(
                 fov_radius_deg * np.cos(phi), fov_radius_deg * np.sin(phi), "k"
             )
-
             ax.plot(
-                [med_cx_deg, med_cx_deg + 100*np.cos(azimuth_main_axis)],
-                [med_cy_deg, med_cx_deg + 100*np.sin(azimuth_main_axis)],
-                ":k"
+                [med_cx_deg, med_cx_deg + 100*np.cos(azimuth_main2_axis)],
+                [med_cy_deg, med_cy_deg + 100*np.sin(azimuth_main2_axis)],
+                ":b"
             )
-
             ax.plot(reco_cx_deg, reco_cy_deg, "og")
             ax.plot(np.rad2deg(true_cx), np.rad2deg(true_cy), "xk")
 
