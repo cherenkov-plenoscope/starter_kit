68f9048fc38a53bb75654d42656d6d4cac9c36bc
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-03-22 16:46:14 +0100

    reduce the logging run

--- plenoirf/plenoirf/logging.py
+++ plenoirf/plenoirf/logging.py
@@ -43,43 +43,27 @@ class TimeDelta:
         return self.stop - self.start
 
 
-KEYS = [
-    "corsika",
-    "merlict",
-    "grid",
-    "prepare_trigger",
-    "trigger",
-    "cherenkov_classification",
-    "feature_extraction",
-    "past_trigger_gz_tar",
-    "export_event_table",
-    "export_grid_histograms",
-    "export_past_trigger",
-]
-
-
-def reduce(
-    list_of_log_paths, out_path, keys=KEYS,
-):
-    log_records = reduce_into_records(
-        list_of_log_paths=list_of_log_paths, keys=keys
-    )
+def reduce(list_of_log_paths, out_path):
+    log_records = reduce_into_records(list_of_log_paths=list_of_log_paths)
     log_df = pd.DataFrame(log_records)
-    log_df.to_csv(out_path + ".tmp", index=False)
+    log_df = log_df.set_index("run_id")
+    log_df = log_df.sort_index()
+    log_df.to_csv(out_path + ".tmp", index=False, na_rep="nan")
     shutil.move(out_path + ".tmp", out_path)
 
 
-def reduce_into_records(list_of_log_paths, keys=KEYS):
+def reduce_into_records(list_of_log_paths):
     list_of_log_records = []
     for log_path in list_of_log_paths:
         run_id = int(os.path.basename(log_path)[0:6])
         run = {"run_id": run_id}
-        for key in keys:
-            run[key] = None
+
         with open(log_path, "rt") as fin:
             for line in fin:
                 logline = json.loads(line)
-                if logline["msg"] in keys:
-                    run[logline["msg"]] = logline["delta"]
+                if "delta" in logline:
+                    name = logline["msg"].replace(":stop", "")
+                    run[name] = logline["delta"]
             list_of_log_records.append(run)
+
     return list_of_log_records
