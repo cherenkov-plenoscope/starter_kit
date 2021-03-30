511bd5c349e8fda997a81cf218ccc86ec448b082
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-03-30 17:38:57 +0200

    only accept passed trigger

--- plenoirf/plenoirf/summary/scripts/0211_simple_light_field.py
+++ plenoirf/plenoirf/summary/scripts/0211_simple_light_field.py
@@ -11,6 +11,10 @@ pa = irf.summary.paths_from_argv(argv)
 
 irf_config = irf.summary.read_instrument_response_config(run_dir=pa["run_dir"])
 
+passing_trigger = irf.json_numpy.read_tree(
+    os.path.join(pa["summary_dir"], "0066_passing_trigger")
+)
+
 for sk in irf_config["config"]["sites"]:
     for pk in irf_config["config"]["particles"]:
 
@@ -23,12 +27,21 @@ for sk in irf_config["config"]["sites"]:
             ),
             structure=irf.table.STRUCTURE,
         )
+
+        evt_tab_trg = spt.cut_table_on_indices(
+            table=event_table,
+            structure=irf.table.STRUCTURE,
+            common_indices=passing_trigger[sk][pk]["passed_trigger"][spt.IDX],
+            level_keys=["reconstructed_trajectory"]
+        )
+
         traj = {}
-        traj[spt.IDX] = event_table["reconstructed_trajectory"][spt.IDX]
-        traj["x"] = event_table["reconstructed_trajectory"]["x_m"]
-        traj["y"] = event_table["reconstructed_trajectory"]["y_m"]
-        traj["cx"] = event_table["reconstructed_trajectory"]["cx_rad"]
-        traj["cy"] = event_table["reconstructed_trajectory"]["cy_rad"]
+        traj[spt.IDX] = evt_tab_trg["reconstructed_trajectory"][spt.IDX]
+        traj["x"] = evt_tab_trg["reconstructed_trajectory"]["x_m"]
+        traj["y"] = evt_tab_trg["reconstructed_trajectory"]["y_m"]
+        traj["cx"] = evt_tab_trg["reconstructed_trajectory"]["cx_rad"]
+        traj["cy"] = evt_tab_trg["reconstructed_trajectory"]["cy_rad"]
+
         irf.json_numpy.write(
             path=os.path.join(site_particle_dir, "reco" + ".json"),
             out_dict=traj,
