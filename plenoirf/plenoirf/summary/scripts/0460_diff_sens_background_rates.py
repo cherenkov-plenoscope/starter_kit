9c620e37d20a41c2e51946fed5e38596be5858dd
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-08-25 15:45:50 +0200

    add non interpolated version

--- plenoirf/plenoirf/summary/scripts/0460_diff_sens_background_rates.py
+++ plenoirf/plenoirf/summary/scripts/0460_diff_sens_background_rates.py
@@ -25,25 +25,46 @@ num_onregion_sizes = len(
 )
 ONREGIONS = range(num_onregion_sizes)
 
-gk = "diffuse"
-
-ienergy_migration = json_numpy.read_tree(
-    os.path.join(pa["summary_dir"], "0420_interpolate_energy_migration_matrix")
+_energy_migration = json_numpy.read_tree(
+    os.path.join(pa["summary_dir"], "0066_energy_estimate_quality")
 )
 
-iacceptance = json_numpy.read_tree(
-    os.path.join(pa["summary_dir"], "0410_interpolate_acceptance")
+_acceptance = json_numpy.read_tree(
+    os.path.join(pa["summary_dir"], "0300_onregion_trigger_acceptance")
 )
 
 airshower_fluxes = json_numpy.read_tree(
-    os.path.join(pa["summary_dir"], "0015_flux_of_airshowers")
+    os.path.join(pa["summary_dir"], "0405_average_flux_of_airshowers")
 )
 
 energy_binning = json_numpy.read(
     os.path.join(pa["summary_dir"], "0005_common_binning", "energy.json")
 )
 energy_bin = energy_binning["trigger_acceptance_onregion"]
-fine_energy_bin = energy_binning["interpolation"]
+
+
+# prepare energy_migration
+# ------------------------
+energy_migration = {}
+for sk in SITES:
+    energy_migration[sk] = {}
+    for pk in COSMIC_RAYS:
+        energy_migration[sk][pk] = {}
+        M = _energy_migration[sk][pk]["confusion_matrix"]
+        dMdE = {}
+        dMdE["ax0_key"] = M["ax0_key"]
+        dMdE["ax1_key"] = M["ax1_key"]
+
+        counts = M["counts"]
+        counts_abs_unc = M["counts_abs_unc"]
+
+        dMdE["counts"], dMdE["counts_abs_unc"] = irf.analysis.differential_sensitivity.derive_migration_matrix_by_ax0(
+            migration_matrix_counts=M["counts_normalized_on_ax0"],
+            migration_matrix_counts_abs_unc=M["counts_normalized_on_ax0_abs_unc"],
+            ax0_bin_widths=energy_bin["width"],
+        )
+        energy_migration[sk][pk] = dMdE
+
 
 # prepare diff-flux
 # -----------------
@@ -51,9 +72,24 @@ diff_flux = {}
 for sk in SITES:
     diff_flux[sk] = {}
     for pk in COSMIC_RAYS:
-        diff_flux[sk][pk] = airshower_fluxes[sk][pk]["differential_flux"][
-            "values"
-        ]
+        diff_flux[sk][pk] = airshower_fluxes[sk][pk]["differential_flux"]
+
+
+gk = "diffuse"
+# prepare acceptance
+# ------------------
+acceptance = {}
+for sk in SITES:
+    acceptance[sk] = {}
+    for pk in COSMIC_RAYS:
+        acceptance[sk][pk] = {gk: {}}
+        Q = _acceptance[sk][pk][gk]["mean"]
+        Q_ru = _acceptance[sk][pk][gk]["relative_uncertainty"]
+        Q_ru[np.isnan(Q_ru)] = 0.0
+        Q_au = Q * Q_ru
+        acceptance[sk][pk][gk]["mean"] = Q
+        acceptance[sk][pk][gk]["absolute_uncertainty"] = Q_au
+
 
 dRtdEt = {}
 dRtdEt_au = {}
@@ -68,17 +104,17 @@ for sk in SITES:
     dRdE_au[sk] = {}
     for pk in COSMIC_RAYS:
         dRtdEt[sk][pk] = np.zeros(
-            (fine_energy_bin["num_bins"], num_onregion_sizes)
+            (energy_bin["num_bins"], num_onregion_sizes)
         )
         dRdE[sk][pk] = np.zeros(
-            (fine_energy_bin["num_bins"], num_onregion_sizes)
+            (energy_bin["num_bins"], num_onregion_sizes)
         )
 
         dRtdEt_au[sk][pk] = np.zeros(
-            (fine_energy_bin["num_bins"], num_onregion_sizes)
+            (energy_bin["num_bins"], num_onregion_sizes)
         )
         dRdE_au[sk][pk] = np.zeros(
-            (fine_energy_bin["num_bins"], num_onregion_sizes)
+            (energy_bin["num_bins"], num_onregion_sizes)
         )
 
         for ok in range(num_onregion_sizes):
@@ -86,20 +122,20 @@ for sk in SITES:
             dFdE = diff_flux[sk][pk]
             dFdE_au = np.zeros(dFdE.shape)
 
-            assert ienergy_migration[sk][pk]["ax0_key"] == "true_energy"
-            assert ienergy_migration[sk][pk]["ax1_key"] == "reco_energy"
-            dMdE = ienergy_migration[sk][pk]["counts"]
-            dMdE_au = ienergy_migration[sk][pk]["counts_abs_unc"]
+            assert energy_migration[sk][pk]["ax0_key"] == "true_energy"
+            assert energy_migration[sk][pk]["ax1_key"] == "reco_energy"
+            dMdE = energy_migration[sk][pk]["counts"]
+            dMdE_au = energy_migration[sk][pk]["counts_abs_unc"]
 
-            Q = iacceptance[sk][pk][gk]["mean"][:, ok]
-            Q_au = iacceptance[sk][pk][gk]["absolute_uncertainty"][:, ok]
+            Q = acceptance[sk][pk][gk]["mean"][:, ok]
+            Q_au = acceptance[sk][pk][gk]["absolute_uncertainty"][:, ok]
 
-            fine_energy_bin_width_au = np.zeros(fine_energy_bin["width"].shape)
+            energy_bin_width_au = np.zeros(energy_bin["width"].shape)
 
-            for ereco in range(fine_energy_bin["num_bins"]):
-                _P = np.zeros(fine_energy_bin["num_bins"])
-                _P_au = np.zeros(fine_energy_bin["num_bins"])
-                for etrue in range(fine_energy_bin["num_bins"]):
+            for ereco in range(energy_bin["num_bins"]):
+                _P = np.zeros(energy_bin["num_bins"])
+                _P_au = np.zeros(energy_bin["num_bins"])
+                for etrue in range(energy_bin["num_bins"]):
 
                     (
                         _P[etrue],
@@ -109,13 +145,13 @@ for sk in SITES:
                             dFdE[etrue],
                             dMdE[etrue, ereco],
                             Q[etrue],
-                            fine_energy_bin["width"][etrue],
+                            energy_bin["width"][etrue],
                         ],
                         x_au=[
                             dFdE_au[etrue],
                             dMdE_au[etrue, ereco],
                             Q_au[etrue],
-                            fine_energy_bin_width_au[etrue],
+                            energy_bin_width_au[etrue],
                         ],
                     )
 
@@ -124,7 +160,7 @@ for sk in SITES:
                     dRtdEt_au[sk][pk][ereco, ok],
                 ) = irf.utils.sum_elemnetwise_au(x=_P, x_au=_P_au,)
 
-            for ee in range(fine_energy_bin["num_bins"]):
+            for ee in range(energy_bin["num_bins"]):
                 (
                     dRdE[sk][pk][ee, ok],
                     dRdE_au[sk][pk][ee, ok],
@@ -135,9 +171,9 @@ for sk in SITES:
             # cross check
             # -----------
             # total rate must not change under energy migration
-            total_R = np.sum(dRdE[sk][pk][:, ok] * fine_energy_bin["width"][:])
+            total_R = np.sum(dRdE[sk][pk][:, ok] * energy_bin["width"][:])
             total_Rt = np.sum(
-                dRtdEt[sk][pk][:, ok] * fine_energy_bin["width"][:]
+                dRtdEt[sk][pk][:, ok] * energy_bin["width"][:]
             )
 
             assert 0.9 < total_R / total_Rt < 1.1
@@ -157,7 +193,7 @@ for sk in SITES:
                 "unit": "s$^{-1} (GeV)$^{-1}$",
                 "mean": dRtdEt[sk][pk],
                 "absolute_uncertainty": dRtdEt_au[sk][pk],
-                "energy_binning_key": "interpolation",
+                "energy_binning_key": energy_bin["key"],
             },
         )
 
@@ -170,7 +206,7 @@ for sk in SITES:
 
         seb.ax_add_histogram(
             ax=ax,
-            bin_edges=fine_energy_bin["edges"],
+            bin_edges=energy_bin["edges"],
             bincounts=dRtdEt[sk][pk][:, Ok],
             bincounts_upper=dRtdEt[sk][pk][:, Ok] - dRtdEt_au[sk][pk][:, Ok],
             bincounts_lower=dRtdEt[sk][pk][:, Ok] + dRtdEt_au[sk][pk][:, Ok],
@@ -183,7 +219,7 @@ for sk in SITES:
         alpha = 0.25
         seb.ax_add_histogram(
             ax=ax,
-            bin_edges=fine_energy_bin["edges"],
+            bin_edges=energy_bin["edges"],
             bincounts=dRdE[sk][pk][:, Ok],
             bincounts_upper=dRdE[sk][pk][:, Ok] - dRdE_au[sk][pk][:, Ok],
             bincounts_lower=dRdE[sk][pk][:, Ok] + dRdE_au[sk][pk][:, Ok],
