9afb4d8dc528c7730e1fe756274fe2af9fffac28
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2021-09-23 18:37:39 +0200

    rework, add options for input output paths, more compact layout, add names to close-ups, use jpg format

--- examples/plot_aberrations_off_axis_pixel.py
+++ examples/plot_aberrations_off_axis_pixel.py
@@ -1,13 +1,22 @@
 import os
+import sys
 import plenopy as pl
-import lixel_summation_tools as lst
-import matplotlib.pyplot as plt
 import scipy
 
-out_dir = os.path.join('.', 'examples', 'aberrations_off_axis_pixel')
+import matplotlib
+
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+plt.rc("text", usetex=True)
+plt.rc("font", family="serif")
+
+assert len(sys.argv) == 3, "Require path to light_field_geometry and out_dir"
+light_field_geometry_path = sys.argv[1]
+out_dir = sys.argv[2]
 os.makedirs(out_dir, exist_ok=True)
 
-light_field_geometry = pl.LightFieldGeometry('./run/light_field_calibration/')
+light_field_geometry = pl.LightFieldGeometry(light_field_geometry_path)
 
 object_distance = 999e3
 
@@ -16,20 +25,21 @@ c_direction = np.array([1, 1])
 c_direction_norm = np.linalg.norm(c_direction)
 cxys = []
 for c_radius in c_radii:
-    cxys.append(c_direction/c_direction_norm*c_radius)
+    cxys.append(c_direction / c_direction_norm * c_radius)
 cxys = np.array(cxys)
+c_names = [chr(c) for c in range(ord("A"), ord("A") + len(c_radii))]
 
 pixel_ids = []
 for cxy in cxys:
-    pixel_ids .append(
-        light_field_geometry.pixel_pos_tree.query(cxy)[1])
+    pixel_ids.append(light_field_geometry.pixel_pos_tree.query(cxy)[1])
 
 
 lixel_cx_cy_tree = scipy.spatial.cKDTree(
-    np.array([light_field_geometry.cx_mean, light_field_geometry.cy_mean]).T)
+    np.array([light_field_geometry.cx_mean, light_field_geometry.cy_mean]).T
+)
 
 
-pixel_radius = np.deg2rad(0.06667)*0.6
+pixel_radius = np.deg2rad(0.06667) * 0.6
 masks = []
 for cxy in cxys:
     proto_mask = lixel_cx_cy_tree.query(cxy, k=2000)
@@ -40,12 +50,9 @@ for cxy in cxys:
     masks.append(np.array(mask))
 
 figure_radius = 0.35
-w = 5
-h = 5
-dpi = 128
-
+dpi = 320
 for i, pixel_id in enumerate(pixel_ids):
-    fig = plt.figure(figsize=(w, h), dpi=dpi)
+    fig = plt.figure(figsize=(960 / dpi, 960 / dpi), dpi=dpi)
     lixel_ids = masks[i]
     x_center = np.mean(light_field_geometry.lixel_positions_x[lixel_ids])
     y_center = np.mean(light_field_geometry.lixel_positions_y[lixel_ids])
@@ -60,11 +67,17 @@ for i, pixel_id in enumerate(pixel_ids):
         lss=light_field_geometry,
         I=mask,
         ax=ax,
-        cmap='binary',
-        edgecolors='k')
+        cmap="binary",
+        edgecolors="grey",
+        linewidths=0.67,
+    )
 
-    ax.set_xlabel('photo-sensor-plane-x/m')
-    ax.set_ylabel('photo-sensor-plane-y/m')
+    # ax.set_xlabel('sensor-plane-x/m')
+    # ax.set_ylabel('sensor-plane-y/m')
+
+    ax.text(
+        s=c_names[i], x=0.1, y=0.8, fontsize=48, transform=ax.transAxes,
+    )
 
     ax.set_xlim([x_center - figure_radius, x_center + figure_radius])
     ax.set_ylim([y_center - figure_radius, y_center + figure_radius])
@@ -72,21 +85,19 @@ for i, pixel_id in enumerate(pixel_ids):
     plt.savefig(
         os.path.join(
             out_dir,
-            'aberration_pixel_{pixel:0d}_{direction:0d}mdeg.png'.format(
-                pixel=pixel_id,
-                direction=int(1000*np.rad2deg(c_radii[i])))),
-        dpi=dpi)
-    plt.close('all')
+            "aberration_pixel_{pixel:0d}_{direction:0d}mdeg.jpg".format(
+                pixel=pixel_id, direction=int(1000 * np.rad2deg(c_radii[i]))
+            ),
+        ),
+        dpi=dpi,
+    )
+    plt.close("all")
 
 # all spreads overview
 # --------------------
 
-w = 6
-h = 6
-dpi = 128*2
-fig = plt.figure(figsize=(w, h), dpi=dpi)
-ax = fig.add_axes([0.12, 0.12, 0.85, 0.85])
-# [x0, y0, width, height]
+fig = plt.figure(figsize=(960 / dpi, 960 / dpi), dpi=dpi)
+ax = fig.add_axes([0.16, 0.16, 0.82, 0.82])
 
 mask = np.zeros(light_field_geometry.number_lixel, dtype=np.bool)
 for lixel_ids in masks:
@@ -96,14 +107,15 @@ pl.plot.light_field_geometry.colored_lixels(
     lss=light_field_geometry,
     I=mask,
     ax=ax,
-    cmap='binary',
-    edgecolors='grey',
-    linewidths=(0.02,))
+    cmap="binary",
+    edgecolors="grey",
+    linewidths=(0.02,),
+)
 
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.set_xlabel('photo-sensor-plane-x/m')
-ax.set_ylabel('photo-sensor-plane-y/m')
+ax.spines["right"].set_visible(False)
+ax.spines["top"].set_visible(False)
+ax.set_xlabel("sensor-plane-x/m")
+ax.set_ylabel("sensor-plane-y/m")
 
 for i, mask in enumerate(masks):
     lixel_ids = mask
@@ -119,29 +131,30 @@ for i, mask in enumerate(masks):
     Dx = x_center - figure_radius
     Dy = y_center + figure_radius
 
-    ax.plot([Ax, Bx], [Ay, By], 'k', linewidth=0.5)
-    ax.plot([Bx, Cx], [By, Cy], 'k', linewidth=0.5)
-    ax.plot([Cx, Dx], [Cy, Dy], 'k', linewidth=0.5)
-    ax.plot([Dx, Ax], [Dy, Ay], 'k', linewidth=0.5)
+    ax.plot([Ax, Bx], [Ay, By], "k", linewidth=0.5)
+    ax.plot([Bx, Cx], [By, Cy], "k", linewidth=0.5)
+    ax.plot([Cx, Dx], [Cy, Dy], "k", linewidth=0.5)
+    ax.plot([Dx, Ax], [Dy, Ay], "k", linewidth=0.5)
+    ax.text(
+        s=c_names[i],
+        x=x_center - 1.0 * figure_radius,
+        y=y_center + 1.25 * figure_radius,
+    )
 
-plt.savefig(
-    os.path.join(out_dir, 'aberration_overview.jpg'),
-    dpi=dpi)
-plt.close('all')
+plt.savefig(os.path.join(out_dir, "aberration_overview.jpg"), dpi=dpi)
+plt.close("all")
 
 
-with open(os.path.join(out_dir, 'aberration_overview.txt'), 'wt') as fout:
+with open(os.path.join(out_dir, "aberration_overview.txt"), "wt") as fout:
     for i in range(len(c_radii)):
         lixel_ids = masks[i]
         x_center = np.mean(light_field_geometry.lixel_positions_x[lixel_ids])
         y_center = np.mean(light_field_geometry.lixel_positions_y[lixel_ids])
         fout.write(
-            "{:d} & {:.2f} & {:.2f} & {:.1f} & {:.2f} & {:.2f}\\\\\n".format(
+            "{:s} & {:d} & {:.2f} & {:.2f}\\\\\n".format(
+                c_names[i],
                 pixel_ids[i],
-                np.rad2deg(cxys[i, 0]),
-                np.rad2deg(cxys[i, 1]),
                 np.rad2deg(c_radii[i]),
-                x_center,
-                y_center
+                np.hypot(x_center, y_center),
             )
         )
