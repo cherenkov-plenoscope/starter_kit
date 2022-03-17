984e00376694c9435b73e0b3ec8b73375554ef6b
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-03-17 20:49:06 +0100

    black -l79

--- examples/visual_instrument_demo.py
+++ examples/visual_instrument_demo.py
@@ -10,8 +10,11 @@ import json_numpy
 WORKD_DIR = os.path.join("examples", "visual")
 SCENERY_PATH = os.path.join(WORKD_DIR, "scenery.json")
 VISUAL_CONFIG_PATH = os.path.join(WORKD_DIR, "visual_config.json")
-MERLICT_CAMERA_SERVER = os.path.join("build", "merlict", "merlict-cameraserver")
+MERLICT_CAMERA_SERVER = os.path.join(
+    "build", "merlict", "merlict-cameraserver"
+)
 os.makedirs(WORKD_DIR, exist_ok=True)
+
 
 def read_ppm_image(fstream):
     magic = fstream.readline()
@@ -24,7 +27,7 @@ def read_ppm_image(fstream):
     num_rows = int(num_rows)
     max_color = int(fstream.readline().decode())
     assert max_color == 255
-    count = num_columns*num_rows*3
+    count = num_columns * num_rows * 3
     raw = np.frombuffer(fstream.read(count), dtype=np.uint8)
     img = raw.reshape((num_rows, num_columns, 3))
     return img
@@ -78,145 +81,128 @@ def camera_command(
 
 
 acp_config = {
-    'pointing': {
-        'azimuth': 20.0,
-        'zenith_distance': 30.0,
-        },
-    'camera': {
-        'expected_imaging_system_focal_length': 106.05,
-        'expected_imaging_system_aperture_radius': 35.35,
-        'max_FoV_diameter_deg': 6.5,
-        'hex_pixel_FoV_flat2flat_deg': 0.083333,
-        'housing_overhead': 1.1,
-        'number_of_paxel_on_pixel_diagonal': 9,
-        'sensor_distance_to_principal_aperture_plane': 106.05,
-        'offset_position': [0, 0, 0],
-        'offset_rotation_tait_bryan': [0, 0, 0],
-        },
-    'system': {
-        'merlict': {
-            'hostname': '192.168.56.101',
-            'username': 'spiros',
-            'key_path': 'C:\\Users\\Spiros Daglas\\Desktop\\ssh\\spiros',
-            'run_path_linux': '/home/spiros/Desktop/run',
-            'ray_tracer_propagation_path_linux':
-                '/home/spiros/Desktop/build/mctPropagate'
-            },
-        'sap2000': {
-            'path':
-                'C:\Program Files\Computers and Structures\SAP2000 19\sap2000.exe',
-            'working_directory':
-                'C:\\Users\\Spiros Daglas\\Desktop\\SAP2000_working_directory\\example_1'
-            }
-        },
-    'structure_spatial_position': {
-        'translational_vector_xyz': [0.0, 0.0, 0.0],
-        # not used anymore. created from the tait bryan angle Ry
-        'rotational_vector_Rx_Ry_Rz': [0.0, 0.0, 0.0]
-        },
-    'reflector': {
-        'main': {
-            'max_outer_radius': 40.8187,
-            'min_inner_radius': 2.5,
-            'number_of_layers': 3,
-            'x_over_z_ratio': 1.66,
-            # for truss function always keep it between 1.36 and 2.26
-            'security_distance_from_ground': 2.6
-            },
-        'optics': {
-            'focal_length': 106.05,
-            'davies_cotton_over_parabola_ratio': 0.0
-            },
-        'facet': {
-            'gap_in_between': 0.025,
-            'inner_hex_radius': 0.75,  # CTA LST facet size
-            'surface_weight': 20.0,
-            'actuator_weight': 0.0
-            },
-        'material': {
-            'specific_weight': 78.5,
-            'e_modul': 210e6,
-            'yielding_point': 1460000.0,
-            'ultimate_point': 1360000.0,
-            'security_factor': 1.05
-            },
-        'bars': {
-            'outer_diameter': 0.10,
-            'thickness': 0.0025,
-            'imperfection_factor': 0.49,
-            'buckling_length_factor': 0.9
-            }
-        },
-    'tension_ring': {
-        'width': 1.1,
-        'support_position': 10,
-        'material': {
-            'specific_weight': 78.5,
-            'e_modul': 210e6,
-            'yielding_point': 1460000.0,
-            'ultimate_point': 1360000.0,
-            'security_factor': 1.05
-            },
-        'bars': {
-            'outer_diameter': 0.081,
-            'thickness': 0.005,
-            'imperfection_factor': 0.49,
-            'buckling_length_factor': 0.9
-            }
-        },
-    'cables': {
-        'material': {
-            'e_modul': 95e6,
-            # according to Bridon Endurance Dyform 18 PI
-            'specific_weight': 89.9,
-            # according to Bridon Endurance Dyform 18 PI
-            'yielding_point': 1671000.0,
-            'ultimate_point': 1671000.0,
-            'security_factor': 1.05
-            },
-        'cross_section_area': 0.000221
+    "pointing": {"azimuth": 20.0, "zenith_distance": 30.0,},
+    "camera": {
+        "expected_imaging_system_focal_length": 106.05,
+        "expected_imaging_system_aperture_radius": 35.35,
+        "max_FoV_diameter_deg": 6.5,
+        "hex_pixel_FoV_flat2flat_deg": 0.083333,
+        "housing_overhead": 1.1,
+        "number_of_paxel_on_pixel_diagonal": 9,
+        "sensor_distance_to_principal_aperture_plane": 106.05,
+        "offset_position": [0, 0, 0],
+        "offset_rotation_tait_bryan": [0, 0, 0],
     },
-    'load_scenario': {
-        'security_factor': {
-            'dead': 1.00,
-            'live': 1.00,
-            'wind': 1.00
-            },
-        'wind': {
-            'direction': 0.0,
+    "system": {
+        "merlict": {
+            "hostname": "192.168.56.101",
+            "username": "spiros",
+            "key_path": "C:\\Users\\Spiros Daglas\\Desktop\\ssh\\spiros",
+            "run_path_linux": "/home/spiros/Desktop/run",
+            "ray_tracer_propagation_path_linux": "/home/spiros/Desktop/build/mctPropagate",
+        },
+        "sap2000": {
+            "path": "C:\Program Files\Computers and Structures\SAP2000 19\sap2000.exe",
+            "working_directory": "C:\\Users\\Spiros Daglas\\Desktop\\SAP2000_working_directory\\example_1",
+        },
+    },
+    "structure_spatial_position": {
+        "translational_vector_xyz": [0.0, 0.0, 0.0],
+        # not used anymore. created from the tait bryan angle Ry
+        "rotational_vector_Rx_Ry_Rz": [0.0, 0.0, 0.0],
+    },
+    "reflector": {
+        "main": {
+            "max_outer_radius": 40.8187,
+            "min_inner_radius": 2.5,
+            "number_of_layers": 3,
+            "x_over_z_ratio": 1.66,
+            # for truss function always keep it between 1.36 and 2.26
+            "security_distance_from_ground": 2.6,
+        },
+        "optics": {
+            "focal_length": 106.05,
+            "davies_cotton_over_parabola_ratio": 0.0,
+        },
+        "facet": {
+            "gap_in_between": 0.025,
+            "inner_hex_radius": 0.75,  # CTA LST facet size
+            "surface_weight": 20.0,
+            "actuator_weight": 0.0,
+        },
+        "material": {
+            "specific_weight": 78.5,
+            "e_modul": 210e6,
+            "yielding_point": 1460000.0,
+            "ultimate_point": 1360000.0,
+            "security_factor": 1.05,
+        },
+        "bars": {
+            "outer_diameter": 0.10,
+            "thickness": 0.0025,
+            "imperfection_factor": 0.49,
+            "buckling_length_factor": 0.9,
+        },
+    },
+    "tension_ring": {
+        "width": 1.1,
+        "support_position": 10,
+        "material": {
+            "specific_weight": 78.5,
+            "e_modul": 210e6,
+            "yielding_point": 1460000.0,
+            "ultimate_point": 1360000.0,
+            "security_factor": 1.05,
+        },
+        "bars": {
+            "outer_diameter": 0.081,
+            "thickness": 0.005,
+            "imperfection_factor": 0.49,
+            "buckling_length_factor": 0.9,
+        },
+    },
+    "cables": {
+        "material": {
+            "e_modul": 95e6,
+            # according to Bridon Endurance Dyform 18 PI
+            "specific_weight": 89.9,
+            # according to Bridon Endurance Dyform 18 PI
+            "yielding_point": 1671000.0,
+            "ultimate_point": 1671000.0,
+            "security_factor": 1.05,
+        },
+        "cross_section_area": 0.000221,
+    },
+    "load_scenario": {
+        "security_factor": {"dead": 1.00, "live": 1.00, "wind": 1.00},
+        "wind": {
+            "direction": 0.0,
             # OK
-            'speed': 55,
+            "speed": 55,
             # m/s.OK
-            'terrain_factor': 1,
+            "terrain_factor": 1,
             # Terrain 1.OK
-            'orography_factor': 1,
+            "orography_factor": 1,
             # No increase of the wind due to mountains etc.OK
-            'K1': 1,
+            "K1": 1,
             # Turbulence factor. No accurate information available.OK
-            'CsCd': 1.2,
+            "CsCd": 1.2,
             # usually 1. But our structure very prone to dynamic efects,
             # so Cd very conservative 1.2.OK
-            'wind_density': 1.25,
+            "wind_density": 1.25,
             # wind density.OK
-            'cpei': 1.5
+            "cpei": 1.5
             # according to EC1-4 Z.7.3(freistehende Dächer) und Z. 7.2 Tab.7.4a
             # (big?, although a preciser definition is impossible), OK
-            },
-        'seismic': {
-            'acceleration': 3.6
-            }
         },
-    'star_light_analysis': {
-        'photons_per_square_meter': 1000,
-        'sensor': {
-            'bin_width_deg': 0.0005,
-            'region_of_interest_deg': 0.5
-            },
-        'ground': {
-            'bin_width_m': 0.1
-            }
-        }
-    }
+        "seismic": {"acceleration": 3.6},
+    },
+    "star_light_analysis": {
+        "photons_per_square_meter": 1000,
+        "sensor": {"bin_width_deg": 0.0005, "region_of_interest_deg": 0.5},
+        "ground": {"bin_width_m": 0.1},
+    },
+}
 
 if not os.path.exists(SCENERY_PATH):
     geometry = rs.Geometry(acp_config)
@@ -228,29 +214,20 @@ if not os.path.exists(SCENERY_PATH):
 
 merlict_visual_config = {
     "max_interaction_depth": 41,
-    "preview": {
-        "cols": 128,
-        "rows": 72,
-        "scale": 10
-    },
+    "preview": {"cols": 128, "rows": 72, "scale": 10},
     "snapshot": {
         "cols": 1920,
-        "rows":  1080,
+        "rows": 1080,
         "noise_level": 25,
         "focal_length_over_aperture_diameter": 0.95,
-        "image_sensor_size_along_a_row": 0.07
+        "image_sensor_size_along_a_row": 0.07,
     },
     "global_illumination": {
         "on": True,
-        "incoming_direction": [-0.15, -0.2, 1.0]
+        "incoming_direction": [-0.15, -0.2, 1.0],
     },
-    "sky_dome": {
-        "path": "",
-        "color": [255, 255, 255]
-    },
-    "photon_trajectories": {
-        "radius": 0.15
-    }
+    "sky_dome": {"path": "", "color": [255, 255, 255]},
+    "photon_trajectories": {"radius": 0.15},
 }
 
 json_numpy.write(VISUAL_CONFIG_PATH, merlict_visual_config)
@@ -258,55 +235,55 @@ json_numpy.write(VISUAL_CONFIG_PATH, merlict_visual_config)
 image_general_config = {
     "sensor_size": 0.3,
     "f_stop": 0.95,
-    "num_columns": 512*2,
-    "num_rows": 288*2,
-    "noise_level": 200
+    "num_columns": 512 * 2,
+    "num_rows": 288 * 2,
+    "noise_level": 200,
 }
 
 image_configs = {
     "top": {
-        "position": [-1.256e+00, 0.000e+00, 1.200e+03],
-        "orientation": np.deg2rad([0 ,-1.800e+02, -7.249e+01]),
+        "position": [-1.256e00, 0.000e00, 1.200e03],
+        "orientation": np.deg2rad([0, -1.800e02, -7.249e01]),
         "object_distance": 1200,
         "field_of_view": np.deg2rad(14),
     },
     "top_total": {
-        "position": [-1.256e+00, 0.000e+00, 1.200e+03],
-        "orientation": np.deg2rad([0 ,-1.800e+02, 45.0]),
+        "position": [-1.256e00, 0.000e00, 1.200e03],
+        "orientation": np.deg2rad([0, -1.800e02, 45.0]),
         "object_distance": 1200,
         "field_of_view": np.deg2rad(30),
     },
     "mirror_closeup": {
-        "position": [3.609e+00,  9.980e+01,  1.825e+01],
-        "orientation": np.deg2rad([0 ,-8.508e+01,  1.032e+02]),
+        "position": [3.609e00, 9.980e01, 1.825e01],
+        "orientation": np.deg2rad([0, -8.508e01, 1.032e02]),
         "object_distance": 90,
         "field_of_view": np.deg2rad(44.4),
     },
     "sensor_closeup_mirror_background": {
-        "position": [5.431e+01,  3.093e+00,  1.530e+02],
-        "orientation": np.deg2rad([0 ,-1.461e+02,  1.726e+02]),
+        "position": [5.431e01, 3.093e00, 1.530e02],
+        "orientation": np.deg2rad([0, -1.461e02, 1.726e02]),
         "object_distance": 150,
-        "field_of_view": np.deg2rad(5.909e+01),
+        "field_of_view": np.deg2rad(5.909e01),
     },
     "side_total_from_distance": {
-        "position": [2.182e+03,  6.191e+02,  8.434e+00 ],
-        "orientation": np.deg2rad([0 ,-8.759e+01, -1.959e+02]),
+        "position": [2.182e03, 6.191e02, 8.434e00],
+        "orientation": np.deg2rad([0, -8.759e01, -1.959e02]),
         "object_distance": 2300,
-        "field_of_view": np.deg2rad(1.063e+01),
+        "field_of_view": np.deg2rad(1.063e01),
     },
 }
 
 call = [
     MERLICT_CAMERA_SERVER,
-    '--scenery',
+    "--scenery",
     SCENERY_PATH,
-    '--config',
-    VISUAL_CONFIG_PATH
+    "--config",
+    VISUAL_CONFIG_PATH,
 ]
 
 merlict = sp.Popen(call, stdin=sp.PIPE, stdout=sp.PIPE)
 for imgkey in image_configs:
-    imgstempath = os.path.join(WORKD_DIR, '{:s}'.format(imgkey))
+    imgstempath = os.path.join(WORKD_DIR, "{:s}".format(imgkey))
     imgpath = imgstempath + ".tiff"
     imgcfgpath = imgstempath + ".json"
 
@@ -318,7 +295,6 @@ for imgkey in image_configs:
         img = read_ppm_image(merlict.stdout)
         io.imsave(imgpath, img)
         json_numpy.write(imgcfgpath, full_config)
-
 
     sp.call(["convert", imgpath, imgstempath + ".jpg"])
 
