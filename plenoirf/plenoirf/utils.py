a652822d12082b9b2891c2123f7736c62ac1a29c
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-03-25 17:24:37 +0100

    add solid angle unit conversion

--- plenoirf/plenoirf/utils.py
+++ plenoirf/plenoirf/utils.py
@@ -10,6 +10,14 @@ def cone_solid_angle(cone_radial_opening_angle_rad):
     return 2.0 * np.pi * cap_hight
 
 
+def sr2squaredeg(solid_angle_sr):
+    return solid_angle_sr * (180.0/np.pi) ** 2
+
+
+def squaredeg2sr(solid_angle_squaredeg):
+    return solid_angle_squaredeg * (np.pi/180) ** 2
+
+
 def contains_same_bytes(path_a, path_b):
     with open(path_a, "rb") as fa, open(path_b, "rb") as fb:
         a_bytes = fa.read()
