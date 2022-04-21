ab17bd59ee81b3a702d298114f4854e2cb557ba9
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-04-21 10:12:54 +0200

    add docu

--- plenoirf/plenoirf/bundle.py
+++ plenoirf/plenoirf/bundle.py
@@ -3,6 +3,23 @@ import copy
 
 
 def make_jobs_in_bundles(jobs, desired_num_bunbles):
+    """
+    When you have too many jobs for your parallel processing queue this
+    function bundle multiple of your jobs into fewer bundles of jobs.
+
+    Parameters
+    ----------
+    jobs : list
+        A list of your jobs.
+    desired_num_bunbles : int
+        The maximum number of bundles. Your jobs will be spread over
+        these many bundles.
+
+    Returns
+    -------
+        A list of bundles where each bundle is a list of jobs.
+        The lengths of the list of bundles is <= desired_num_bunbles.
+    """
     assert desired_num_bunbles > 0
     bundles = []
     num_jobs = len(jobs)
@@ -21,10 +38,19 @@ def make_jobs_in_bundles(jobs, desired_num_bunbles):
 
 
 def _run_job_example(job):
+    """
+    For testing. This function processes one job.
+    You already have this for your jobs.
+    """
     return job * job
 
 
 def _run_jobs_in_bundles_example(bundle):
+    """
+    For testing. This function processes one bundle of jobs by
+    looping over the jobs in a bundle.
+    You need to provide this function.
+    """
     results = []
     for j, job in enumerate(bundle):
         result = _run_job_example(job=job)
