9dc885b5d8f24957ec4bd805043b4ec67f3b66f8
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-09-22 08:17:12 +0200

    tools to estimate what an outer-telescope-array can do for us

--- plenoirf/plenoirf/analysis/__init__.py
+++ plenoirf/plenoirf/analysis/__init__.py
@@ -5,3 +5,4 @@ from . import cuts
 from . import spectral_energy_distribution
 from . import energy
 from . import reweight
+from . import outer_telescope_array
---
+++ plenoirf/plenoirf/analysis/outer_telescope_array.py
@@ -0,0 +1,38 @@
+import numpy as np
+import skimage
+
+NUM_BINS_ON_EDGE = 25
+CENTER_BIN = NUM_BINS_ON_EDGE // 2
+
+
+
+def init_telescope_positions_in_annulus(outer_radius, inner_radius):
+    ccouter, rrouter = skimage.draw.circle(c=0, r=0, radius=outer_radius)
+    ccinner, rrinner = skimage.draw.circle(c=0, r=0, radius=inner_radius)
+    pos = set()
+    for i in range(len(ccouter)):
+        pos.add((ccouter[i], rrouter[i]))
+    for i in range(len(ccinner)):
+        pos.remove((ccinner[i], rrinner[i]))
+    pos = list(pos)
+    out = [[p[0], p[1]] for p in pos]
+    return out
+
+
+def init_mask_from_telescope_positions(positions):
+    mask = np.zeros(shape=(NUM_BINS_ON_EDGE, NUM_BINS_ON_EDGE), dtype=np.bool)
+    for pos in positions:
+        mask[pos[0] + CENTER_BIN, pos[1] + CENTER_BIN] = True
+    return mask
+
+
+EXAMPLE_CONFIGURATION = {
+    "mirror_diameter_m": 12.0,
+    "positions": init_telescope_positions_in_annulus(
+        outer_radius=2.5,
+        inner_radius=0.5,
+    ),
+}
+EXAMPLE_CONFIGURATION["mask"] = init_mask_from_telescope_positions(
+    positions=EXAMPLE_CONFIGURATION["positions"]
+)
