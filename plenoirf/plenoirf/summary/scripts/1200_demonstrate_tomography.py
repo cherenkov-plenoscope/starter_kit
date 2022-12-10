53c7f9b43978f38b383a186bc7369568f2d44391
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 23:07:27 +0000

    Adding tarfile member sanitization to extractall()

--- plenoirf/plenoirf/summary/scripts/1200_demonstrate_tomography.py
+++ plenoirf/plenoirf/summary/scripts/1200_demonstrate_tomography.py
@@ -77,7 +77,26 @@ def read_simulation_truth_for_tomography(
             raw_sensor_response_dir, uid_str + ".tar"
         )
         with tarfile.open(response_tar_path, "r") as tf:
-            tf.extractall(response_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tf, response_path)
             event = pl.Event(
                 path=response_path, light_field_geometry=light_field_geometry,
             )
