63aedd1232944b4972217470b9eb983ff45a5e75
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-05-22 13:23:58 +0200

    plot instruments and scenarios

--- aberration_demo/aberration_demo/full/scripts/plot_image_of_star_vs_offaxis.py
+++ aberration_demo/aberration_demo/full/scripts/plot_image_of_star_vs_offaxis.py
@@ -125,7 +125,38 @@ SOLID_ANGLE_SCALE = 1e6
 ylabel_name = r"solid angle containing 80%"
 label_sep = r"$\,/\,$"
 
+
+same_scenario_different_sensors = {}
+for mirror_def_key in config["mirror_deformations"]:
+    for sensor_tra_key in config["sensor_transformations"]:
+        sstt_key = (mirror_def_key, sensor_tra_key)
+        same_scenario_different_sensors[sstt_key] = []
+
+        for instrument_key in INSTRUMENTS:
+            is_mirror_def = config["instruments"][instrument_key]["mirror_deformation"] == mirror_def_key
+            is_sensor_tra = config["instruments"][instrument_key]["sensor_transformation"] == sensor_tra_key
+            if is_mirror_def and is_sensor_tra:
+                same_scenario_different_sensors[sstt_key].append(instrument_key)
+
+
+PLOTS = []
 for instrument_key in INSTRUMENTS:
+    PLOTS.append({
+        "instruments": [instrument_key],
+        "filename": "instrument_{:s}".format(instrument_key)
+    })
+
+
+for scenario_key in same_scenario_different_sensors:
+    if len(same_scenario_different_sensors[scenario_key]):
+        mirror_def_key, sensor_tra_key = scenario_key
+        PLOTS.append({
+            "instruments": same_scenario_different_sensors[scenario_key],
+            "filename": "mirror_deformation_{:s}_sensor_transformation_{:s}".format(mirror_def_key, sensor_tra_key)
+        })
+
+
+for PLOT in PLOTS:
 
     fig = sebplt.figure(style={"rows": 720, "cols": 1280, "fontsize": 1})
     ax_usr = sebplt.add_axes(fig, [0.15, 0.2, 0.725, 0.75])
@@ -138,44 +169,45 @@ for instrument_key in INSTRUMENTS:
     )
     ax_usr.set_ylabel(ylabel_name + label_sep + r"$\mu$sr")
 
-    solid_angle_80_deg2_start = plenoirf.utils.sr2squaredeg(
+    SOLID_ANGLE_80_DEG2_START = plenoirf.utils.sr2squaredeg(
         SOLID_ANGLE_80_SR_START
     )
-    solid_angle_80_deg2_stop = plenoirf.utils.sr2squaredeg(
+    SOLID_ANGLE_80_DEG2_STOP = plenoirf.utils.sr2squaredeg(
         SOLID_ANGLE_80_SR_STOP
     )
     ax_deg2.set_ylim(
-        np.array([solid_angle_80_deg2_start, solid_angle_80_deg2_stop])
+        np.array([SOLID_ANGLE_80_DEG2_START, SOLID_ANGLE_80_DEG2_STOP])
     )
     ax_deg2.set_ylabel(r"(1$^{\circ}$)$^2$")
 
-    oa_rad = psf_vs_oa[instrument_key]["mean"]
-    oa_std_rad = psf_vs_oa[instrument_key]["std"]
+    for instrument_key in PLOT["instruments"]:
+        oa_rad = psf_vs_oa[instrument_key]["mean"]
+        oa_std_rad = psf_vs_oa[instrument_key]["std"]
 
-    sa_usr = SOLID_ANGLE_SCALE * plenoirf.utils.cone_solid_angle(
-        cone_radial_opening_angle_rad=oa_rad
-    )
-    sa_upper_usr = SOLID_ANGLE_SCALE * plenoirf.utils.cone_solid_angle(
-        cone_radial_opening_angle_rad=oa_rad + oa_std_rad
-    )
-    sa_lower_usr = SOLID_ANGLE_SCALE * plenoirf.utils.cone_solid_angle(
-        cone_radial_opening_angle_rad=oa_rad - oa_std_rad
-    )
+        sa_usr = SOLID_ANGLE_SCALE * plenoirf.utils.cone_solid_angle(
+            cone_radial_opening_angle_rad=oa_rad
+        )
+        sa_upper_usr = SOLID_ANGLE_SCALE * plenoirf.utils.cone_solid_angle(
+            cone_radial_opening_angle_rad=oa_rad + oa_std_rad
+        )
+        sa_lower_usr = SOLID_ANGLE_SCALE * plenoirf.utils.cone_solid_angle(
+            cone_radial_opening_angle_rad=oa_rad - oa_std_rad
+        )
 
-    sebplt.ax_add_histogram(
-        ax=ax_usr,
-        bin_edges=oa_bin_edges_deg ** 2,
-        bincounts=sa_usr,
-        linestyle="-",
-        linecolor="k",
-        linealpha=1.0,
-        bincounts_upper=sa_upper_usr,
-        bincounts_lower=sa_lower_usr,
-        face_color="k",
-        face_alpha=0.1,
-        label=None,
-        draw_bin_walls=True,
-    )
+        sebplt.ax_add_histogram(
+            ax=ax_usr,
+            bin_edges=oa_bin_edges_deg ** 2,
+            bincounts=sa_usr,
+            linestyle="-",
+            linecolor="k",
+            linealpha=1.0,
+            bincounts_upper=sa_upper_usr,
+            bincounts_lower=sa_lower_usr,
+            face_color="k",
+            face_alpha=0.1,
+            label=None,
+            draw_bin_walls=True,
+        )
 
     xt_deg2 = ax_usr.get_xticks()
     ax_usr.set_xticklabels(
@@ -183,193 +215,33 @@ for instrument_key in INSTRUMENTS:
     )
 
     ax_usr.set_xlabel(r"(offaxis angle)$^{2}\,/\,(1^{\circ{}})^{2}$")
-    fig_filename = "instrument_{:s}.jpg".format(instrument_key)
+    fig_filename = "{:s}.jpg".format(PLOT["filename"])
     fig.savefig(os.path.join(out_dir, fig_filename))
     sebplt.close(fig)
 
 
-"""
-
-# plot psf80 vs off-axes
-# ----------------------
-NUM_PAXEL_STYLE = {
-    "paxel000001": {
-        "color": "gray",
-        "linestyle": "-",
-        "alpha": 1,
-        "marker": "P",
-    },
-    "paxel000003": {
-        "color": "gray",
-        "linestyle": "-",
-        "alpha": 0.3,
-        "marker": "s",
-    },
-    "paxel000009": {
-        "color": "black",
-        "linestyle": "-",
-        "alpha": 1,
-        "marker": "o",
-    },
-}
-SOLID_ANGLE_SCALE = 1e6
-
-solid_angle_80_sr_start = 0
-solid_angle_80_sr_stop = 20e-6
-
-fig = sebplt.figure(style={"rows": 640, "cols": 1280, "fontsize": 1})
-ax = sebplt.add_axes(fig=fig, span=[0.15, 0.2, 0.72, 0.75],)
-ax_deg2 = ax.twinx()
-ax_deg2.spines["top"].set_visible(False)
-
-
-ylabel_name = r"solid angle containing 80%"
-label_sep = r"$\,/\,$"
-ax.set_ylim(
-    SOLID_ANGLE_SCALE
-    * np.array([solid_angle_80_sr_start, solid_angle_80_sr_stop])
-)
-# ax.semilogy()
-ax.set_ylabel(ylabel_name + label_sep + r"$\mu$sr")
-
-solid_angle_80_deg2_start = plenoirf.utils.sr2squaredeg(
-    solid_angle_80_sr_start
-)
-solid_angle_80_deg2_stop = plenoirf.utils.sr2squaredeg(solid_angle_80_sr_stop)
-ax_deg2.set_ylim(
-    np.array([solid_angle_80_deg2_start, solid_angle_80_deg2_stop])
-)
-# ax_deg2.semilogy()
-ax_deg2.set_ylabel(r"(1$^{\circ}$)$^2$")
-
-sebplt.ax_add_grid(ax=ax, add_minor=True)
-
-average_angle80_in_fov = {}
-for isens, pkey in enumerate(coll):
-
-    offaxis_angles_deg = config["sources"]["off_axis_angles_deg"]
-    angles80_rad = np.zeros(len(offaxis_angles_deg))
-    for iang, akey in enumerate(coll[pkey]):
-        angles80_rad[iang] = coll[pkey][akey]["image"]["angle80"]
-
-    cone80_solid_angle_sr = np.zeros(len(angles80_rad))
-    for iang in range(len(angles80_rad)):
-        cone80_solid_angle_sr[iang] = plenoirf.utils.cone_solid_angle(
-            cone_radial_opening_angle_rad=angles80_rad[iang]
-        )
-
-    off_axis_weight = np.pi * offaxis_angles_deg ** 2
+average_angle80_rad = {}
+for instrument_key in INSTRUMENTS:
+    off_axis_weight = np.pi * psf[instrument_key]["cc_deg"] ** 2
     off_axis_weight /= np.sum(off_axis_weight)
-    average_angle80_in_fov[pkey] = np.average(
-        angles80_rad, weights=off_axis_weight,
+    average_angle80_rad[instrument_key] = np.average(
+        psf[instrument_key]["angle80_rad"], weights=off_axis_weight,
     )
 
-    ax.plot(
-        offaxis_angles_deg,
-        cone80_solid_angle_sr * SOLID_ANGLE_SCALE,
-        color=NUM_PAXEL_STYLE[pkey]["color"],
-        linestyle=NUM_PAXEL_STYLE[pkey]["linestyle"],
-        alpha=NUM_PAXEL_STYLE[pkey]["alpha"],
-    )
+# export average
+# --------------
 
-    for iang in range(len(angles80_rad)):
-        if iang in OFFAXIS_ANGLE_IDXS:
-            markersize = 8
-        else:
-            markersize = 3
-        marker = NUM_PAXEL_STYLE[pkey]["marker"]
-        ax.plot(
-            offaxis_angles_deg[iang],
-            cone80_solid_angle_sr[iang] * SOLID_ANGLE_SCALE,
-            color=NUM_PAXEL_STYLE[pkey]["color"],
-            alpha=NUM_PAXEL_STYLE[pkey]["alpha"],
-            marker=marker,
-            markersize=markersize,
-            linewidth=0,
-        )
+out_average_angle80_rad = {}
+for instrument_key in INSTRUMENTS:
+    ha_rad = average_angle80_rad[instrument_key]
 
-ax.set_xlabel(r"angle off the mirror's optical axis$\,/\,1^{\circ}$")
+    sa_sr = plenoirf.utils.cone_solid_angle(ha_rad)
+    sa_deg2 = plenoirf.utils.sr2squaredeg(sa_sr)
 
-fig.savefig(os.path.join(out_dir, "psf_vs_num_paxel_vs_off_axis.jpg"))
-sebplt.close(fig)
+    out_average_angle80_rad[instrument_key] = {
+        "half_angle": {"deg": np.rad2deg(ha_rad), "rad": ha_rad},
+        "solid_angle": {"sr": sa_sr, "deg2": sa_deg2}
+    }
 
-
-with open(
-    os.path.join(out_dir, "psf_vs_num_paxel_vs_off_axis.txt"), "wt"
-) as f:
-    f.write("{:>20s},".format("offaxis/deg"))
-    for isens, pkey in enumerate(coll):
-        f.write("{:>20s},".format(pkey))
-    f.write("\n")
-
-    # half angle
-    # ==========
-    f.write("{:>20s},".format(""))
-    for isens, pkey in enumerate(coll):
-        f.write("{:>20s},".format("half-angle-80/deg"))
-    f.write("\n")
-
-    for iang, akey in enumerate(coll[pkey]):
-        offax_deg = config["sources"]["off_axis_angles_deg"][iang]
-        f.write("{: 20.2},".format(offax_deg))
-        for isens, pkey in enumerate(coll):
-
-            half_angle80_rad = coll[pkey][akey]["image"]["angle80"]
-            half_angle80_deg = np.rad2deg(half_angle80_rad)
-            f.write("{: 20.2},".format(half_angle80_deg))
-
-        f.write("\n")
-
-    f.write("\n")
-
-    # solid angle
-    # ===========
-    f.write("{:>20s},".format(""))
-    for isens, pkey in enumerate(coll):
-        f.write("{:>20s},".format("solid-angle-80/usr"))
-    f.write("\n")
-
-    for iang, akey in enumerate(coll[pkey]):
-        offax_deg = config["sources"]["off_axis_angles_deg"][iang]
-        f.write("{: 20.2},".format(offax_deg))
-        for isens, pkey in enumerate(coll):
-
-            half_angle80_rad = coll[pkey][akey]["image"]["angle80"]
-            solid_angle_80_sr = plenoirf.utils.cone_solid_angle(
-                cone_radial_opening_angle_rad=half_angle80_rad
-            )
-            f.write("{: 20.2},".format(SOLID_ANGLE_SCALE * solid_angle_80_sr))
-
-        f.write("\n")
-
-    f.write("\n")
-
-    # average
-    # =======
-    f.write("{:>20s},".format(""))
-    for pkey in coll:
-        f.write("{:>20s},".format("avg-solid-80/usr"))
-    f.write("\n")
-
-    f.write("{:>20s},".format(""))
-    for pkey in coll:
-        avg_solid_angle_usr = (
-            SOLID_ANGLE_SCALE
-            * plenoirf.utils.cone_solid_angle(
-                cone_radial_opening_angle_rad=average_angle80_in_fov[pkey]
-            )
-        )
-        f.write("{: 20.2},".format(avg_solid_angle_usr))
-    f.write("\n")
-
-    f.write("{:>20s},".format(""))
-    for pkey in coll:
-        f.write("{:>20s},".format("avg-angle-80/deg"))
-    f.write("\n")
-
-    f.write("{:>20s},".format(""))
-    for pkey in coll:
-        average_angle80_in_fov_deg = np.rad2deg(average_angle80_in_fov[pkey])
-        f.write("{: 20.2},".format(average_angle80_in_fov_deg))
-    f.write("\n")
-"""
+with open(os.path.join(out_dir, "average_containment80.txt"), "wt") as f:
+    f.write(json_numpy.dumps(out_average_angle80_rad, indent=4))
