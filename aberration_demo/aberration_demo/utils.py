37810f6b1a41644f8a94a37cc3774097472b8efd
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-05-24 12:07:03 +0200

    obsolete and blac

--- aberration_demo/aberration_demo/utils.py
+++ aberration_demo/aberration_demo/utils.py
@@ -1,28 +1,5 @@
 import os
-import json_numpy
 import plenopy
-
-
-ANGLE_FMT = "angle{:06d}"
-PAXEL_FMT = "paxel{:06d}"
-
-
-def read_config(work_dir):
-    """
-    Returns the config in work_dir/config.json.
-
-    Parameters
-    ----------
-    work_dir : str
-        Path to the work_dir
-    """
-    return read_json(path=os.path.join(work_dir, "config.json"))
-
-
-def read_json(path):
-    with open(path, "rt") as f:
-        cont = json_numpy.loads(f.read())
-    return cont
 
 
 def guess_scaling_of_num_photons_used_to_estimate_light_field_geometry(
