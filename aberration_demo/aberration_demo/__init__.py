0bcda8b1a143fe1230243ffb9a3dd3d1b618582f
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-05-24 11:59:49 +0200

    obsolete

--- aberration_demo/aberration_demo/__init__.py
+++ aberration_demo/aberration_demo/__init__.py
@@ -7,23 +7,3 @@ from . import portal
 from . import analysis
 from . import utils
 from . import image
-import os
-
-
-def init(work_dir):
-    os.makedirs(work_dir, exist_ok=True)
-    offaxis.init(work_dir=os.path.join(work_dir, "offaxis"))
-    deformations.init(work_dir=os.path.join(work_dir, "deformations"))
-
-
-def run(work_dir, map_and_reduce_pool, logger):
-    offaxis.run(
-        work_dir=os.path.join(work_dir, "offaxis"),
-        map_and_reduce_pool=map_and_reduce_pool,
-        logger=logger,
-    )
-    deformations.run(
-        work_dir=os.path.join(work_dir, "deformations"),
-        map_and_reduce_pool=map_and_reduce_pool,
-        logger=logger,
-    )
